af2cd0638c3f7afe57d9e99bd9667e8683cd333a
Michał Walenciak <Kicer86@gmail.com>
2013-02-17 12:42:29 +0100

    EntryData class introduced. Config related files moved to new dir - "configuration"

---
+++ src/configuration/entrydata.hpp
@@ -0,0 +1,35 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2013  Michał Walenciak <MichalWalenciak@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#ifndef ENTRYDATA_HPP
+#define ENTRYDATA_HPP
+
+class EntryData
+{
+
+    public:
+        EntryData();
+        EntryData(const EntryData& other);
+        virtual ~EntryData();
+        virtual EntryData& operator=(const EntryData& other);
+        virtual bool operator==(const EntryData& other) const;
+};
+
+#endif // ENTRYDATA_HPP
