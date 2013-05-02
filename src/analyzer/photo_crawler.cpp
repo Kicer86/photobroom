84e51292ef82d952da11a253170f5deaff8dd7dc
Michał Walenciak <Kicer86@gmail.com>
2013-05-02 21:55:56 +0200

    photo_crawler: empty implementation

--- src/analyzer/photo_crawler.cpp
+++ src/analyzer/photo_crawler.cpp
@@ -1,6 +1,18 @@
 
 #include "photo_crawler.hpp"
 
+namespace
+{
+	struct Crawler: public IPhotoCrawler
+	{
+		void crawl(const std::vector<std::string> &paths)
+		{
+
+		}
+	};
+}
+
+
 IPhotoCrawler* PhotoCrawlerFactory::get()
 {
 	return nullptr;
