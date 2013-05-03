2988d58875c05c1124d39d5374bb2d69144b0fd7
Michał Walenciak <Kicer86@gmail.com>
2013-05-03 15:47:15 +0200

    FileAnalyzer introduced

---
+++ src/analyzer/implementation/ifile_analyzer.hpp
@@ -0,0 +1,11 @@
+
+#ifndef ANALYZER_IFILE_ANALYZER
+#define ANALYZER_IFILE_ANALYZER
+
+struct IFileAnalyzer
+{
+	virtual ~IFileAnalyzer () {}
+	virtual bool isImage(const std::string &) = 0;
+};
+
+#endif
