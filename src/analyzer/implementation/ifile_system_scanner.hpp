0d3825bb549e2a19a76fbc01c476952546455192
Michał Walenciak <Kicer86@gmail.com>
2013-05-03 13:31:22 +0200

    IFileSystemScanner introduced

---
+++ src/analyzer/implementation/ifile_system_scanner.hpp
@@ -0,0 +1,15 @@
+
+#ifndef ANALYZER_FILESYSTEM_SCANNER_HPP
+#define ANALYZER_FILESYSTEM_SCANNER_HPP
+
+#include <vector>
+#include <string>
+
+struct IFileSystemScanner
+{
+    virtual IFileSystemScanner() {}
+    
+    virtual std::vector<std::string> getFilesFor(const std::string &) = 0;
+};
+
+#endif
