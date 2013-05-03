13e72b1991aa1662724a7f16cd46e538d153f311
Michał Walenciak <Kicer86@gmail.com>
2013-05-03 15:34:22 +0200

    FileSystemScanner: primary implementation

--- src/analyzer/implementation/filesystemscanner.cpp
+++ src/analyzer/implementation/filesystemscanner.cpp
@@ -20,9 +20,21 @@
 
 #include "filesystemscanner.hpp"
 
-std::vector< std::string, std::allocator< std::string > > FileSystemScanner::getFilesFor(const std::string &path)
+#include <boost/filesystem.hpp>
+
+std::vector< std::string, std::allocator< std::string > > FileSystemScanner::getFilesFor(const std::string &dir_path)
 {
 	std::vector<std::string> results;
+
+	boost::filesystem::path p(dir_path);
+
+	boost::filesystem::recursive_directory_iterator dirIt(p), dirItEnd;
+
+	for(; dirIt != dirItEnd; ++dirIt)
+	{
+		const boost::filesystem::path &entry = *dirIt;
+		results.push_back( entry.string() );
+	}
 
 	return results;
 }
