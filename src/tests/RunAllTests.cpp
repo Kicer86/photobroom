fba37dc49cefd1ba89d21fba1a3c02737e211071
Michał Walenciak <Kicer86@gmail.com>
2013-05-03 09:06:36 +0200

    tests added

---
+++ src/tests/RunAllTests.cpp
@@ -0,0 +1,10 @@
+
+
+#include <CppUTest/CommandLineTestRunner.h>
+
+IMPORT_TEST_GROUP(EntryDataTests);
+
+int main(int argc, char** argv)
+{
+    return RUN_ALL_TESTS(argc, argv);
+}
---
+++ src/tests/entrydata_tests.cpp
@@ -0,0 +1,13 @@
+
+#include <CppUTest/TestHarness.h>
+
+#include "configuration/entrydata.hpp"
+
+TEST_GROUP(EntryDataTests)
+{
+};
+
+TEST(EntryDataTests, Should)
+{
+
+}
