a7b0bb5be866d3e508df49fca883f0191247c7db
Michał Walenciak <Kicer86@gmail.com>
2013-05-03 22:36:54 +0200

    IFileAnalyzer: adding function for registering analyzers

--- src/analyzer/implementation/ifile_analyzer.hpp
+++ src/analyzer/implementation/ifile_analyzer.hpp
@@ -4,10 +4,14 @@
 
 #include <string>
 
+struct IAnalyzer;
+
 struct IFileAnalyzer
 {
 	virtual ~IFileAnalyzer () {}
+
 	virtual bool isImage(const std::string &) = 0;
+	virtual void registerAnalyzer(IAnalyzer *) = 0;
 };
 
 #endif
