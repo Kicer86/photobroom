8e86a73cde6e91daa3fef6abedbd39b170e5b730
Michał Walenciak <Kicer86@gmail.com>
2013-05-04 11:36:26 +0200

    Ianalyzer introduced

---
+++ src/analyzer/ianalyzer.hpp
@@ -0,0 +1,14 @@
+
+#ifndef ANALYZER_IANALYZER_HPP
+#define ANALYZER_IANALYZER_HPP
+
+#include <string>
+
+struct IAnalyzer
+{
+    virtual ~IAnalyzer() {}
+    
+    virtual bool isImage(const std::string &) = 0;
+};
+
+#endif
