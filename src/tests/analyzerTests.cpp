66925f4a14d2130158dbfe92bf0dcf767be7a05d
Michał Walenciak <Kicer86@gmail.com>
2013-05-04 18:39:56 +0200

    enabling tests

---
+++ src/tests/analyzerTests.cpp
@@ -0,0 +1,12 @@
+
+#include <CppUTest/TestHarness.h>
+
+
+TEST_GROUP(Analyzer)
+{
+};
+
+TEST(Analyzer, Should)
+{
+
+}
--- src/tests/entrydata_tests.cpp
+++ src/tests/entrydata_tests.cpp
@@ -1,7 +1,6 @@
 
 #include <CppUTest/TestHarness.h>
 
-#include "configuration/entrydata.hpp"
 
 TEST_GROUP(EntryDataTests)
 {
