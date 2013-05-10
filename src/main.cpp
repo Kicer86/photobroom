168eb64a527c90e2d4041bbaea8be4ea50c12d15
Michał Walenciak <Kicer86@gmail.com>
2013-05-10 18:27:11 +0200

    main: using database for storing files

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,24 @@
 
 #include "analyzer/photo_crawler_builder.hpp"
 #include "analyzer/iphoto_crawler.hpp"
+#include "database/idatabase.hpp"
+#include "database/databasefactory.hpp"
 
 int main(int argc, char **argv)
 {
-	std::shared_ptr<IPhotoCrawler> crawler = PhotoCrawlerBuilder().build();
+    std::shared_ptr<IPhotoCrawler> crawler = PhotoCrawlerBuilder().build();
 
-	std::string path = "/home/michal/projects/sources/broom/Asia i kot";
-	std::vector<std::string> paths;
+    std::string path = "/home/michal/projects/sources/broom/Asia i kot";
+    std::vector<std::string> paths;
 
-	paths.push_back(path);
+    paths.push_back(path);
 
-	crawler->crawl(paths);
+    std::vector<std::string> files = crawler->crawl(paths);
+
+    Database::IDatabase *database = Database::Factory().get();
+
+    for (auto f: files)
+        database->addFile(f, Database::Description());
 
     return 0;
 }
