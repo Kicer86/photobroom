e1d20bae6beead76176c6d6677b1b76ae83a8c45
Michał Walenciak <Kicer86@gmail.com>
2013-05-13 19:40:11 +0200

    comments

--- src/database/iconfiguration.hpp
+++ src/database/iconfiguration.hpp
@@ -26,7 +26,7 @@ namespace Database
 {
     struct IConfiguration
     {
-        virtual std::string getLocation() const = 0;
+        virtual std::string getLocation() const = 0;      //returns base dir for configuration. Like ~/.config/broom
     };
 }
 
