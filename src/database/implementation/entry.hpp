4cce63ece8ac46e0cbd1a11b60751fbaae2d2958
Michał Walenciak <Kicer86@gmail.com>
2013-05-14 18:31:22 +0200

    EntryWritter renamed to Entry

---
+++ src/database/implementation/entry.hpp
@@ -0,0 +1,42 @@
+/*
+    Database entries manipulator
+    Copyright (C) 2013  Michał Walenciak <MichalWalenciak@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#ifndef ENTRY_HPP
+#define ENTRY_HPP
+
+#include <memory>
+
+class FS;
+
+class Entry
+{
+    public:
+        Entry(const std::shared_ptr<FS> &);
+        virtual ~Entry();
+        
+    private:
+        Entry(const Entry&);
+        virtual Entry& operator=(const Entry&);
+        virtual bool operator==(const Entry&) const;
+        
+        std::shared_ptr<FS> m_fs;
+};
+
+#endif // ENTRY_HPP
