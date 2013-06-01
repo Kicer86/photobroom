2733fbfad0e26828bdd54e255fed74dbfa115d8d
Michał Walenciak <Kicer86@gmail.com>
2013-06-01 14:49:17 +0200

    QtGui introduced; main gui lib cleaned

--- src/gui/gui.hpp
+++ src/gui/gui.hpp
@@ -1,1 +1,26 @@
+
+/* GUI itnerface */
+
+#ifndef GUI_HPP
+#define GUI_HPP
+
+#include <memory>
+
+namespace Gui
+{
  
+    struct IUi
+    {
+        virtual ~IUi() {}
+        virtual void init (int argc, char **argv) = 0;
+        virtual int run() = 0;
+    };
+    
+    
+    struct Factory
+    {
+        static std::shared_ptr<IUi> get();
+    };
+}
+
+#endif
