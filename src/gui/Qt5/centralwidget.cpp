4be9830ddfc2c1951f38c5057baeaef1a415b275
Michał Walenciak <Kicer86@gmail.com>
2013-07-19 22:00:08 +0200

    CentralWidget: using tr() for texts; adding new tab - add photos

--- src/gui/Qt5/centralwidget.cpp
+++ src/gui/Qt5/centralwidget.cpp
@@ -2,8 +2,10 @@
 #include "centralwidget.hpp"
 
 #include "main_view_widget.hpp"
+#include "preparing_photos_widget.hpp"
 
 CentralWidget::CentralWidget(QWidget *parent): QTabWidget(parent)
 {
-    addTab(new MainViewWidget, "Photos");
+    addTab(new MainViewWidget, tr("Photos"));
+    addTab(new PreparingPhotosWidget, tr("Add photos"));
 }
