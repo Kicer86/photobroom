c80a18a67b3d2465972132cdeb190f2d44a47f5e
Michał Walenciak <Kicer86@gmail.com>
2013-07-19 21:52:38 +0200

    MainViewWidget introduced

---
+++ src/gui/Qt5/main_view_widget.cpp
@@ -0,0 +1,6 @@
+
+#include "main_view_widget.hpp"
+
+MainViewWidget::MainViewWidget(QWidget *parent): QWidget(parent)
+{
+}
---
+++ src/gui/Qt5/main_view_widget.hpp
@@ -0,0 +1,17 @@
+
+#ifndef MAIN_VIEW_WIDGET_HPP
+#define MAIN_VIEW_WIDGET_HPP
+
+#include <QWidget>
+
+class MainViewWidget : public QWidget
+{
+
+    public:
+        explicit MainViewWidget(QWidget *parent = 0);
+
+
+
+};
+
+#endif // MAIN_VIEW_WIDGET_HPP
