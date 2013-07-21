66bb42287593d75d8a3a27fe3ea47c166f8ac0fb
Michał Walenciak <Kicer86@gmail.com>
2013-07-21 10:22:55 +0200

    PreparingPhotosWidget: introducing BrowseLine

--- src/gui/Qt5/preparing_photos_widget.cpp
+++ src/gui/Qt5/preparing_photos_widget.cpp
@@ -2,14 +2,46 @@
 #include "preparing_photos_widget.hpp"
 
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QLabel>
 
 namespace
 {
+
+    struct BrowseLine: public QWidget
+    {
+
+        BrowseLine(QWidget *parent): QWidget(parent), m_button(nullptr), m_line(nullptr)
+        {
+            m_button = new QPushButton(tr("Browse"), this);
+            m_line = new QLineEdit(this);
+
+            QHBoxLayout *layout = new QHBoxLayout(this);
+
+            layout->addWidget(new QLabel(tr("Path to photos:"), this));
+            layout->addWidget(m_line);
+            layout->addWidget(m_button);
+        }
+
+        ~BrowseLine() {}
+
+        QFileDialog *m_dialog;
+        QPushButton *m_button;
+        QLineEdit *m_line;
+
+    };
+
 
     struct BrowseList: public QWidget
     {
         BrowseList(QWidget *parent): QWidget(parent)
         {
+            QVBoxLayout *layout = new QVBoxLayout(this);
+
+            layout->addWidget(new BrowseLine(this));
         }
 
         ~BrowseList()
@@ -27,6 +59,7 @@ struct PreparingPhotosWidget::GuiData
         QVBoxLayout *layout = new QVBoxLayout(preparer);
 
         layout->addWidget(new BrowseList(preparer));
+        layout->addWidget(new QWidget(preparer));
     }
 
     ~GuiData() {}
