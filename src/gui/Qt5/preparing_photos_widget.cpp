bb7fc52555270b05cb1ed6c378a11a5d41686c97
Michał Walenciak <Kicer86@gmail.com>
2013-07-21 09:31:21 +0200

    PreparePhotosWidget: introducing private data

--- src/gui/Qt5/preparing_photos_widget.cpp
+++ src/gui/Qt5/preparing_photos_widget.cpp
@@ -1,7 +1,37 @@
 
 #include "preparing_photos_widget.hpp"
 
-PreparingPhotosWidget::PreparingPhotosWidget(QWidget *parent): QWidget(parent)
+#include <QVBoxLayout>
+
+
+struct BrowseList: public QWidget
+{
+    BrowseList(QWidget *parent): QWidget(parent)
+    {
+    }
+
+    ~BrowseList()
+    {
+    }
+};
+
+
+struct PreparingPhotosWidget::GuiData
+{
+    GuiData(QWidget *preparer): m_preparer(preparer)
+    {
+        QVBoxLayout *layout = new QVBoxLayout(preparer);
+
+        layout->addWidget(new BrowseList(preparer));
+    }
+
+    ~GuiData() {}
+
+    QWidget *m_preparer;
+};
+
+
+PreparingPhotosWidget::PreparingPhotosWidget(QWidget *parent): QWidget(parent), m_gui(new GuiData(this))
 {
 
 }
--- src/gui/Qt5/preparing_photos_widget.hpp
+++ src/gui/Qt5/preparing_photos_widget.hpp
@@ -2,6 +2,8 @@
 #ifndef PREPARING_PHOTOS_WIDGET_H
 #define PREPARING_PHOTOS_WIDGET_H
 
+#include <memory>
+
 #include <QWidget>
 
 class PreparingPhotosWidget: public QWidget
@@ -10,6 +12,10 @@ class PreparingPhotosWidget: public QWidget
     public:
         explicit PreparingPhotosWidget(QWidget *parent = 0);
         virtual ~PreparingPhotosWidget();
+
+    private:
+        struct GuiData;
+        std::unique_ptr<GuiData> m_gui;
 
 };
 
