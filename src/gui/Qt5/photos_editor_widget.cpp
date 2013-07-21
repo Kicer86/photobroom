4ee01c57373df3187d84fe2fd70b33b0b0ac08a7
Michał Walenciak <Kicer86@gmail.com>
2013-07-21 20:27:34 +0200

    gui: PhotosEditorWidget: preparing private gui data

--- src/gui/Qt5/photos_editor_widget.cpp
+++ src/gui/Qt5/photos_editor_widget.cpp
@@ -1,12 +1,25 @@
 
 #include "photos_editor_widget.hpp"
 
-PhotosEditorWidget::PhotosEditorWidget(QWidget *parent): QWidget(parent)
+
+struct PhotosEditorWidget::GuiData
+{
+    GuiData() {}
+    ~GuiData() {}
+};
+
+PhotosEditorWidget::PhotosEditorWidget(QWidget *parent): QWidget(parent), m_gui(new GuiData)
 {
 }
 
 
 PhotosEditorWidget::~PhotosEditorWidget()
+{
+
+}
+
+
+void PhotosEditorWidget::addPhoto(const std::string &photo)
 {
 
 }
--- src/gui/Qt5/photos_editor_widget.hpp
+++ src/gui/Qt5/photos_editor_widget.hpp
@@ -2,6 +2,8 @@
 #ifndef PHOTOS_EDITOR_WIDGET_HPP
 #define PHOTOS_EDITOR_WIDGET_HPP
 
+#include <memory>
+
 #include <QWidget>
 
 class PhotosEditorWidget : public QWidget
@@ -9,6 +11,21 @@ class PhotosEditorWidget : public QWidget
     public:
         explicit PhotosEditorWidget(QWidget *parent = 0);
         virtual ~PhotosEditorWidget();
+
+        template<class T>
+        void addPhotos(const T &collection)
+        {
+            for(auto &photo: collection)
+            {
+                addPhoto(photo);
+            }
+        }
+
+        void addPhoto(const std::string &);
+
+    private:
+        struct GuiData;
+        std::unique_ptr<GuiData> m_gui;
 };
 
 #endif // PHOTOS_EDITOR_WIDGET_HPP
