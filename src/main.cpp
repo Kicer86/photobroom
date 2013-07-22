01c02b2de23fa755e46c2117336e2ce411ffbed9
Michał Walenciak <Kicer86@gmail.com>
2013-07-22 15:10:08 +0200

    main: cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,10 @@
 #include <string>
 #include <memory>
 
-#include "analyzer/photo_crawler_builder.hpp"
-#include "analyzer/iphoto_crawler.hpp"
-#include "database/idatabase.hpp"
-#include "database/databasebuilder.hpp"
 #include "gui/gui.hpp"
 
 int main(int argc, char **argv)
 {
-    std::shared_ptr<IPhotoCrawler> crawler = PhotoCrawlerBuilder().build();
-
-    std::string path = "Asia i kot";
-    std::vector<std::string> paths;
-
-    paths.push_back(path);
-
-    std::vector<std::string> files = crawler->crawl(paths);
-    
-    Database::IFrontend *database = Database::Builder().get();
-
-    for (auto f: files)
-        database->addFile(f, Database::IFrontend::Description());
-
     std::shared_ptr<Gui::IUi> gui = Gui::Factory::get();
 
     gui->run(argc, argv);
