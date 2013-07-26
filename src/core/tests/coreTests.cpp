c9d64b54a005658d48bcf6db7d1c5b8077d6e5b0
Michał Walenciak <Kicer86@gmail.com>
2013-07-26 20:44:16 +0200

    core: adding test for overwrite

--- src/core/tests/coreTests.cpp
+++ src/core/tests/coreTests.cpp
@@ -39,3 +39,17 @@ TEST(TagDataShould, returnWhatWasInserted)
     CHECK_EQUAL("test2", dataSet[0].values);
 }
 
+
+TEST(TagDataShould, overwritePreviousTagsValues)
+{
+    TagData data;
+    
+    data.setTag("test1", "test2");
+    data.setTag("test1", "test3");
+    
+    std::vector<TagData::TagInfo> dataSet = data.getTags();
+    
+    CHECK_EQUAL(1, static_cast<int>(dataSet.size()));
+    CHECK_EQUAL("test1", dataSet[0].name);
+    CHECK_EQUAL("test3", dataSet[0].values);
+}
