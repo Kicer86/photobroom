8b2829e14ddca700100496e876f8121a353cba43
Michał Walenciak <Kicer86@gmail.com>
2013-09-24 21:33:59 +0200

    core tests: adaptation to changes

--- src/core/tests/coreTests.cpp
+++ src/core/tests/coreTests.cpp
@@ -19,7 +19,7 @@ TEST(TagDataShould, returnEmptySetWhenConstructed)
 {
     TagData data;
     
-    std::vector<TagData::TagInfo> dataSet = data.getTags();
+    const ITagData::TagsList &dataSet = data.getTags();
     
     CHECK_EQUAL(0, static_cast<int>(dataSet.size()));
 }
@@ -31,12 +31,12 @@ TEST(TagDataShould, returnWhatWasInserted)
     
     data.setTag("test1", "test2");
     
-    std::vector<TagData::TagInfo> dataSet = data.getTags();
+    const ITagData::TagsList &dataSet = data.getTags();
     
     CHECK_EQUAL(1, static_cast<int>(dataSet.size()));
     
-    CHECK_EQUAL(true, dataSet[0].name == "test1");
-    CHECK_EQUAL(true, dataSet[0].values == "test2");
+    CHECK_EQUAL(true, ITagData::TagInfo(dataSet.begin()).name() == "test1");
+    CHECK_EQUAL(true, ITagData::TagInfo(dataSet.begin()).values() == "test2");
 }
 
 
@@ -47,11 +47,11 @@ TEST(TagDataShould, overwritePreviousTagsValues)
     data.setTag("test1", "test2");
     data.setTag("test1", "test3");
     
-    std::vector<TagData::TagInfo> dataSet = data.getTags();
+    const ITagData::TagsList& dataSet = data.getTags();
     
     CHECK_EQUAL(1, static_cast<int>(dataSet.size()));
-    CHECK_EQUAL(true, dataSet[0].name == "test1");
-    CHECK_EQUAL(true, dataSet[0].values == "test3");
+    CHECK_EQUAL(true, ITagData::TagInfo(dataSet.begin()).name() == "test1");
+    CHECK_EQUAL(true, ITagData::TagInfo(dataSet.begin()).values() == "test3");
 }
 
 
@@ -75,14 +75,34 @@ TEST(TagDataCompositeShould, fillItsMinions)
     
     auto m1 = minion1.getTags();
     CHECK_EQUAL(1, m1.size());
-    CHECK_EQUAL(true, m1[0].name == "name");
-    CHECK_EQUAL(true, m1[0].values == "value");
+    CHECK_EQUAL(true, ITagData::TagInfo(m1.begin()).name() == "name");
+    CHECK_EQUAL(true, ITagData::TagInfo(m1.begin()).values() == "value");
     
+    auto m2 = minion2.getTags();
     CHECK_EQUAL(1, m1.size());
-    CHECK_EQUAL(true, m1[0].name == "name");
-    CHECK_EQUAL(true, m1[0].values == "value");
+    CHECK_EQUAL(true, ITagData::TagInfo(m2.begin()).name() == "name");
+    CHECK_EQUAL(true, ITagData::TagInfo(m2.begin()).values() == "value");
     
+    auto m3 = minion3.getTags();
     CHECK_EQUAL(1, m1.size());
-    CHECK_EQUAL(true, m1[0].name == "name");
-    CHECK_EQUAL(true, m1[0].values == "value");
+    CHECK_EQUAL(true, ITagData::TagInfo(m3.begin()).name() == "name");
+    CHECK_EQUAL(true, ITagData::TagInfo(m3.begin()).values() == "value");
+}
+
+
+TEST(TagDataCompositeShould, getTagsFromMinionsWhenAllDataAreTheSame)
+{
+    TagDataComposite data;
+
+    TagData minion1, minion2, minion3;
+    
+    data.setTagDatas( {&minion1, &minion2, &minion3} );
+    
+    minion1.setTag("name", "value");
+    minion2.setTag("name", "value");
+    minion3.setTag("name", "value");
+    
+    auto info = data.getTags();
+    
+    CHECK_EQUAL(2, info.size());
 }
