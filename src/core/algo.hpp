f3e74ed759a4b0651f440261745d4cd44f75913e
Michał Walenciak <Kicer86@gmail.com>
2013-09-30 20:50:46 +0200

    Revert "core: algo: introducing itnersectors"
    This reverts commit 1b34906d822cfbad017b19668dc79d8544f2a338.

--- src/core/algo.hpp
+++ src/core/algo.hpp
@@ -6,43 +6,19 @@
 #include <map>
 
 namespace Algo
-{
-    struct KeyIntersector
-    {
-        KeyIntersector() {}
-        ~KeyIntersector() {}
-
-        template<typename T>
-        bool equal(const T& v1, const T& v2) const
-        {
-            return v1->first == v2->first;
-        }
-    };
-
-    struct KeyValueIntersector
-    {
-        KeyValueIntersector() {}
-        ~KeyValueIntersector() {}
-
-        template<typename T>
-        bool equal(const T& v1, const T& v2) const
-        {
-            return v1->first == v2->first && v1->second == v2->second;
-        }
-    };
+{     
         
-    template<typename KeyType, typename ValType, typename Intersector = KeyIntersector>
+    template<typename KeyType, typename ValType>
     std::map<KeyType, ValType> map_intersection(const std::map<KeyType, ValType> &m1, const std::map<KeyType, ValType> &m2)
     {
         std::map<KeyType, ValType> results;
-        Intersector intersector;
         
         auto it1 = m1.begin();
         auto it2 = m2.begin();
         
         while (it1 != m1.end() && it2 != m2.end())
         {
-            if (intersector.equal(it1, it2))
+            if (*it1 == *it2)
             {
                 results.insert(*it1);
                 
