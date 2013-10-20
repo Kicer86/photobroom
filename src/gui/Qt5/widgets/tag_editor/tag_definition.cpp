6afabc41b8929cca89d1203728b96725f85c107e
Michał Walenciak <Kicer86@gmail.com>
2013-10-20 17:55:50 +0200

    gui: TagDefinition introduced

--- src/gui/Qt5/widgets/tag_editor/tag_definition.cpp
+++ src/gui/Qt5/widgets/tag_editor/tag_definition.cpp
@@ -1,1 +1,22 @@
- 
+
+#include "tag_definition.hpp"
+
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QComboBox>
+
+TagDefinition::TagDefinition(QWidget* parent, Qt::WindowFlags f): QWidget(parent, f)
+{    
+    m_button = new QPushButton("+", this);
+    m_comboBox = new QComboBox(this);
+    QHBoxLayout* layout = new QHBoxLayout(this);
+    
+    layout->addWidget(m_comboBox);
+    layout->addWidget(m_button);
+}
+
+
+TagDefinition::~TagDefinition()
+{
+
+}
--- src/gui/Qt5/widgets/tag_editor/tag_definition.hpp
+++ src/gui/Qt5/widgets/tag_editor/tag_definition.hpp
@@ -1,1 +1,21 @@
- 
+
+#ifndef TAG_DEFINITION_HPP 
+#define TAG_DEFINITION_HPP 
+
+#include <QWidget>
+
+class QPushButton;
+class QComboBox;
+
+class TagDefinition: public QWidget
+{
+    public:
+        explicit TagDefinition(QWidget* parent = 0, Qt::WindowFlags f = 0);
+        virtual ~TagDefinition();
+        
+    private:
+        QComboBox* m_comboBox;
+        QPushButton* m_button;
+};
+
+#endif
