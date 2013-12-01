df8fbde08bce86af66fcaf72735b24209657ec8b
Michał Walenciak <Kicer86@gmail.com>
2013-12-01 11:29:52 +0100

    FindGMock: adding GMock's base dir to includes

--- cmake/FindGMock.cmake
+++ cmake/FindGMock.cmake
@@ -10,9 +10,12 @@
 
 find_path(GMOCK_INCLUDE_DIR gmock/gmock.h
           HINTS ${GMOCK_DIR}/include)
+          
+find_path(GMOCK_INTERNALS src/gmock.cc
+          HINTS ${GMOCK_INCLUDE_DIR}/.. ${GMOCK_DIR})
 
 find_file(GMOCK_SOURCE  src/gmock.cc
-          HINTS ${GMOCK_INCLUDE_DIR}/.. ${GMOCK_DIR})
+          HINTS ${GMOCK_INTERNALS})
 
 set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR} )
 set(GMOCK_SOURCES      ${GMOCK_SOURCE} )
@@ -21,6 +24,6 @@ include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GMOCK_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(GTest  DEFAULT_MSG
-                                  GMOCK_SOURCE GMOCK_INCLUDE_DIR)
+                                  GMOCK_SOURCE GMOCK_INCLUDE_DIR GMOCK_INTERNALS)
 
-mark_as_advanced(GMOCK_SOURCE GMOCK_INCLUDE_DIR)
+mark_as_advanced(GMOCK_SOURCE GMOCK_INCLUDE_DIR GMOCK_INTERNALS)
