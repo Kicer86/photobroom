adcff9f4a5623eff6a9b372c89f122756b0aa9eb
Michał Walenciak <Kicer86@gmail.com>
2013-12-06 09:23:37 +0100

    cmake: adopting gtest finders to work with static libraries

--- cmake/FindGMock.cmake
+++ cmake/FindGMock.cmake
@@ -3,30 +3,23 @@
 
 # - Try to find GMock
 # Once done this will define
-#  GMOCK_FOUND - System has GMock
+#  GMOCK_FOUND        - System has GMock
 #  GMOCK_INCLUDE_DIRS - The GMock include directories
-#  GMOCK_SOURCES - Extra sources to be added to build
-
+#  GMOCK_LIBRARY      - GMock library
+#  GMOCK_MAIN_LIBRARY - GMock main library
 
 find_path(GMOCK_INCLUDE_DIR gmock/gmock.h
           HINTS ${GMOCK_DIR}/include)
           
-find_path(GMOCK_INTERNALS src/gmock-all.cc
-          HINTS ${GMOCK_INCLUDE_DIR}/.. 
-                ${GMOCK_DIR}
-                /usr/src/gmock                 #debian
-         )
+find_library(GMOCK_LIBRARY      libgmock.a)
+find_library(GMOCK_MAIN_LIBRARY libgmock_main.a)
 
-find_file(GMOCK_SOURCE  src/gmock-all.cc
-          HINTS ${GMOCK_INTERNALS})
-
-set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR} ${GMOCK_INTERNALS})
-set(GMOCK_SOURCES      ${GMOCK_SOURCE} )
+set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GMOCK_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(GTest  DEFAULT_MSG
-                                  GMOCK_SOURCE GMOCK_INCLUDE_DIR GMOCK_INTERNALS)
+find_package_handle_standard_args(GTest DEFAULT_MSG
+                                  GMOCK_INCLUDE_DIR)
 
-mark_as_advanced(GMOCK_SOURCE GMOCK_INCLUDE_DIR GMOCK_INTERNALS)
+mark_as_advanced(GMOCK_INCLUDE_DIR)
--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -1,33 +1,25 @@
 
+
+
 # - Try to find GTest
 # Once done this will define
 #  GTEST_FOUND - System has GTest
 #  GTEST_INCLUDE_DIRS - The GTest include directories
-#  GTEST_SOURCES - Extra sources to be added to build
-#  GTEST_LIBRARIES - GTest libraries
-
-find_package(Threads)
+#  GTEST_LIBRARY - GTest library
+#  GTEST_MAIN_LIBRARY - GTest main library
 
 find_path(GTEST_INCLUDE_DIR gtest/gtest.h
           HINTS ${GTEST_DIR}/include)
-          
-find_path(GTEST_INTERNALS src/gtest-all.cc
-          HINTS ${GTEST_INCLUDE_DIR}/.. 
-                ${GTEST_DIR}
-                /usr/src/gtest                      #debian
-         )
+                    
+find_library(GTEST_LIBRARY      libgtest.a)
+find_library(GTEST_MAIN_LIBRARY libgtest_main.a)
 
-find_file(GTEST_SOURCE  src/gtest-all.cc
-          HINTS ${GTEST_INTERNALS})
-          
-set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR} ${GTEST_INTERNALS})
-set(GTEST_SOURCES      ${GTEST_SOURCE} )
-set(GTEST_LIBRARIES    pthread)
+set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GTEST_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(GTest  DEFAULT_MSG
-                                  GTEST_SOURCE GTEST_INCLUDE_DIR GTEST_INTERNALS)
+find_package_handle_standard_args(GTest DEFAULT_MSG
+                                  GTEST_INCLUDE_DIR )
 
-mark_as_advanced(GTEST_SOURCE GTEST_INCLUDE_DIR GTEST_INTERNALS)
+mark_as_advanced(GTEST_INCLUDE_DIR)
