5210da6e508cf7b65c9f4972f4289a2daf24d007
Michał Walenciak <Kicer86@gmail.com>
2013-12-10 21:50:38 +0100

    ConfigurationFactoryTest: Making sure that there is only one configuration

--- src/configuration/tests/configurationfactory_tests.cpp
+++ src/configuration/tests/configurationfactory_tests.cpp
@@ -9,3 +9,13 @@ TEST(ConfigurationFactoryShould, returnConfiguration)
     
     ASSERT_NE(nullptr, config.get());
 }
+
+
+TEST(ConfigurationFactoryShould, alwaysReturnTheSameConfiguration)
+{
+    std::shared_ptr<IConfiguration> config1 = ConfigurationFactory::get();
+    std::shared_ptr<IConfiguration> config2 = ConfigurationFactory::get();
+    
+    ASSERT_EQ(config1.get(), config2.get());
+}
+
