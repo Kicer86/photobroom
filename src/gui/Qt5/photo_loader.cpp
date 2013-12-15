c5f554ad871e96bc42a80340f34ac856210a4132
Michał Walenciak <Kicer86@gmail.com>
2013-12-15 22:13:02 +0100

    PhotoLoader: introducing ThreadSafeResource; eating tasks in TaskEater
    ThreadSafeResource is meant to be a class wrapping any object with accessor which allows only one thread at time to access it

--- src/gui/Qt5/photo_loader.cpp
+++ src/gui/Qt5/photo_loader.cpp
@@ -2,51 +2,148 @@
 #include "photo_loader.hpp"
 
 #include <thread>
+#include <unordered_map>
+
+#include <QPixmap>
 
 #include <OpenLibrary/palgorithm/ts_queue.hpp>
 
 static void trampoline(PhotoLoader::Data *);
+
+namespace
+{
+    //TODO: remove, use config
+    const int photoWidth = 120;
+
+    struct hash
+    {
+        std::size_t operator()(const QString& str) const
+        {
+            std::hash<std::string> hash_fn;
+            const std::size_t h = hash_fn(str.toStdString());
+            return h;
+        }
+    };
+}
+
+template<typename T>
+class ThreadSafeResource
+{
+        struct Deleter
+        {
+            Deleter(std::unique_lock<std::mutex>* lock): m_lock(lock) {}
+            virtual ~Deleter()
+            {
+                assert(m_lock == nullptr);
+            }
+
+            void operator() (T* resource)
+            {
+                //do not delete resource - delete lock instead, it will lock resource inside of ThreadSafeResource again
+
+                delete m_lock, m_lock = nullptr;     //mutex will be free again
+            }
+
+            std::unique_lock<std::mutex>* m_lock;
+        };
+
+    public:
+        friend struct Deleter;
+
+        template<typename... Args>
+        ThreadSafeResource(const Args&... args): m_resource(args...)
+        {
+        }
+
+        std::unique_ptr<T, Deleter> get()
+        {
+            std::unique_lock<std::mutex>* lock = new std::unique_lock<std::mutex>(m_mutex);    //lock on mutex
+            std::unique_ptr<T, Deleter> result(&m_resource, Deleter(lock));
+
+            return result;             //return resource, but don't release mutex - it will be released when Deleter is called
+        }
+
+    private:
+        std::mutex m_mutex;
+        T m_resource;
+};
 
 
 struct PhotoLoader::Data
 {
     struct Task
     {
+        Task(const QString& p, IPhotoLoader::INotifier* n): path(p), notifier(n) {}
+        Task(const Task &) = default;
+        Task(): path(), notifier(nullptr) {}
+
+        Task& operator=(const Task &) = default;
+
         QString path;
         IPhotoLoader::INotifier *notifier;
     };
 
+    struct Results
+    {
+        QString path;
+        QPixmap thumbnail;
+    };
 
     //TODO: configurable?
-    Data(): m_tasks(2048), m_taskEater(trampoline, this)
+    Data(): m_tasks(2048), m_results(2048), m_taskEater(trampoline, this)
     {
 
     }
 
+    ~Data()
+    {
+        m_tasks.break_popping();
+        assert(m_taskEater.joinable());
+        m_taskEater.join();
+    }
+
     void addTask(const QString& path, IPhotoLoader::INotifier* notifier)
     {
+        Task task(path, notifier);
+
+        m_tasks.push_back(task);
     }
 
     private:
         friend void trampoline(PhotoLoader::Data *data);
         TS_Queue<Task> m_tasks;
+        ThreadSafeResource<std::unordered_map<QString, QPixmap, hash>> m_results;
         std::thread m_taskEater;
 
         void taskEater()
         {
             while(true)
             {
+                boost::optional<Task> task = m_tasks.pop_front();
+
+                if (task)
+                    eatTask(task.get());
+                else
+                    break;
             }
         }
 
+        void eatTask(const Task& task)
+        {
+            QPixmap pixmap(task.path);
+            QPixmap thumbnail = pixmap.scaled(photoWidth, photoWidth, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+        }
 };
+
 
 static void trampoline(PhotoLoader::Data *data)
 {
     data->taskEater();
 }
 
-PhotoLoader::PhotoLoader()
+
+PhotoLoader::PhotoLoader():m_data(new Data)
 {
 
 }
@@ -59,7 +156,7 @@ PhotoLoader::~PhotoLoader()
 
 void PhotoLoader::generateThumbnail(const QString& path, IPhotoLoader::INotifier *notifier)
 {
-
+    m_data->addTask(path, notifier);
 }
 
 
