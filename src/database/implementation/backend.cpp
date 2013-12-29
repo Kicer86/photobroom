bb6e4ead6304f71d1e72f59bf7e1c3b7df0adcfc
Michał Walenciak <Kicer86@gmail.com>
2013-12-29 11:34:47 +0100

    Database: Backend: creating database directory if doesn't exist

--- src/database/implementation/backend.cpp
+++ src/database/implementation/backend.cpp
@@ -1,13 +1,36 @@
 
 #include "backend.hpp"
 
+#include <stdexcept>
+
+#include <boost/filesystem.hpp>
+
+#include "configuration/configurationfactory.hpp"
+#include "configuration/iconfiguration.hpp"
+#include "configuration/entrydata.hpp"
+
 #include "entry.hpp"
+#include "databasebuilder.hpp"
+
 
 namespace Database
 {
 
     DefaultBackend::DefaultBackend()
     {
+        auto entry = ConfigurationFactory::get()->findEntry(Database::databaseLocation);
+
+        if (entry)
+        {
+            boost::filesystem::path storage(entry->value());
+
+            if (boost::filesystem::exists(storage) == false)
+            {
+                const bool status = boost::filesystem::create_directories(storage);
+                if (status == false)
+                    throw std::runtime_error("Could not create database directory: " + storage.string());
+            }
+        }
     }
 
 
