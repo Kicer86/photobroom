46f60bf044ccfe69a631fd4a6f0cdd689b68c3ed
Michał Walenciak <Kicer86@gmail.com>
2013-12-29 22:59:48 +0100

    Database tests: code format

--- src/database/tests/databaseTests.cpp
+++ src/database/tests/databaseTests.cpp
@@ -16,51 +16,51 @@ namespace
     {
         FSImpl(): m_stream(new std::stringstream) {}
 
-		//open and return file
-        std::shared_ptr<std::iostream> openStream(const std::string &, std::ios_base::openmode) override
-		{
+        //open and return file
+        std::shared_ptr<std::iostream> openStream(const std::string&, std::ios_base::openmode) override
+        {
             return m_stream;
-		}
+        }
 
         std::shared_ptr<std::stringstream> m_stream;
-	};
+    };
 
-	struct Backend: public Database::IBackend
-	{
+    struct Backend: public Database::IBackend
+    {
         Backend(): m_entries() {}
-		virtual ~Backend() {}
+        virtual ~Backend() {}
 
-		virtual bool store(const Database::Entry &entry)
-		{
-			m_entries.push_back(entry);
+        virtual bool store(const Database::Entry& entry)
+        {
+            m_entries.push_back(entry);
 
-			return true;
-		}
+            return true;
+        }
 
-		virtual bool init()
+        virtual bool init()
         {
             return true;
         }
 
-		std::vector<Database::Entry> m_entries;
-	};
+        std::vector<Database::Entry> m_entries;
+    };
 }
 
 
 TEST(MemoryDatabaseShould, AcceptAFileAndSendItToBackendAsSoonAsBackendIsSet)
 {
-	std::shared_ptr<FSImpl> fs = std::make_shared<FSImpl>();
+    std::shared_ptr<FSImpl> fs = std::make_shared<FSImpl>();
 
     *(fs->m_stream) << "Test content of file to store";
 
-	Database::MemoryDatabase *db = new Database::MemoryDatabase(fs);
-	db->addPhoto(APhotoInfo::Ptr());
+    Database::MemoryDatabase* db = new Database::MemoryDatabase(fs);
+    db->addPhoto(APhotoInfo::Ptr());
 
-	std::shared_ptr<Backend> backend = std::make_shared<Backend>();
-	db->setBackend(backend);
+    std::shared_ptr<Backend> backend = std::make_shared<Backend>();
+    db->setBackend(backend);
 
     delete db;             //"flush" data ;)
 
     const int s = backend->m_entries.size();
-	ASSERT_EQ(1, s);
+    ASSERT_EQ(1, s);
 }
