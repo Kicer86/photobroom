28f929b38a2b0c4144dd0ffa28173b91b253e977
Michał Walenciak <Kicer86@gmail.com>
2014-01-11 20:55:02 +0100

    configuration: introducing constaints file

---
+++ src/configuration/constraints.hpp
@@ -0,0 +1,11 @@
+
+#ifndef CONFIGURATION_CONSTRAINTS
+#define CONFIGURATION_CONSTRAINTS
+
+namespace Constraints
+{
+    extern const unsigned int database_tag_name_len;
+    extern const unsigned int database_tag_value_len;
+}
+
+#endif
---
+++ src/configuration/private/constraints.cpp
@@ -0,0 +1,8 @@
+
+#include "constraints.hpp"
+
+namespace Constraints
+{
+    const unsigned int database_tag_name_len = 256;
+    const unsigned int database_tag_value_len = 2048;
+}
