69cd1ab65ee94441efc2281450fecc334a41d784
Michał Walenciak <Kicer86@gmail.com>
2014-01-25 22:41:42 +0100

    MainViewWidget: view/model members added

--- src/gui/Qt5/main_view_widget.cpp
+++ src/gui/Qt5/main_view_widget.cpp
@@ -1,6 +1,18 @@
 
 #include "main_view_widget.hpp"
 
-MainViewWidget::MainViewWidget(QWidget *p): QWidget(p)
+#include "model_view/images_view.hpp"
+#include "model_view/images_model.hpp"
+
+MainViewWidget::MainViewWidget(QWidget *p): QWidget(p), m_imagesModel(nullptr), m_imagesView(nullptr)
 {
+    m_imagesModel = new ImagesModel(this);
+    m_imagesView = new ImagesView(this);
 }
+
+
+MainViewWidget::~MainViewWidget()
+{
+
+}
+
--- src/gui/Qt5/main_view_widget.hpp
+++ src/gui/Qt5/main_view_widget.hpp
@@ -4,11 +4,18 @@
 
 #include <QWidget>
 
+class ImagesView;
+class ImagesModel;
+
 class MainViewWidget : public QWidget
 {
     public:
         explicit MainViewWidget(QWidget *parent = 0);
+        virtual ~MainViewWidget();
 
+    private:
+        ImagesModel* m_imagesModel;
+        ImagesView*  m_imagesView;
 };
 
 #endif // MAIN_VIEW_WIDGET_HPP
