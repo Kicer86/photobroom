2390fd78b3a11ca6a35cf6d0800d91fc337f9162
Michał Walenciak <Kicer86@gmail.com>
2014-02-23 20:59:37 +0100

    gui: MainViewWidget: adding layout

--- src/gui/Qt5/widgets/browse_view/main_view_widget.cpp
+++ src/gui/Qt5/widgets/browse_view/main_view_widget.cpp
@@ -1,6 +1,8 @@
 
 #include "main_view_widget.hpp"
 
+#include <QVBoxLayout>
+
 #include "model_view/images_view.hpp"
 #include "model_view/images_model.hpp"
 
@@ -8,6 +10,9 @@ MainViewWidget::MainViewWidget(QWidget *p): QWidget(p), m_imagesModel(nullptr), 
 {
     m_imagesModel = new ImagesModel(this);
     m_imagesView = new ImagesView(this);
+
+    QVBoxLayout* main_layout = new QVBoxLayout(this);
+    main_layout->addWidget(m_imagesView);
 }
 
 
