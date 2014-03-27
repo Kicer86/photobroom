fd45d37865c271005578cbf93ce758d2311540e0
Michał Walenciak <Kicer86@gmail.com>
2014-03-27 07:24:33 +0100

    database: moving PhotoIterator tests to another file

---
+++ src/database/tests/photoIteratorTests.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "photo_iterator.hpp"
+
+
+namespace
+{
+    struct MockQuery: Database::IQuery
+    {
+        MOCK_METHOD0(gotoNext, bool());
+        MOCK_METHOD1(getField, QVariant(const QString &));
+        MOCK_CONST_METHOD0(valid, bool());
+        MOCK_METHOD0(clone, std::shared_ptr<IQuery>());
+    };
+}
+
+
+TEST(PhotoIteratorShould, Work)
+{
+
+}
