49573297f580f3432611b668d2f85e791f25999d
Michał Walenciak <Kicer86@gmail.com>
2014-04-24 22:14:38 +0200

    core: introducing TimeGuardian

---
+++ src/core/implementation/time_guardian.cpp
@@ -0,0 +1,67 @@
+/*
+ * Time measure utility
+ * Copyright (C) 2014  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "time_guardian.hpp"
+
+#include <chrono>
+#include <iostream>
+
+struct TimeGuardian::Data
+{
+    Data(): m_limit(0), m_limitMessage(), m_title(), m_start() {}
+
+    unsigned int m_limit;
+    std::string m_limitMessage;
+    std::string m_title;
+
+    std::chrono::time_point<std::chrono::steady_clock> m_start;
+};
+
+
+TimeGuardian::TimeGuardian(): m_data(new Data)
+{
+    m_data->m_start = std::chrono::steady_clock::now();
+}
+
+
+TimeGuardian::~TimeGuardian()
+{
+    const auto end = std::chrono::steady_clock::now();
+    const auto diff = end - m_data->m_start;
+    const auto diff_ms = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
+
+    if (diff_ms > m_data->m_limit)
+        std::cerr << m_data->m_title << ": " << diff_ms << "ms. " << m_data->m_limitMessage << std::endl;
+    else
+        std::cout << m_data->m_title << ": " << diff_ms << "ms." << std::endl;
+}
+
+
+void TimeGuardian::setLimit(unsigned int ms, const std::string& message)
+{
+    m_data->m_limit = ms;
+    m_data->m_limitMessage = message;
+}
+
+
+void TimeGuardian::setTitle(const std::string& title)
+{
+    m_data->m_title = title;
+}
+
---
+++ src/core/time_guardian.hpp
@@ -0,0 +1,48 @@
+/*
+ * Time measure utility
+ * Copyright (C) 2014  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TIMEGUARDIAN_H
+#define TIMEGUARDIAN_H
+
+#include <memory>
+
+#include "core_export.h"
+
+class string;
+
+#define TIME_GUARDIAN(t,l,m) TimeGuardian _guardian; _guardian.setLimit(l,m); _guardian.setTitle(t)
+
+class CORE_EXPORT TimeGuardian
+{
+    public:
+        TimeGuardian();
+        TimeGuardian(const TimeGuardian& other) = delete;
+        ~TimeGuardian();
+
+        void setLimit(unsigned int ms, const std::string& message);
+        void setTitle(const std::string &);
+
+        TimeGuardian& operator=(const TimeGuardian& other) = delete;
+
+    private:
+        struct Data;
+        std::unique_ptr<Data> m_data;
+};
+
+#endif // TIMEGUARDIAN_H
