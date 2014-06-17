fb13b169f99a98eee9131983fafc21d597b1b56f
Michał Walenciak <Kicer86@gmail.com>
2014-06-17 21:44:57 +0200

    useful link added

--- cmake/win32.cmake
+++ cmake/win32.cmake
@@ -1,4 +1,4 @@
 
-#CMake script preparing build environemtn for windows
-
+# CMake script preparing build environemtn for windows
+# http://stackoverflow.com/questions/17446981/cmake-externalproject-add-and-findpackage
 
