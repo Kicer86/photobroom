baf59c88103cbb74fb7c665f431b663db92a04eb
Michał Walenciak <Kicer86@gmail.com>
2014-06-17 21:45:40 +0200

    initial implementation of easy_exif_tag_feeder

--- src/core/implementation/easy_exif_tag_feeder.cpp
+++ src/core/implementation/easy_exif_tag_feeder.cpp
@@ -1,1 +1,39 @@
- 
+
+#include "easy_exif_tag_feeder.hpp"
+
+#include <assert.h>
+
+#include <QStringList>
+#include <easyexif/exif.h>
+
+#include <configuration/constants.hpp>
+
+#include "base_tags.hpp"
+
+
+EasyExifTagFeeder::EasyExifTagFeeder()
+{
+
+}
+
+
+std::unique_ptr<ITagData> EasyExifTagFeeder::getTagsFor(const std::string &path)
+{
+	std::unique_ptr<ITagData> tagData(new TagData);
+	feed(path, tagData.get());
+
+	return tagData;
+}
+
+
+void EasyExifTagFeeder::update(ITagData *, const std::string &)
+{
+
+}
+
+
+void EasyExifTagFeeder::feed(const std::string& path, ITagData* tagData)
+{
+	
+}
+
--- src/core/implementation/easy_exif_tag_feeder.hpp
+++ src/core/implementation/easy_exif_tag_feeder.hpp
@@ -1,1 +1,26 @@
- 
+
+#ifndef EASY_EXIF_TAG_FEEDER_HPP
+#define EASY_EXIF_TAG_FEEDER_HPP
+
+#include <memory>
+#include <string>
+
+#include "core_export.h"
+#include "itagfeeder.hpp"
+
+struct ITagData;
+
+class CORE_EXPORT EasyExifTagFeeder : public ITagFeeder
+{
+public:
+	EasyExifTagFeeder();
+
+	virtual std::unique_ptr<ITagData> getTagsFor(const std::string& path) override;
+	virtual void update(ITagData *, const std::string& path) override;
+
+private:
+	void feed(const std::string& path, ITagData *);
+};
+
+#endif
+
