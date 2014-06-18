9e04fd39df0d0ac2029306ed1bc234b609c7ceeb
Michał Walenciak <Kicer86@gmail.com>
2014-06-18 22:35:19 +0200

    EasyExif: using custom download process. Enhacements in EasyExif finder

--- cmake/3rd_party/easyexif.cmake
+++ cmake/3rd_party/easyexif.cmake
@@ -1,16 +1,22 @@
 
-#script for easyexif download & build
+# script for easyexif download & build
+# ExternalProject module could be used here, but it has serious drawback: 
+# stackoverflow.com/questions/17446981/cmake-externalproject-add-and-findpackage
 
 include(ExternalProject)
 
 function(addEasyExif)
 
-    configure_file(${CMAKE_SOURCE_DIR}/cmake/3rd_party/easyexif.cmakelists ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/CMakeLists.txt)
+	find_program(SVN_EXECUTABLE svn DOC "Path to subversion executable")
 
-    ExternalProject_Add(easyexif
-                        PREFIX ${CMAKE_BINARY_DIR}/3rd_party
-                        SVN_REPOSITORY http://easyexif.googlecode.com/svn/trunk/
-                        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/3rd_party/root
-                        INSTALL_DIR ${CMAKE_BINARY_DIR}/3rd_party/root)
+	if (SVN_EXECUTABLE)
+		add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp
+						   COMMAND ${SVN_EXECUTABLE} checkout http://easyexif.googlecode.com/svn/trunk/ easyexif
+						   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/3rd_party/src)
 
+		add_library(easyexif STATIC ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp)
+
+		set_source_files_properties(${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp  PROPERTIES GENERATED TRUE)
+	endif()
+	
 endfunction(addEasyExif)
