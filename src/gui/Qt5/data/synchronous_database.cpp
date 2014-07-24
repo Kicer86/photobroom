319f47ac33e34da8325e6a4cef37177f1e407cce
Michał Walenciak <Kicer86@gmail.com>
2014-07-24 21:07:34 +0200

    Revert "gui: preparing internal client for SynchronousDatabase"
    This reverts commit b33f8fefdac75c1a98fabe3a19bab078bf97d721.

--- src/gui/Qt5/data/synchronous_database.cpp
+++ src/gui/Qt5/data/synchronous_database.cpp
@@ -1,5 +1,5 @@
 /*
- * Synchronous database. Implementation of IDatabase
+ * <one line to give the program's name and a brief idea of what it does.>
  * Copyright (C) 2014  Michał Walenciak <MichalWalenciak@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -20,39 +20,7 @@
 #include "synchronous_database.hpp"
 
 
-struct SynchronousDatabase::Impl: Database::IDatabaseClient
-{
-    Database::IDatabase* m_database;
-
-    Impl(): m_database(nullptr) {}
-
-    virtual void got_getAllPhotos(const Database::Task& task, const Database::QueryList& query)
-    {
-    }
-
-    virtual void got_getPhoto(const Database::Task& task, const PhotoInfo::Ptr& photo)
-    {
-    }
-
-    virtual void got_getPhotos(const Database::Task& task, const Database::QueryList& query)
-    {
-    }
-
-    virtual void got_listTags(const Database::Task& task, const std::vector<TagNameInfo>& tags)
-    {
-    }
-
-    virtual void got_listTagValues(const Database::Task& task, const std::deque<TagValueInfo>& tags)
-    {
-    }
-
-    virtual void got_storeStatus(const Database::Task& task)
-    {
-    }
-};
-
-
-SynchronousDatabase::SynchronousDatabase(): m_impl(new Impl)
+SynchronousDatabase::SynchronousDatabase()
 {
 
 }
@@ -64,73 +32,67 @@ SynchronousDatabase::~SynchronousDatabase()
 }
 
 
-void SynchronousDatabase::setDatabase(Database::IDatabase* database)
-{
-    m_impl->m_database = database;
-}
-
-
 void SynchronousDatabase::closeConnections()
 {
-    m_impl->m_database->closeConnections();
+
 }
 
 
-void SynchronousDatabase::getAllPhotos(const Database::Task& task)
+void SynchronousDatabase::getAllPhotos(const Database::Task&)
 {
-    m_impl->m_database->getAllPhotos(task);
+
 }
 
 
-void SynchronousDatabase::getPhoto(const Database::Task& task, const PhotoInfo::Id& id)
+void SynchronousDatabase::getPhoto(const Database::Task&, const PhotoInfo::Id&)
 {
-    m_impl->m_database->getPhoto(task, id);
+
 }
 
 
-void SynchronousDatabase::getPhotos(const Database::Task& task, const std::deque<Database::IFilter::Ptr>& filters)
+void SynchronousDatabase::getPhotos(const Database::Task&, const std::deque< Database::IFilter::Ptr >&)
 {
-    m_impl->m_database->getPhotos(task, filters);
+
 }
 
 
-bool SynchronousDatabase::init(const Database::Task& task, const std::string& s)
+bool SynchronousDatabase::init(const Database::Task&, const std::string&)
 {
-    return m_impl->m_database->init(task, s);
+
 }
 
 
-void SynchronousDatabase::listTags(const Database::Task& task)
+void SynchronousDatabase::listTags(const Database::Task&)
 {
-    m_impl->m_database->listTags(task);
+
 }
 
 
-void SynchronousDatabase::listTagValues(const Database::Task& task, const TagNameInfo& tag)
+void SynchronousDatabase::listTagValues(const Database::Task&, const TagNameInfo&)
 {
-    m_impl->m_database->listTagValues(task, tag);
+
 }
 
 
-void SynchronousDatabase::listTagValues(const Database::Task& task, const TagNameInfo& tag, const std::deque<Database::IFilter::Ptr>& filters)
+void SynchronousDatabase::listTagValues(const Database::Task&, const TagNameInfo&, const std::deque< Database::IFilter::Ptr >&)
 {
-    m_impl->m_database->listTagValues(task, tag, filters);
+
 }
 
 
 Database::ADatabaseSignals* SynchronousDatabase::notifier()
 {
-    return m_impl->m_database->notifier();
+
 }
 
 
-Database::Task SynchronousDatabase::prepareTask(Database::IDatabaseClient* client)
+Database::Task SynchronousDatabase::prepareTask(Database::IDatabaseClient*)
 {
-    return m_impl->m_database->prepareTask(client);
+
 }
 
 
-void SynchronousDatabase::store(const Database::Task& task, const PhotoInfo::Ptr& photo)
+void SynchronousDatabase::store(const Database::Task&, const PhotoInfo::Ptr&)
 {
-    m_impl->m_database->store(task, photo);
+
 }
