a8844c4f5c45c3198621f5c06a50f576289750bb
Michał Walenciak <Kicer86@gmail.com>
2014-08-12 15:55:18 +0200

    gui: PhotosMatcher introduced

---
+++ src/gui/Qt5/model_view/model_helpers/photos_matcher.hpp
@@ -0,0 +1,33 @@
+/*
+ * class used for finding right place in tree for new photos
+ * Copyright (C) 2014  Michał Walenciak <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PHOTOSMATCHER_HPP
+#define PHOTOSMATCHER_HPP
+
+class PhotosMatcher
+{
+    public:
+        PhotosMatcher();
+        PhotosMatcher(const PhotosMatcher &) = delete;
+        ~PhotosMatcher();
+        
+        PhotosMatcher& operator=(const PhotosMatcher &) = delete;
+};
+
+#endif // PHOTOSMATCHER_HPP
