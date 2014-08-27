9c85ba459e7b5672a41fc4782f1829ed3a0cdb99
Michał Walenciak <Kicer86@gmail.com>
2014-08-27 15:16:49 +0200

    cmake: easyexif: link error fix

--- cmake/3rd_party/easyexif.cmake
+++ cmake/3rd_party/easyexif.cmake
@@ -23,6 +23,12 @@ function(addEasyExif)
                            DEPENDS ${CMAKE_BINARY_DIR}/3rd_party/src)
 
         add_library(easyexif STATIC ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp)
+        
+        if(MSVC)
+        
+        else()
+            set_source_files_properties(${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp PROPERTIES COMPILE_FLAGS -fPIC)    #use fPIC as easyexif will be linked against dynamic objects
+        endif()
 
         set_source_files_properties(${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp PROPERTIES GENERATED TRUE)
     endif()
