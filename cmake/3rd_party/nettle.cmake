e909efcace3e9cf40905cec86133318f62cd64af
Michał Walenciak <Kicer86@gmail.com>
2014-08-27 14:40:46 +0200

    code format

--- cmake/3rd_party/nettle.cmake
+++ cmake/3rd_party/nettle.cmake
@@ -6,9 +6,9 @@ include(ExternalProject)
 function(addNettle)
   
     ExternalProject_Add(Nettle 
-			PREFIX ${CMAKE_BINARY_DIR}/3rd_party
-			URL https://ftp.gnu.org/gnu/nettle/nettle-3.0.tar.gz
-			CONFIGURE_COMMAND ../Nettle/configure --prefix=${CMAKE_BINARY_DIR}/3rd_party/root
-			INSTALL_DIR ${CMAKE_BINARY_DIR}/3rd_party/root)
+                        PREFIX ${CMAKE_BINARY_DIR}/3rd_party
+                        URL https://ftp.gnu.org/gnu/nettle/nettle-3.0.tar.gz
+                        CONFIGURE_COMMAND ../Nettle/configure --prefix=${CMAKE_BINARY_DIR}/3rd_party/root
+                        INSTALL_DIR ${CMAKE_BINARY_DIR}/3rd_party/root)
 
 endfunction(addNettle)
