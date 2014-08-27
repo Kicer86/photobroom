912f8bc17dd8c7b0c746544b440450546bdc1b30
Michał Walenciak <Kicer86@gmail.com>
2014-08-27 16:07:40 +0200

    cmake: easyexif: useless part removed

--- cmake/3rd_party/easyexif.cmake
+++ cmake/3rd_party/easyexif.cmake
@@ -10,17 +10,11 @@ function(addEasyExif)
     find_program(SVN_EXECUTABLE svn DOC "Path to subversion executable")
 
     if (SVN_EXECUTABLE)
-    
-        #create dir rule
-        add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/3rd_party/src
-                           COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/3rd_party/src
-                           WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-                           
+                              
         #download easyexif rule
         add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp
                            COMMAND ${SVN_EXECUTABLE} checkout http://easyexif.googlecode.com/svn/trunk/ easyexif
-                           WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/3rd_party/src
-                           DEPENDS ${CMAKE_BINARY_DIR}/3rd_party/src)
+                           WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/3rd_party/src)
 
         add_library(easyexif STATIC ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp)
         
