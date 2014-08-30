b833d021eb7b8bd1dcc09f12d1173cf3a7bd162d
Michał Walenciak <Kicer86@gmail.com>
2014-08-30 21:41:51 +0200

    project utils: introducing project and ProjectManager

---
+++ src/project_utils/project_manager.hpp
@@ -0,0 +1,41 @@
+/*
+ * Managing projects
+ * Copyright (C) 2014  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PROJECTMANAGER_HPP
+#define PROJECTMANAGER_HPP
+
+#include "project_utils_export.h"
+
+#include "iproject_manager.hpp"
+
+class QString;
+
+class PROJECT_UTILS_EXPORT ProjectManager: public IProjectManager
+{
+    public:
+        ProjectManager();
+        ProjectManager(const ProjectManager &) = delete;
+        virtual ~ProjectManager();
+
+        ProjectManager& operator=(const ProjectManager &) = delete;
+
+        std::shared_ptr<IProject> open(const QString &) override;
+};
+
+#endif // PROJECTMANAGER_HPP
