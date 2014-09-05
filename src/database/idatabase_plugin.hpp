07cc24e01aea7facc4c1788646651b45e98f2bf5
Michał Walenciak <Kicer86@gmail.com>
2014-09-05 21:18:14 +0200

    comments

--- src/database/idatabase_plugin.hpp
+++ src/database/idatabase_plugin.hpp
@@ -43,8 +43,8 @@ namespace Database
 
         virtual ~IPlugin() {}
         
-        virtual std::unique_ptr<IBackend> constructBackend() = 0;
-        virtual QString backendName() const = 0;                          
+        virtual std::unique_ptr<IBackend> constructBackend() = 0;         //return backend object
+        virtual QString backendName() const = 0;                          //return backend name
         virtual PrjData initPrjDir(const QString &) const = 0;            //prepares database in provided directory
     };
 }
