9917a17e142f774e705e9a68b7ea7b44a5085a74
Michał Walenciak <Kicer86@gmail.com>
2014-09-07 15:47:21 +0200

    core: introducing TagUpdater

---
+++ src/core/implementation/tag_updater.cpp
@@ -0,0 +1,62 @@
+/*
+ * Tool for updating Photo's tags
+ * Copyright (C) 2014  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "tag_updater.hpp"
+
+TagUpdater::TagUpdater()
+{
+
+}
+
+
+TagUpdater::~TagUpdater()
+{
+
+}
+
+
+bool TagUpdater::isValid()
+{
+
+}
+
+void TagUpdater::clear()
+{
+
+}
+
+void TagUpdater::setTags(const ITagData::TagsList& )
+{
+
+}
+
+void TagUpdater::setTag(const TagNameInfo& name, const TagValueInfo& value)
+{
+
+}
+
+void TagUpdater::setTag(const TagNameInfo& name, const ITagData::ValuesSet& values)
+{
+
+}
+
+ITagData::TagsList TagUpdater::getTags()
+{
+
+}
