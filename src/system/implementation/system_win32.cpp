350c208a61ed949440fe2ab1593edbf5db0dc3c4
Michał Walenciak <kicer86@gmail.com>
2014-10-15 14:54:09 +0200

    system: fixing build for MinGW

--- src/system/implementation/system_win32.cpp
+++ src/system/implementation/system_win32.cpp
@@ -10,12 +10,18 @@ namespace
 {
     QString readVar(const char* name)
     {
+#ifdef __GNUC__
+        const QString result(getenv(name));
+
+        return result;
+#else
         char* result = nullptr;
         size_t size = 0;
 
         _dupenv_s(&result, &size, name);
 
         return QString(result);
+#endif
     }
 }
 
