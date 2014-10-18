276288e4274bb66fa64c4482803dfb5439549027
Michał Walenciak <Kicer86@gmail.com>
2014-10-18 17:19:22 +0200

    first polish translation

---
+++ tr/photo_broom_pl.ts
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>Default Tag Name</name>
+    <message>
+        <location filename="../src/core/implementation/base_tags.cpp" line="38"/>
+        <source>People</source>
+        <translation>Osoby</translation>
+    </message>
+    <message>
+        <location filename="../src/core/implementation/base_tags.cpp" line="39"/>
+        <source>Place</source>
+        <translation>Miejsce</translation>
+    </message>
+    <message>
+        <location filename="../src/core/implementation/base_tags.cpp" line="40"/>
+        <source>Date</source>
+        <translation>Data</translation>
+    </message>
+    <message>
+        <location filename="../src/core/implementation/base_tags.cpp" line="41"/>
+        <source>Time</source>
+        <translation>Godzina</translation>
+    </message>
+    <message>
+        <location filename="../src/core/implementation/base_tags.cpp" line="42"/>
+        <source>Event</source>
+        <translation>Wydarzenie</translation>
+    </message>
+</context>
+<context>
+    <name>LevelEditor</name>
+    <message>
+        <location filename="../src/gui/Qt5/components/filters_editor/level_editor.cpp" line="205"/>
+        <source>Grouping:</source>
+        <translation>Grupowanie:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="14"/>
+        <source>Photo broom</source>
+        <translation>Photo broom</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="23"/>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="50"/>
+        <source>Photos</source>
+        <translation>Zdjęcia</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="28"/>
+        <source>Staged photos</source>
+        <translation>Zdjęcia do zatwierdzenia</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="43"/>
+        <source>Project</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="57"/>
+        <source>Help</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="63"/>
+        <source>Windows</source>
+        <translation>Okna</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="74"/>
+        <source>New project</source>
+        <translation>Nowy</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="79"/>
+        <source>Open project</source>
+        <translation>Otwórz</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="87"/>
+        <source>Add photos</source>
+        <translation>Dodaj zdjęcia</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="92"/>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.ui" line="97"/>
+        <source>Browse staged photos</source>
+        <translation>Przeglądaj zdjęcia do zatwierdzenia</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.cpp" line="151"/>
+        <source>Open File</source>
+        <translation>Otwórz plik</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.cpp" line="153"/>
+        <source>Broom projects (*.bpj)</source>
+        <translation>Projekty programu Broom (*.bpj)</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/ui/mainwindow.cpp" line="161"/>
+        <source>Choose directory with photos</source>
+        <translation>Wybierz katalog ze zdjęciami</translation>
+    </message>
+</context>
+<context>
+    <name>PhotosStagingArea</name>
+    <message>
+        <location filename="../src/gui/Qt5/components/staged_photos_view.cpp" line="128"/>
+        <source>save photos</source>
+        <translation>Zapisz zdjęcia</translation>
+    </message>
+</context>
+<context>
+    <name>PhotosView</name>
+    <message>
+        <location filename="../src/gui/Qt5/ui/photos_view.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PrjLocationDialog</name>
+    <message>
+        <location filename="../src/gui/Qt5/components/project_creator/project_creator_dialog.cpp" line="49"/>
+        <source>Project File Name</source>
+        <translation>Nazwa projektu</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/components/project_creator/project_creator_dialog.cpp" line="50"/>
+        <source>Broom project (*.bpj)</source>
+        <translation>Projekty programu Broom (*.bpj)</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectCreatorDialog</name>
+    <message>
+        <location filename="../src/gui/Qt5/components/project_creator/project_creator_dialog.cpp" line="73"/>
+        <source>Project creator</source>
+        <translation>Kreator projektu</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/components/project_creator/project_creator_dialog.cpp" line="77"/>
+        <source>Project location:</source>
+        <translation>Lokacja projektu:</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/components/project_creator/project_creator_dialog.cpp" line="78"/>
+        <source>Browse</source>
+        <translation>Przeglądaj</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/components/project_creator/project_creator_dialog.cpp" line="89"/>
+        <source>Database engine:</source>
+        <translation>Silnik bazy danych:</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/Qt5/components/project_creator/project_creator_dialog.cpp" line="98"/>
+        <source>Engine options</source>
+        <translation>Opcje silnika</translation>
+    </message>
+</context>
+</TS>
