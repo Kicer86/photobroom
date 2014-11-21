54b4084894a0c188944070ed48f3fdb494034cac
Michał Walenciak <Kicer86@gmail.com>
2014-11-21 23:04:50 +0100

    tools: making Launcher to launch ;)

--- tools/main.cpp
+++ tools/main.cpp
@@ -1,6 +1,7 @@
 
 #include <windows.h>
 #include <tchar.h>
+#include <string>
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -11,6 +12,21 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	ZeroMemory(&pi, sizeof(pi));
 	si.cb = sizeof(si);
 
-	SetDllDirectory("./lib");
-	CreateProcess(NULL, "./bin/photo_broom.exe", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+	char path[MAX_PATH + 1];
+	GetCurrentDirectory(MAX_PATH, path);
+
+	const std::string base(path);
+	const std::string bin(base + "/bin");
+	const std::string lib(base + "/lib");
+	
+	//add lib dir to PATH
+	const std::string path_var = std::string("PATH=") + getenv("PATH") + ";" + lib;
+	_putenv(path_var.c_str());
+	
+	//some extra setup
+	SetDllDirectory(lib.c_str());
+	SetCurrentDirectory(bin.c_str());
+
+	//launch process
+	CreateProcess(NULL, "photo_broom.exe", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
 }
