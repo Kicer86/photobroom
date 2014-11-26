6ee2363f7b065a8b68f0b0ab45b22ef38ea1e56a
Michał Walenciak <Kicer86@gmail.com>
2014-11-26 23:38:08 +0100

    tools: img_manip: 3rd parameter describes size

--- tools/img_manip.cpp
+++ tools/img_manip.cpp
@@ -12,14 +12,19 @@ int main(int argc, char** argv)
     const QString src = argv[1];
     const QString dst = argv[2];
 
+    int size = 0;
+    if (argc > 2)
+        size = QString(argv[3]).toInt();
+
     QImage srcImg(src);
 
-    qCritical() << "dupa" << src << dst;
+    if (size > 0)
+        srcImg = srcImg.scaled(size, size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
 
     QImageWriter dstImg(dst);
     const bool status = dstImg.write(srcImg);
 
-    if(!status)
+    if (!status)
         qCritical() << dstImg.errorString();
 
     return status? 0: 2;
