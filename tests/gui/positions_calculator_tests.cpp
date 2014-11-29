174bbc3134a834bdf374b499e576c9f2e0137d7a
Michał Walenciak <Kicer86@gmail.com>
2014-11-29 22:43:02 +0100

    gui tests: instantiating mock model instead of real one

--- tests/gui/positions_calculator_tests.cpp
+++ tests/gui/positions_calculator_tests.cpp
@@ -9,7 +9,7 @@
 #include "data.hpp"
 
 #include "mock_configuration.hpp"
-
+#include "mock_qabstractitemmodel.hpp"
 
 TEST(PositionsCalculatorShould, BeConstructable)
 {
@@ -18,10 +18,10 @@ TEST(PositionsCalculatorShould, BeConstructable)
 
     EXPECT_NO_THROW({
         MockConfiguration config;
+        MockQAbstractItemModel model;
 
         EXPECT_CALL(config, findEntry(Configuration::BasicKeys::thumbnailWidth, _)).Times(1).WillOnce(Return("20"));
 
-        QStandardItemModel model;
         Data data;
         data.m_configuration = &config;
 
