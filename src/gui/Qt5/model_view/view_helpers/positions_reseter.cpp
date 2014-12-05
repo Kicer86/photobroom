4d143de8f450d623f56ab66141a7be1e7fd3870f
Michał Walenciak <Kicer86@gmail.com>
2014-12-05 11:49:51 +0100

    gui: fixing broken tests

--- src/gui/Qt5/model_view/view_helpers/positions_reseter.cpp
+++ src/gui/Qt5/model_view/view_helpers/positions_reseter.cpp
@@ -65,7 +65,7 @@ void PositionsReseter::itemChanged(const QModelIndex& idx)
     invalidateItemOverallRect(parent);
 
     //invalidate all items which are after 'pos'
-    const QModelIndex sibling = parent.child(idx.row() + 1, 0);
+    const QModelIndex sibling = parent.child(idx.row(), 0);
     invalidateSiblingsRect(sibling);
 }
 
@@ -76,7 +76,7 @@ void PositionsReseter::childrenRemoved(const QModelIndex& parent, int pos)
     invalidateItemOverallRect(parent);
 
     //invalidate all items which are after 'pos'
-    const QModelIndex sibling = parent.child(pos, 0);
+    const QModelIndex sibling = parent.child(pos - 1 , 0);   //-1: we need to start invalidation starting from 'pos', however 'invalidateSiblingsRect' starts from pos + 1
     invalidateSiblingsRect(sibling);
 }
 
