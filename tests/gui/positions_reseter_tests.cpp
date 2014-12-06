9a4eca1a3ca79d650988862a292c4ddb91513799
Michał Walenciak <Kicer86@gmail.com>
2014-12-07 00:19:41 +0100

    tests: gui: introducing new tests

--- tests/gui/positions_reseter_tests.cpp
+++ tests/gui/positions_reseter_tests.cpp
@@ -311,6 +311,99 @@ TEST(PositionsReseterShould, ResetProperItemsWhenChildChanged)
 }
 
 
+TEST(PositionsReseterShould, ResetProperItemsWhenNodeChanges)
+{
+    //preparations
+    const int img_w = 100;
+    const int img_h = 50;
+    const int canvas_w = 500;
+
+    static MockConfiguration config;
+    static QStandardItemModel model;
+
+    Data data;
+    data.m_configuration = &config;
+
+    const QPixmap pixmap(img_w, img_h);
+    const QIcon icon(pixmap);
+
+    QStandardItem* top = new QStandardItem("Empty");
+    QStandardItem* child1 = new QStandardItem(icon, "Empty1");
+    QStandardItem* child2 = new QStandardItem(icon, "Empty2");
+    QStandardItem* child3 = new QStandardItem(icon, "Empty3");
+    QStandardItem* child4 = new QStandardItem(icon, "Empty4");
+    QStandardItem* child5 = new QStandardItem(icon, "Empty5");
+
+    QStandardItem* top2 = new QStandardItem("Empty");
+    QStandardItem* child2_1 = new QStandardItem(icon, "Empty1");
+    QStandardItem* child2_2 = new QStandardItem(icon, "Empty2");
+    QStandardItem* child2_3 = new QStandardItem(icon, "Empty3");
+    QStandardItem* child2_4 = new QStandardItem(icon, "Empty4");
+    QStandardItem* child2_5 = new QStandardItem(icon, "Empty5");
+
+    top->appendRow(child1);
+    top->appendRow(child2);
+    top->appendRow(child3);
+    top->appendRow(child4);
+    top->appendRow(child5);
+
+    top2->appendRow(child2_1);
+    top2->appendRow(child2_2);
+    top2->appendRow(child2_3);
+    top2->appendRow(child2_4);
+    top2->appendRow(child2_5);
+
+    model.appendRow(top);
+    model.appendRow(top2);
+
+    //expand main nodes to show children
+    {
+        ModelIndexInfo top_info = data.get(top->index());
+        top_info.expanded = true;
+        data.update(top_info);
+
+        ModelIndexInfo top2_info = data.get(top2->index());
+        top2_info.expanded = true;
+        data.update(top2_info);
+    }
+
+    {
+        PositionsCalculator calculator(&model, &data, canvas_w);
+        calculator.updateItems();
+    }
+
+    //// test
+    PositionsReseter reseter(&data);
+    reseter.itemChanged(top->index());
+
+    //expectations
+    {
+        ModelIndexInfo info = data.get(top->index());
+        EXPECT_EQ(QRect(), info.getRect());            // Node's size should be reseted.
+        EXPECT_EQ(QRect(), info.getOverallRect());     // So overall size should.
+    }
+
+
+    {
+        //children should not be reseted
+        ModelIndexInfo info1 = data.get(child1->index());
+        ModelIndexInfo info5 = data.get(child5->index());
+        EXPECT_NE(QRect(), info1.getRect());
+        EXPECT_NE(QRect(), info5.getRect());
+    }
+
+    {
+        ModelIndexInfo info = data.get(top2->index());           //top2's and all its children's positions should be reseted
+        ModelIndexInfo info1 = data.get(child2_1->index());
+        ModelIndexInfo info5 = data.get(child2_5->index());
+
+        EXPECT_EQ(QRect(), info.getRect());
+        EXPECT_EQ(QRect(), info1.getRect());
+        EXPECT_EQ(QRect(), info5.getRect());
+    }
+}
+
+
 TEST(PositionsReseterShould, ResetAllItemsWhenAllAreToBeInvalidated)
 {
     //preparations
