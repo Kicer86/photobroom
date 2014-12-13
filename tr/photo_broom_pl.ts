bdab1bd830f3f4431668f98bf03c2a45a7bb5ec8
Michał Walenciak <Kicer86@gmail.com>
2014-12-13 01:12:42 +0100

    tr: update for ts file

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -32,7 +32,7 @@
 <context>
     <name>IdxDataManager</name>
     <message>
-        <location filename="../src/gui/Qt5/model_view/model_helpers/idx_data_manager.cpp" line="624"/>
+        <location filename="../src/gui/Qt5/model_view/model_helpers/idx_data_manager.cpp" line="625"/>
         <source>Nonmatching</source>
         <translation type="unfinished"></translation>
     </message>
