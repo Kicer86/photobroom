763d592e2445e1d4c9ba2f5e3e0882716695f198
Michał Walenciak <Kicer86@gmail.com>
2014-12-13 01:14:05 +0100

    tests: gui: next steps in IdxDataManagerShould::AddUniversalNodeOnTopWhenPhotoDoesntMatchOtherTopNodes

--- tests/gui/model/idx_data_manager_tests.cpp
+++ tests/gui/model/idx_data_manager_tests.cpp
@@ -1,11 +1,28 @@
 
 #include <gtest/gtest.h>
 
+#include <QPixmap>
+
+#include <core/base_tags.hpp>
 #include <Qt5/model_view/model_helpers/idx_data_manager.hpp>
 #include <Qt5/components/photos_data_model.hpp>
 
-#include "mock_database.hpp"
-#include "internal_task_executor.hpp"
+#include "test_helpers/mock_database.hpp"
+#include "test_helpers/internal_task_executor.hpp"
+#include "test_helpers/mock_photo_info.hpp"
+
+
+namespace
+{
+    struct DatabaseNotifier: Database::ADatabaseSignals
+    {
+    };
+
+    struct PhotoInfo: IPhotoInfo
+    {
+
+    };
+}
 
 
 TEST(IdxDataManagerShould, BeConstructable)
@@ -19,15 +36,27 @@ TEST(IdxDataManagerShould, BeConstructable)
 
 TEST(IdxDataManagerShould, AddUniversalNodeOnTopWhenPhotoDoesntMatchOtherTopNodes)
 {
+    using ::testing::Return;
+
     //construct objects
+    DatabaseNotifier notifier;
     MockDatabase database;
     InternalTaskExecutor executor;
+
+    Hierarchy hierarchy;
+    hierarchy.levels = { { BaseTags::get(BaseTagsList::Date), Hierarchy::Level::Order::ascending } };
     
     PhotosDataModel model;
     model.setDatabase(&database);
+    model.setHierarchy(hierarchy);
     model.set(&executor);
+
+    //prepare data
+    MockPhotoInfo::Ptr photoInfo = std::make_shared<MockPhotoInfo>();
     
     //define expectations
-    
+    EXPECT_CALL(database, notifier()).WillRepeatedly(Return(&notifier));
+
     //do test
+    notifier.photoAdded(photoInfo);
 }
