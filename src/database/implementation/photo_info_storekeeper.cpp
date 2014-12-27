cc5e7e74c2d156c47df6374ba1dbb7ec14e3ce49
Michał Walenciak <Kicer86@gmail.com>
2014-12-27 21:42:28 +0100

    database: introducing photo storekeeper

---
+++ src/database/implementation/photo_info_storekeeper.cpp
@@ -0,0 +1,89 @@
+/*
+ * PhotoInfoStorekeeper - stores changed photos in database
+ * Copyright (C) 2014  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "photo_info_storekeeper.hpp"
+
+#include <database/iphoto_info.hpp>
+#include <idatabase.hpp>
+
+namespace
+{
+    struct Update: Database::IStorePhotoTask
+    {
+        virtual ~Update() {}
+        virtual void got(bool) {}
+    };
+}
+
+struct PhotoInfoStorekeeper::Data
+{
+    Data(): m_cache(nullptr), m_database(nullptr) {}
+    Data(const Data &) = delete;
+    Data& operator=(const Data &) = delete;
+
+    ~Data() {}
+
+    Database::IPhotoInfoCache* m_cache;
+    Database::IDatabase* m_database;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+PhotoInfoStorekeeper::PhotoInfoStorekeeper(): m_data(new Data)
+{
+}
+
+
+PhotoInfoStorekeeper::~PhotoInfoStorekeeper()
+{
+
+}
+
+
+void PhotoInfoStorekeeper::setDatabase(Database::IDatabase* database)
+{
+    m_data->m_database = database;
+}
+
+
+void PhotoInfoStorekeeper::setCache(Database::IPhotoInfoCache* cache)
+{
+    m_data->m_cache = cache;
+}
+
+
+//TODO: those conditions there don't look nice...
+//is there nicer way for direct access to IPhotoInfo::Ptr?
+void PhotoInfoStorekeeper::photoUpdated(IPhotoInfo* photoInfo)
+{
+    //find photo in cache
+    const IPhotoInfo::Id id = photoInfo->getID();
+    IPhotoInfo::Ptr ptr = m_data->m_cache->find(id);
+
+    //we should be aware of all exisitng photo info
+    assert(ptr.get() != nullptr);
+
+    //when found update changed photo in database
+    if (ptr.get() != nullptr && ptr->isFullyInitialized())
+    {
+        std::unique_ptr<Database::IStorePhotoTask> task(new Update);
+
+        m_data->m_database->exec(std::move(task), ptr);
+    }
+}
---
+++ src/database/implementation/photo_info_storekeeper.hpp
@@ -0,0 +1,53 @@
+/*
+ * PhotoInfoStorekeeper - stores changed photos in database
+ * Copyright (C) 2014  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PHOTO_INFO_STOREKEEPER_HPP
+#define PHOTO_INFO_STOREKEEPER_HPP
+
+#include <memory>
+
+#include <database/iphoto_info.hpp>
+
+#include "iphoto_info_manager.hpp"
+
+namespace Database
+{
+    struct IDatabase;
+}
+
+class PhotoInfoStorekeeper: public IPhotoInfo::IObserver
+{
+    public:
+        PhotoInfoStorekeeper();
+        PhotoInfoStorekeeper(const PhotoInfoStorekeeper& other) = delete;
+        ~PhotoInfoStorekeeper();
+
+        PhotoInfoStorekeeper& operator=(const PhotoInfoStorekeeper& other) = delete;
+
+        void setDatabase(Database::IDatabase *);
+        void setCache(Database::IPhotoInfoCache *);
+
+    private:
+        struct Data;
+        std::unique_ptr<Data> m_data;
+
+        virtual void photoUpdated(IPhotoInfo *) override;
+};
+
+#endif // PHOTO_INFO_STOREKEEPER_HPP
