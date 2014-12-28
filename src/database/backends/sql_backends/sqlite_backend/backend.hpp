78d2b594d3d8f77d546bf15bad0ebcb7dabcd14c
Michał Walenciak <Kicer86@gmail.com>
2014-12-28 14:09:22 +0100

    database: moving sql_backends to backends dir

--- src/database/backends/sql_backends/sqlite_backend/backend.hpp
+++ src/database/backends/sql_backends/sqlite_backend/backend.hpp
@@ -8,8 +8,8 @@
 #include <QtPlugin>
 
 #include <database/idatabase.hpp>
-#include <database/sql_backends/sql_backend.hpp>
-#include <database/sql_backends/generic_sql_query_constructor.hpp>
+#include <database/backends/sql_backends/sql_backend.hpp>
+#include <database/backends/sql_backends/generic_sql_query_constructor.hpp>
 #include <database/implementation/ibackend_qt_interface.hpp>
 
 #include "database_sqlite_backend_export.h"
