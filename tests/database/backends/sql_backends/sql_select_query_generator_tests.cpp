29aec4718d65285b276206ca546eb47f36f6d745
Michał Walenciak <Kicer86@gmail.com>
2015-01-03 17:41:32 +0100

    tests: sql_backend_base: adding tests

--- tests/database/backends/sql_backends/sql_select_query_generator_tests.cpp
+++ tests/database/backends/sql_backends/sql_select_query_generator_tests.cpp
@@ -20,12 +20,25 @@ TEST(SqlSelectQueryGeneratorTest, HandlesFalgsFilter)
     std::deque<Database::IFilter::Ptr> filters;
 
     std::shared_ptr<Database::FilterPhotosWithFlags> filter = std::make_shared<Database::FilterPhotosWithFlags>();
-    filter->flag = IPhotoInfo::FlagsE::ExifLoaded;
-    filter->value = 1;
-
     filters.push_back(filter);
 
-    const QString query = generator.generate(filters);
-
+    filter->flag = IPhotoInfo::FlagsE::ExifLoaded;
+    filter->value = 1;
+    QString query = generator.generate(filters);
     EXPECT_EQ("SELECT photos.id AS photos_id FROM photos JOIN (flags) ON (flags.photo_id = photos.id) WHERE flags.tags_loaded = '1'", query);
+
+    filter->flag = IPhotoInfo::FlagsE::Sha256Loaded;
+    filter->value = 2;
+    query = generator.generate(filters);
+    EXPECT_EQ("SELECT photos.id AS photos_id FROM photos JOIN (flags) ON (flags.photo_id = photos.id) WHERE flags.hash_loaded = '2'", query);
+
+    filter->flag = IPhotoInfo::FlagsE::StagingArea;
+    filter->value = 3;
+    query = generator.generate(filters);
+    EXPECT_EQ("SELECT photos.id AS photos_id FROM photos JOIN (flags) ON (flags.photo_id = photos.id) WHERE flags.staging_area = '3'", query);
+
+    filter->flag = IPhotoInfo::FlagsE::ThumbnailLoaded;
+    filter->value = 4;
+    query = generator.generate(filters);
+    EXPECT_EQ("SELECT photos.id AS photos_id FROM photos JOIN (flags) ON (flags.photo_id = photos.id) WHERE flags.thumbnail_loaded = '4'", query);
 }
