85f34d769aec5a02ae421927b353b4eea1147643
Michał Walenciak <Kicer86@gmail.com>
2015-01-03 22:18:24 +0100

    tests: database: adding tests

--- tests/database/backends/sql_backends/sql_select_query_generator_tests.cpp
+++ tests/database/backends/sql_backends/sql_select_query_generator_tests.cpp
@@ -42,3 +42,43 @@ TEST(SqlSelectQueryGeneratorTest, HandlesFalgsFilter)
     query = generator.generate(filters);
     EXPECT_EQ("SELECT photos.id AS photos_id FROM photos JOIN (flags) ON (flags.photo_id = photos.id) WHERE flags.thumbnail_loaded = '4'", query);
 }
+
+
+TEST(SqlSelectQueryGeneratorTest, HandlesTagsFilter)
+{
+    Database::SqlSelectQueryGenerator generator;
+    std::deque<Database::IFilter::Ptr> filters;
+
+    std::shared_ptr<Database::FilterPhotosWithTag> filter = std::make_shared<Database::FilterPhotosWithTag>();
+    filters.push_back(filter);
+
+    filter->tagName = TagNameInfo("test_name", TagNameInfo::Text);
+    filter->tagValue = "test_value";
+
+    const QString query = generator.generate(filters);
+
+    EXPECT_EQ("SELECT photos.id AS photos_id FROM photos "
+              "JOIN (tags, tag_names) "
+              "ON (tags.photo_id = photos.id AND tags.name_id = tag_names.id) "
+              "WHERE tag_names.name = 'test_name' AND tags.value = 'test_value'", query);
+}
+
+
+TEST(SqlSelectQueryGeneratorTest, HandlesWithoutTagsFilter)
+{
+    Database::SqlSelectQueryGenerator generator;
+    std::deque<Database::IFilter::Ptr> filters;
+
+    std::shared_ptr<Database::FilterPhotosWithoutTag> filter = std::make_shared<Database::FilterPhotosWithoutTag>();
+    filters.push_back(filter);
+
+    filter->tagName = TagNameInfo("test_name", TagNameInfo::Text);
+
+    const QString query = generator.generate(filters);
+
+    EXPECT_EQ("SELECT photos.id AS photos_id FROM photos "
+              "WHERE photos.id NOT IN "
+              "(SELECT tags.photo_id FROM tags "
+              "JOIN tag_names ON ( tag_names.id = tags.name_id) "
+              "WHERE tag_names.name = 'test_name')", query);
+}
