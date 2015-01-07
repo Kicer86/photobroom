74f09b8e7996d4de826535ffd38c5871968413f4
Michał Walenciak <Kicer86@gmail.com>
2015-01-07 14:57:08 +0100

    tests: gui: adopting MockDatabase to Database::IDatabase changes

--- tests/gui/test_helpers/mock_database.hpp
+++ tests/gui/test_helpers/mock_database.hpp
@@ -11,18 +11,18 @@
 struct MockDatabase: Database::IDatabase
 {
     /*
-    MOCK_METHOD2(addPath, void(const Database::Task &, const QString &));
-    MOCK_METHOD0(closeConnections, void());
-    MOCK_METHOD1(getAllPhotos, void(const Database::Task &));
-    MOCK_METHOD2(getPhoto, void(const Database::Task &, const IPhotoInfo::Id &));
-    MOCK_METHOD2(getPhotos, void(const Database::Task &, const std::deque<Database::IFilter::Ptr> &));
-    MOCK_METHOD2(init, bool(const Database::Task &, const Database::ProjectInfo &));
-    MOCK_METHOD1(listTags, void(const Database::Task &));
-    MOCK_METHOD3(listTagValues, void(const Database::Task &, const TagNameInfo &, const std::deque<Database::IFilter::Ptr> &));
-    MOCK_METHOD2(listTagValues, void(const Database::Task &, const TagNameInfo &));
     MOCK_METHOD0(notifier, Database::ADatabaseSignals*());
-    MOCK_METHOD1(prepareTask, Database::Task(Database::IDatabaseClient *));
-    MOCK_METHOD2(update, void(const Database::Task &, const IPhotoInfo::Ptr &));
+    MOCK_METHOD2(exec, void(std::unique_ptr<Database::IStorePhotoTask> &&, const QString &));
+    MOCK_METHOD2(exec, void(std::unique_ptr<Database::IStorePhotoTask> &&, const IPhotoInfo::Ptr &));
+    MOCK_METHOD2(exec, void(std::unique_ptr<Database::IStoreTagTask> &&, const TagNameInfo &));
+    MOCK_METHOD1(exec, void(std::unique_ptr<Database::IListTagsTask> &&));
+    MOCK_METHOD2(exec, void(std::unique_ptr<Database::IListTagValuesTask> &&, const TagNameInfo &));
+    MOCK_METHOD3(exec, void(std::unique_ptr<Database::IListTagValuesTask> &&, const TagNameInfo &, const std::deque<Database::IFilter::Ptr> &));
+    MOCK_METHOD1(exec, void(std::unique_ptr<Database::IGetPhotosTask> &&));
+    MOCK_METHOD2(exec, void(std::unique_ptr<Database::IGetPhotosTask> &&, const std::deque<Database::IFilter::Ptr> &));
+    MOCK_METHOD2(exec, void(std::unique_ptr<Database::IGetPhotoTask> &&, const IPhotoInfo::Id &));
+    MOCK_METHOD2(exec, bool(std::unique_ptr<Database::IInitTask> &&, const Database::ProjectInfo &));
+    MOCK_METHOD0(closeConnections, void());
     */
 };
 
