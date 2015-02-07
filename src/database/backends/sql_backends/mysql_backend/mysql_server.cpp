a04d80162373cf609bb2d181572bd200e2d53af5
Michał Walenciak <Kicer86@gmail.com>
2015-02-07 10:53:15 +0100

    database: MySQL: cleanups in config file

--- src/database/backends/sql_backends/mysql_backend/mysql_server.cpp
+++ src/database/backends/sql_backends/mysql_backend/mysql_server.cpp
@@ -49,33 +49,7 @@ namespace
     const char* MySQL_daemon = "Database::Backend::MySQL::Server";
 
     const char* MySQL_config =
-        "#Config based on Akonadi's config file                                                        \n"
         "[mysqld]                                                                                     \n"
-
-        "# strict query parsing/interpretation                                                        \n"
-        "# TODO: make Akonadi work with those settings enabled                                        \n"
-        "# sql_mode=strict_trans_tables,strict_all_tables,strict_error_for_division_by_zero,no_auto_create_user,no_auto_value_on_zero,no_engine_substitution,no_zero_date,no_zero_in_date,only_full_group_by,pipes_as_concat\n"
-        "# sql_mode=strict_trans_tables                                                               \n"
-
-        "# DEBUGGING:\n"
-        "# log all queries, useful for debugging but generates an enormous amount of data\n"
-        "# log=mysql.full\n"
-        "# log queries slower than n seconds, log file name relative to datadir (for debugging only)\n"
-        "# log_slow_queries=mysql.slow\n"
-        "# long_query_time=1\n"
-        "# log queries not using indices, debug only, disable for production use\n"
-        "# log_queries_not_using_indexes=1\n"
-        "#\n"
-        "# mesure database size and adjust innodb_buffer_pool_size\n"
-        "# SELECT sum(data_length) as bla, sum(index_length) as blub FROM information_schema.tables WHERE table_schema not in (\"mysql\", \"information_schema\");\n"
-
-        "# NOTES:\n"
-        "# Keep Innob_log_waits and keep Innodb_buffer_pool_wait_free small (see show global status like \"inno%\", show global variables)\n"
-
-        "#expire_logs_days=3\n"
-
-        "#sync_bin_log=0\n"
-
         "# Use UTF-8 encoding for tables\n"
         "character_set_server=utf8\n"
         "collation_server=utf8_general_ci\n"
@@ -86,10 +60,6 @@ namespace
         "# memory pool InnoDB uses to store data dictionary information and other internal data structures (default:1M)\n"
         "# Deprecated in MySQL >= 5.6.3\n"
         "innodb_additional_mem_pool_size=1M\n"
-
-        "# memory buffer InnoDB uses to cache data and indexes of its tables (default:128M)\n"
-        "# Larger values means less I/O\n"
-        "innodb_buffer_pool_size=80M\n"
 
         "# Create a .ibd file for each table (default:0)\n"
         "innodb_file_per_table=1\n"
@@ -113,35 +83,11 @@ namespace
         "# Convert table named to lowercase\n"
         "lower_case_table_names=1\n"
 
-        "# Maximum size of one packet or any generated/intermediate string. (default:1M)\n"
-        "max_allowed_packet=32M\n"
-
-        "# Maximum simultaneous connections allowed (default:100)\n"
-        "max_connections=256\n"
-
-        "# The two options below make no sense with prepared statements and/or transactions\n"
-        "# (make sense when having the same query multiple times)\n"
-
-        "# Memory allocated for caching query results (default:0 (disabled))\n"
-        "query_cache_size=0\n"
-
-        "# Do not cache results (default:1)\n"
-        "query_cache_type=0\n"
-
         "# Do not use the privileges mechanisms\n"
         "skip_grant_tables\n"
 
         "# Do not listen for TCP/IP connections at all\n"
         "skip_networking\n"
-
-        "# The number of open tables for all threads. (default:64)\n"
-        "table_open_cache=200\n"
-
-        "# How many threads the server should cache for reuse (default:0)\n"
-        "thread_cache_size=3\n"
-
-        "# wait 365d before dropping the DB connection (default:8h)\n"
-        "wait_timeout=31536000\n"
 
         "[client]\n"
         "default-character-set=utf8\n";
