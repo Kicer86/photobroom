d379985a11fca17d8dabdefdd7b65c6a7b615ac8
Michał Walenciak <Kicer86@gmail.com>
2015-02-12 22:18:40 +0100

    tests: introducing core tests

---
+++ tests/core/tree_tests.cpp
@@ -0,0 +1,24 @@
+
+#include <core/tree.hpp>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+
+TEST(treeTest, isConstructible)
+{
+    EXPECT_NO_THROW(
+    {
+        tree<int> tr;
+    });
+}
+
+
+
+TEST(treeTest, isConstructedEmpty)
+{
+    tree<int> tr;
+
+    EXPECT_EQ(true, tr.empty());
+    EXPECT_EQ(true, tr.end() == tr.begin());
+}
