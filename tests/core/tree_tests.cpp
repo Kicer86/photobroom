5b66b95a27f76232b4cacb195cd27e93dcc522e6
Michał Walenciak <Kicer86@gmail.com>
2015-02-14 16:16:34 +0100

    tests: core: tree: adding test

--- tests/core/tree_tests.cpp
+++ tests/core/tree_tests.cpp
@@ -81,3 +81,20 @@ TEST(treeTest, acceptsChildLevelInserts)
     EXPECT_EQ(2, tr.end() - tr.begin());
     EXPECT_EQ("(1(3) 2)", dump<int>(tr));
 }
+
+
+TEST(treeTest, acceptsChildLevelInsertsAtRandomLocations)
+{
+    tree<int> tr;
+
+    tr.insert(tr.end(), 1);                         // (1)
+    auto it = tr.insert(tr.end(), 3);               // (1 3)
+
+    tr.insert(it.children_begin(), 4);              // (1 3(4))
+    it = tr.insert(it.children_begin(), 2);         // (1 3(2 4))
+    tr.insert(it.children_end(), 7);                // (1 3(2(7) 4))
+    tr.insert(it.children_end(), 8);                // (1 3(2(7 8) 4))
+
+    EXPECT_EQ(2, tr.end() - tr.begin());
+    EXPECT_EQ("(1 3(2(7 8) 4))", dump<int>(tr));
+}
