aaf4c7f38ef4a6d7c895c90baef25512f4121f1f
Michał Walenciak <Kicer86@gmail.com>
2015-02-14 15:50:14 +0100

    core: introducing tree_private with private classes; adding const_iterator
    tree::iterator was transformed into template and has two instantations: const and non-const

--- src/core/tree.hpp
+++ src/core/tree.hpp
@@ -17,279 +17,25 @@
  *
  */
 
-#ifndef TREE_H
-#define TREE_H
+#ifndef TREE_HPP
+#define TREE_HPP
 
 #include <deque>
 #include <memory>
 #include <stack>
-#include <sstream>
+#include <ostream>
 
 #include <OpenLibrary/utils/data_ptr.hpp>
+
+#include "implementation/tree_private.hpp"
 
 template<typename T>
 class tree final
 {
     public:
-        class node;
-        typedef std::deque<node> nodes;
-
-        class node final
-        {
-            public:
-                explicit node(const T& v): m_item(v), m_children() {}
-
-                node(const node& other): m_item(other.m_item), m_children(other.m_children)
-                {
-                }
-
-                ~node() {}
-
-                node& operator=(const node& other)
-                {
-                    if (this != &other)
-                    {
-                        m_item = other.m_item;
-                        m_children = other.m_children;
-                    }
-
-                    return *this;
-                }
-
-                bool operator==(const node& other) const
-                {
-                    return m_item == other.m_item && m_children == other.m_children;
-                }
-
-                const T& operator*() const
-                {
-                    return m_item;
-                }
-
-                T& operator*()
-                {
-                    return m_item;
-                }
-
-                bool has_children() const
-                {
-                    return m_children->empty() == false;
-                }
-
-                const nodes& children() const
-                {
-                    return *m_children;
-                }
-
-                nodes& children()
-                {
-                    return *m_children;
-                }
-
-            private:
-                T m_item;
-                ol::data_ptr<nodes> m_children;
-
-                friend std::ostream& operator<<(std::ostream& st, const node& n)
-                {
-                    st << *n;
-
-                    return st;
-                }
-        };
-
-
-        class iterator final
-        {
-            public:
-                iterator(): m_tree(nullptr), m_nodes(nullptr), m_node() {}
-
-                iterator(const iterator& other): m_tree(other.m_tree), m_nodes(nullptr), m_node(other.m_node) { }
-
-                ~iterator() {}
-
-                iterator& operator=(const iterator& other)
-                {
-                    if (this != &other)
-                    {
-                        m_tree = other.m_tree;
-                        m_nodes = other.m_nodes;
-                        m_node = other.m_node;
-                    }
-
-                    return *this;
-                }
-
-                bool operator==(const iterator& other) const
-                {
-                    return m_tree == other.m_tree &&
-                           m_nodes == other.m_nodes &&
-                           m_node == other.m_node;
-                }
-
-                iterator& operator++()
-                {
-                    ++m_node;
-
-                    return *this;
-                }
-
-                iterator operator++(int)
-                {
-                    iterator it = *this;
-                    ++(*this);
-
-                    return it;
-                }
-
-                int operator-(const iterator& it) const
-                {
-                    return m_node - it.m_node;
-                }
-
-                T& operator*()
-                {
-                    return *m_node;
-                }
-
-                const T& operator&() const
-                {
-                    return *m_node;
-                }
-
-                T* operator->()
-                {
-                    return *m_node;
-                }
-
-                const T* operator->() const
-                {
-                    return *m_node;
-                }
-
-                iterator children_begin() const
-                {
-                    return iterator(m_tree, &m_node->children(), m_node->children().begin());
-                }
-
-                iterator children_end() const
-                {
-                    return iterator(m_tree, &m_node->children(), m_node->children().end());
-                }
-
-                bool has_children() const
-                {
-                    return m_node->has_children();
-                }
-
-                const nodes& children() const
-                {
-                    return m_node->children();
-                }
-
-            private:
-                friend class tree;
-
-                tree*  m_tree;
-                nodes* m_nodes;
-                typename nodes::iterator m_node;
-
-                iterator(tree* tr, nodes* ns, typename nodes::iterator n): m_tree(tr), m_nodes(ns), m_node(n) { }
-        };
-
-
-        class recursive_iterator final
-        {
-            public:
-                recursive_iterator(const iterator& b, const iterator& e): m_iterators()
-                {
-                    m_iterators.push(std::make_pair(b, e));
-                }
-
-                recursive_iterator(const recursive_iterator& other): m_iterators(other.iterators) { }
-
-                ~recursive_iterator() {}
-
-                recursive_iterator& operator=(const recursive_iterator& other)
-                {
-                    if (this != &other)
-                        m_iterators = other.m_iterators;
-
-                    return *this;
-                }
-
-                bool operator==(const recursive_iterator& other) const
-                {
-                    return m_iterators == other.m_iterators;
-                }
-
-                recursive_iterator& operator++()
-                {
-                    iterator& c = current();
-                    const nodes& ch = c->children();
-
-                    if (ch.empty() == false)                          //dive
-                        m_iterators.push(ch.begin(), ch.end());
-                    else
-                    {                                                 //iterate
-                        ++c;
-
-                        if (c == last())                              //last one at current level? pop out an keep going
-                        {
-                            if (m_iterators.size() > 1)               //anything to pop? (don't pop out from last)
-                            {
-                                m_iterators.pop();
-                                ++(*this);
-                            }
-                        }
-                    }
-
-                    return *this;
-                }
-
-                recursive_iterator operator++(int)
-                {
-                    iterator it = *this;
-                    ++(*this);
-
-                    return it;
-                }
-
-                T& operator*()
-                {
-                    return *current();
-                }
-
-                const T& operator&() const
-                {
-                    return *current();
-                }
-
-                T* operator->()
-                {
-                    return *current();
-                }
-
-                const T* operator->() const
-                {
-                    return *current();
-                }
-
-            private:
-                std::stack< std::pair<iterator, iterator> > m_iterators;
-
-                iterator& current()
-                {
-                    return m_iterators.top().first;
-                }
-
-                iterator& last()
-                {
-                    return m_iterators.top().second;
-                }
-
-        };
-
+        typedef tree_private::iterator<tree_utils::node<T>, T> iterator;
+        typedef tree_private::iterator<const tree_utils::node<T>, T> const_iterator;
+        typedef tree_utils::node<T> node;
 
         tree(): m_roots()
         {
@@ -318,12 +64,22 @@ class tree final
 
         iterator begin()
         {
-            return iterator(this, &m_roots, m_roots.begin());
+            return iterator(&m_roots, m_roots.begin());
         }
 
         iterator end()
         {
-            return iterator(this, &m_roots, m_roots.end());
+            return iterator(&m_roots, m_roots.end());
+        }
+
+        const_iterator cbegin() const
+        {
+            return iterator(&m_roots, m_roots.begin());
+        }
+
+        const_iterator cend() const
+        {
+            return iterator(&m_roots, m_roots.end());
         }
 
         bool empty() const
@@ -336,43 +92,117 @@ class tree final
             const node n(value);
             auto it = pos.m_nodes->insert(pos.m_node, n);
 
-            return iterator(this, pos.m_nodes, it);
-        }
-
-        std::string dump() const
-        {
-            std::stringstream d;
-
-            d << m_roots;
-
-            return d.str();
+            return iterator(pos.m_nodes, it);
         }
 
     private:
-        nodes m_roots;
+        tree_utils::nodes<T> m_roots;
 
-        friend std::ostream& operator<<(std::ostream& st, const nodes& ns)
+        friend std::ostream& operator<<(std::ostream& st, const tree& tr)
         {
-            st << "(";
-
-            for(auto it = ns.begin(); it != ns.end();)
-            {
-                const node& n = *it;
-                st << n;
-
-                if (n.has_children())
-                    st << n.children();
-
-                ++it;
-
-                if (it != ns.end())
-                    st << " ";
-            }
-
-            st << ")";
+            st << tr.m_roots;
 
             return st;
         }
 };
+
+
+namespace tree_utils
+{
+    template<typename NT, typename T>
+    class recursive_iterator final
+    {
+            typedef typename tree<T>::iterator iterator;
+
+        public:
+            recursive_iterator(const iterator& b, const iterator& e): m_iterators()
+            {
+                m_iterators.push(std::make_pair(b, e));
+            }
+
+            recursive_iterator(const recursive_iterator& other): m_iterators(other.iterators) { }
+
+            ~recursive_iterator() {}
+
+            recursive_iterator& operator=(const recursive_iterator& other)
+            {
+                if (this != &other)
+                    m_iterators = other.m_iterators;
+
+                return *this;
+            }
+
+            bool operator==(const recursive_iterator& other) const
+            {
+                return m_iterators == other.m_iterators;
+            }
+
+            recursive_iterator& operator++()
+            {
+                iterator& c = current();
+                const nodes<T>& ch = c->children();
+
+                if (ch.empty() == false)                          //dive
+                    m_iterators.push(ch.begin(), ch.end());
+                else
+                {                                                 //iterate
+                    ++c;
+
+                    if (c == last())                              //last one at current level? pop out an keep going
+                    {
+                        if (m_iterators.size() > 1)               //anything to pop? (don't pop out from last)
+                        {
+                            m_iterators.pop();
+                            ++(*this);
+                        }
+                    }
+                }
+
+                return *this;
+            }
+
+            recursive_iterator operator++(int)
+            {
+                iterator it = *this;
+                ++(*this);
+
+                return it;
+            }
+
+            T& operator*()
+            {
+                return *current();
+            }
+
+            const T& operator&() const
+            {
+                return *current();
+            }
+
+            T* operator->()
+            {
+                return *current();
+            }
+
+            const T* operator->() const
+            {
+                return *current();
+            }
+
+        private:
+            typedef std::pair<iterator, iterator> level_info;
+            std::stack<level_info> m_iterators;
+
+            iterator& current()
+            {
+                return m_iterators.top().first;
+            }
+
+            iterator& last()
+            {
+                return m_iterators.top().second;
+            }
+    };
+}
 
 #endif // TREE_HPP
--- tests/core/tree_tests.cpp
+++ tests/core/tree_tests.cpp
@@ -1,9 +1,20 @@
 
-#include <core/tree.hpp>
+#include <sstream>
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <core/tree.hpp>
+
+template<typename T>
+std::string dump(const tree<T>& tr)
+{
+    std::stringstream s;
+
+    s << tr;
+
+    return s.str();
+}
 
 TEST(treeTest, isConstructible)
 {
@@ -34,14 +45,14 @@ TEST(treeTest, acceptsRootLevelInserts)
     tr.insert(tr.begin(), 3);
 
     EXPECT_EQ(3, tr.end() - tr.begin());
-    EXPECT_EQ("(3 2 1)", tr.dump());
+    EXPECT_EQ("(3 2 1)", dump<int>(tr));
 
     tr.insert(tr.end(), 4);
     tr.insert(tr.end(), 5);
     tr.insert(tr.end(), 6);
 
     EXPECT_EQ(6, tr.end() - tr.begin());
-    EXPECT_EQ("(3 2 1 4 5 6)", tr.dump());
+    EXPECT_EQ("(3 2 1 4 5 6)", dump<int>(tr));
 }
 
 
@@ -54,5 +65,5 @@ TEST(treeTest, acceptsChildLevelInserts)
     tr.insert(tr.begin().children_begin(), 3);
 
     EXPECT_EQ(2, tr.end() - tr.begin());
-    EXPECT_EQ("(1(3) 2)", tr.dump());
+    EXPECT_EQ("(1(3) 2)", dump<int>(tr));
 }
