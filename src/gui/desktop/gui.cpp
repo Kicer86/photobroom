603517911efa58f74ae015da534e054919eb7901
Michał Walenciak <Kicer86@gmail.com>
2015-02-20 13:57:40 +0100

    code format

--- src/gui/desktop/gui.cpp
+++ src/gui/desktop/gui.cpp
@@ -48,9 +48,9 @@ void Gui::set(ILogger* logger)
 
 void Gui::run(int argc, char **argv)
 {
-	// On Windows, add extra location for Qt plugins
+    // On Windows, add extra location for Qt plugins
 #ifdef OS_WIN
-	QCoreApplication::addLibraryPath(FileSystem().getLibrariesPath());
+    QCoreApplication::addLibraryPath(FileSystem().getLibrariesPath());
 #endif
 
     QApplication app(argc, argv);
