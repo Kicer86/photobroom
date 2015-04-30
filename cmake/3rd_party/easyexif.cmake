f79a99f4d28c311cc7a2c288e1bc8310a5e9cf03
Michał Walenciak <Kicer86@gmail.com>
2015-04-30 22:25:05 +0200

    cmake: resigning from AUTODOWNLOAD_EASYEXIF. Sources can be added manualy to 3rd_party dir in top source dir

--- cmake/3rd_party/easyexif.cmake
+++ cmake/3rd_party/easyexif.cmake
@@ -1,39 +1,12 @@
 
-# script for easyexif download & build
-# ExternalProject module could be used here, but it has serious drawback: 
-# stackoverflow.com/questions/17446981/cmake-externalproject-add-and-findpackage
-
-include(ExternalProject)
-
 function(addEasyExif)
 
-    find_package(Git)
+    add_library(easyexif STATIC ${CMAKE_SOURCE_DIR}/3rd_party/easyexif/exif.cpp)
 
-    if(GIT_FOUND)
-                              
-        #download easyexif rule
-        add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif_downloaded
-                           COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif
-                           COMMAND ${GIT_EXECUTABLE} clone https://github.com/mayanklahiri/easyexif.git
-                           COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif_downloaded
-                           DEPENDS ${CMAKE_BINARY_DIR}/3rd_party/src/dir
-                           WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/3rd_party/src)
+    if(MSVC)
 
-        set_source_files_properties(${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp PROPERTIES GENERATED TRUE)
-        
-        add_custom_target(download_easy_exif
-                          DEPENDS ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif_downloaded)
-        
-        add_library(easyexif STATIC ${CMAKE_BINARY_DIR}/3rd_party/src/easyexif/exif.cpp)
-        add_dependencies(easyexif download_easy_exif)
-        
-        if(MSVC)
-        
-        else()
-            set_target_properties(easyexif PROPERTIES COMPILE_FLAGS -fPIC)    #use fPIC as easyexif will be linked against dynamic objects
-        endif()
     else()
-        message(FATAL_ERROR "EasyEXIF cannot be downloaded. No git command found.")
+        set_target_properties(easyexif PROPERTIES COMPILE_FLAGS -fPIC)    #use fPIC as easyexif will be linked against dynamic objects
     endif()
 
 endfunction(addEasyExif)
