1badedb470a9ecd5f393f82730587b3e7c3ed2e0
Michał Walenciak <Kicer86@gmail.com>
2015-05-03 15:25:02 +0200

    tr: updating ts file

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -4,27 +4,27 @@
 <context>
     <name>BaseTags</name>
     <message>
-        <location filename="../src/core/implementation/base_tags.cpp" line="33"/>
+        <location filename="../src/core/implementation/base_tags.cpp" line="+33"/>
         <source>People</source>
         <translation>Osoby</translation>
     </message>
     <message>
-        <location filename="../src/core/implementation/base_tags.cpp" line="30"/>
+        <location line="-3"/>
         <source>Place</source>
         <translation>Miejsce</translation>
     </message>
     <message>
-        <location filename="../src/core/implementation/base_tags.cpp" line="31"/>
+        <location line="+1"/>
         <source>Date</source>
         <translation>Data</translation>
     </message>
     <message>
-        <location filename="../src/core/implementation/base_tags.cpp" line="32"/>
+        <location line="+1"/>
         <source>Time</source>
         <translation>Godzina</translation>
     </message>
     <message>
-        <location filename="../src/core/implementation/base_tags.cpp" line="29"/>
+        <location line="-3"/>
         <source>Event</source>
         <translation>Wydarzenie</translation>
     </message>
@@ -32,15 +32,67 @@
 <context>
     <name>IdxDataManager</name>
     <message>
-        <location filename="../src/gui/desktop/model_view/model_helpers/idx_data_manager.cpp" line="864"/>
+        <location filename="../src/gui/desktop/model_view/model_helpers/idx_data_manager.cpp" line="+864"/>
         <source>Unlabeled</source>
         <translation>Nieopisane</translation>
     </message>
 </context>
 <context>
+    <name>InfoGenerator</name>
+    <message>
+        <location filename="../src/gui/desktop/utils/info_generator.cpp" line="+130"/>
+        <source>No photo collection is opened.
+
+Use &apos;open&apos; action form &apos;Photo collection&apos; menu to choose one
+or &apos;new&apos; action and create new collection.</source>
+        <translation type="unfinished">Nie otwarto kolekcji zdjęć.
+
+Aby wybrać istniejącą kolekcję, wybierz akcję &quot;Otwórz&quot; z menu &quot;Kolekcja zdjęć&quot;,
+lub wybierz akcję &quot;Nowa&quot;, aby stworzyć nową kolekcję.</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>%1.
+
+All new photos are added to special area where they can be reviewed before they will be added to collection.
+To se those photos choose %2 and then %3
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Photos are being loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Photos waiting for review</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Windows</source>
+        <translation type="unfinished">Okna</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Staged area</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>There are no photos in your collection.
+
+Add some by choosing &apos;Add photos&apos; action from &apos;Photos&apos; menu.</source>
+        <translation type="unfinished">W twojej kolekcji nie ma żadnych zdjęć.
+
+Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdjęcia&quot;.</translation>
+    </message>
+</context>
+<context>
     <name>LevelEditor</name>
     <message>
-        <location filename="../src/gui/desktop/widgets/filters_editor/level_editor.cpp" line="208"/>
+        <location filename="../src/gui/desktop/widgets/filters_editor/level_editor.cpp" line="+208"/>
         <source>Grouping:</source>
         <translation>Grupowanie:</translation>
     </message>
@@ -48,93 +100,93 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="14"/>
+        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="+14"/>
         <source>Photo broom</source>
         <translation>Photo broom</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="56"/>
+        <location line="+42"/>
         <source>Photo collection</source>
         <translation>Kolekcja zdjęć</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="67"/>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="150"/>
+        <location line="+11"/>
+        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="+153"/>
         <source>Photos</source>
         <translation>Zdjęcia</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="74"/>
+        <location line="+7"/>
         <source>He&amp;lp</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="94"/>
+        <location line="+20"/>
         <source>Photo &amp;information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="103"/>
+        <location line="+9"/>
         <source>&amp;New</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="108"/>
+        <location line="+5"/>
         <source>&amp;Open</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="116"/>
+        <location line="+8"/>
         <source>&amp;Add photos</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="121"/>
+        <location line="+5"/>
         <source>&amp;About Photo Broom</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="126"/>
+        <location line="+5"/>
         <source>&amp;Close</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="131"/>
+        <location line="+5"/>
         <source>&amp;Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="136"/>
+        <location line="+5"/>
         <source>About &amp;Qt</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="141"/>
+        <location line="+5"/>
         <source>&amp;Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="157"/>
+        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="+7"/>
         <source>Staged photos</source>
         <translation>Zdjęcia do zatwierdzenia</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="203"/>
+        <location line="+42"/>
         <source>Photo broom: </source>
         <translation>Photo broom: </translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="203"/>
+        <location line="+0"/>
         <source>No collection opened</source>
         <translation>Nie otwarto kolekcji zdjęć</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="322"/>
+        <location line="+116"/>
         <source>Unsupported photo collection version</source>
         <translation>Nieobsługiwana wersja kolekcji</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="323"/>
+        <location line="+1"/>
         <source>Photo collection you are trying to open uses database in version which is not supported.
 It means your application is too old to open it.
 
@@ -145,7 +197,7 @@ Oznacza to, że twoja aplikacja jest zbyt stara aby ją otworzyć.
 Proszę zaktualizować aplikację.</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="333"/>
+        <location line="+10"/>
         <source>An unexpected error occured while opening photo collection.
 Please report a bug.
 Error code: </source>
@@ -154,41 +206,20 @@ Proszę zgłosić błąd w programie.
 Kod błędu: </translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="360"/>
         <source>No photo collection is opened.
 
 Use &apos;open&apos; action form &apos;Photo collection&apos; menu to choose one
 or &apos;new&apos; action and create new collection.</source>
-        <translation>Nie otwarto kolekcji zdjęć.
+        <translation type="vanished">Nie otwarto kolekcji zdjęć.
 
 Aby wybrać istniejącą kolekcję, wybierz akcję &quot;Otwórz&quot; z menu &quot;Kolekcja zdjęć&quot;,
 lub wybierz akcję &quot;Nowa&quot;, aby stworzyć nową kolekcję.</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="371"/>
-        <source>%1.
-
-All new photos are added to special area where they can be reviewed before they will be added to collection.
-To se those photos choose %2 and then %3
-</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="374"/>
-        <source>Photos are being loaded</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="374"/>
-        <source>Photos waiting for review</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="379"/>
         <source>There are no photos in your collection.
 
 Add some by choosing &apos;Add photos&apos; action from &apos;Photos&apos; menu.</source>
-        <translation type="unfinished">W twojej kolekcji nie ma żadnych zdjęć.
+        <translation type="obsolete">W twojej kolekcji nie ma żadnych zdjęć.
 
 Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdjęcia&quot;.</translation>
     </message>
@@ -199,7 +230,7 @@ Add some by choosing &apos;Add photos&apos; action from &apos;Photos&apos; menu.
 Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdjęcia&quot;.</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="332"/>
+        <location line="-1"/>
         <source>Unexpected error</source>
         <translation>Niespodziewany błąd</translation>
     </message>
@@ -208,7 +239,7 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
         <translation type="vanished">Pomoc</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="83"/>
+        <location filename="../src/gui/desktop/ui/mainwindow.ui" line="-58"/>
         <source>Windows</source>
         <translation>Okna</translation>
     </message>
@@ -229,7 +260,7 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
         <translation type="vanished">Dodaj zdjęcia</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="302"/>
+        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="-29"/>
         <source>About Photo Broom</source>
         <translation>O programie</translation>
     </message>
@@ -242,12 +273,12 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
         <translation type="vanished">Zakończ</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="308"/>
+        <location line="+6"/>
         <source>About Qt</source>
         <translation>O Qt</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="271"/>
+        <location line="-35"/>
         <source>Choose directory with photos</source>
         <translation>Wybierz katalog ze zdjęciami</translation>
     </message>
@@ -255,17 +286,17 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
 <context>
     <name>PhotosView</name>
     <message>
-        <location filename="../src/gui/desktop/ui/photos_view.ui" line="14"/>
+        <location filename="../src/gui/desktop/ui/photos_view.ui" line="+14"/>
         <source>Photos view</source>
         <translation>Podgląd zdjęć</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/photos_view.cpp" line="37"/>
+        <location filename="../src/gui/desktop/ui/photos_view.cpp" line="+37"/>
         <source>Date and time</source>
         <translation>Data i godzina</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/photos_view.cpp" line="38"/>
+        <location line="+1"/>
         <source>People</source>
         <translation>Osoby</translation>
     </message>
@@ -273,22 +304,22 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
 <context>
     <name>ProjectCreatorDialog</name>
     <message>
-        <location filename="../src/gui/desktop/widgets/project_creator/project_creator_dialog.cpp" line="62"/>
+        <location filename="../src/gui/desktop/widgets/project_creator/project_creator_dialog.cpp" line="+62"/>
         <source>Photo collection creator</source>
         <translation>Tworzenie kolekcji zdjęć</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/widgets/project_creator/project_creator_dialog.cpp" line="66"/>
+        <location line="+4"/>
         <source>Collection name:</source>
         <translation>Nazwa kolekcji:</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/widgets/project_creator/project_creator_dialog.cpp" line="75"/>
+        <location line="+9"/>
         <source>Database engine:</source>
         <translation>Silnik bazy danych:</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/widgets/project_creator/project_creator_dialog.cpp" line="84"/>
+        <location line="+9"/>
         <source>Engine options</source>
         <translation>Opcje silnika</translation>
     </message>
@@ -296,32 +327,32 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
 <context>
     <name>ProjectPicker</name>
     <message>
-        <location filename="../src/gui/desktop/ui/project_picker.ui" line="14"/>
+        <location filename="../src/gui/desktop/ui/project_picker.ui" line="+14"/>
         <source>Collections browser</source>
         <translation>Przeglądarka kolekcji</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/project_picker.ui" line="26"/>
+        <location line="+12"/>
         <source>New</source>
         <translation>Nowa</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/project_picker.ui" line="33"/>
+        <location line="+7"/>
         <source>Open</source>
         <translation>Otwórz</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/project_picker.ui" line="53"/>
+        <location line="+20"/>
         <source>Delete</source>
         <translation>Usuń</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/project_picker.cpp" line="83"/>
+        <location filename="../src/gui/desktop/ui/project_picker.cpp" line="+83"/>
         <source>Project deletion</source>
         <translation>Usunięcie projektu</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/project_picker.cpp" line="84"/>
+        <location line="+1"/>
         <source>Are you sure to remove project %1?</source>
         <translation>Czy na pewno usunąć projekt %1?</translation>
     </message>
@@ -329,7 +360,7 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/gui/desktop/widgets/tag_editor/helpers/tags_operator.cpp" line="61"/>
+        <location filename="../src/gui/desktop/widgets/tag_editor/helpers/tags_operator.cpp" line="+61"/>
         <source>&lt;multiple values&gt;</source>
         <translation>&lt;różne wartości&gt;</translation>
     </message>
@@ -337,12 +368,12 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
 <context>
     <name>StagedPhotosWidget</name>
     <message>
-        <location filename="../src/gui/desktop/widgets/staged_photos_widget.cpp" line="35"/>
+        <location filename="../src/gui/desktop/widgets/staged_photos_widget.cpp" line="+35"/>
         <source>Apply</source>
         <translation>Zastosuj</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/widgets/staged_photos_widget.cpp" line="41"/>
+        <location line="+6"/>
         <source>Changed photos:</source>
         <translation>Zmodyfikowane zdjęcia:</translation>
     </message>
@@ -350,12 +381,12 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
 <context>
     <name>TagsModel</name>
     <message>
-        <location filename="../src/gui/desktop/widgets/tag_editor/helpers/tags_model.cpp" line="124"/>
+        <location filename="../src/gui/desktop/widgets/tag_editor/helpers/tags_model.cpp" line="+124"/>
         <source>Name</source>
         <translation>Nazwa</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/widgets/tag_editor/helpers/tags_model.cpp" line="124"/>
+        <location line="+0"/>
         <source>Value</source>
         <translation>Wartość</translation>
     </message>
