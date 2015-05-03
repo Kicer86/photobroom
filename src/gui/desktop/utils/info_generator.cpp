92d601f49667b9d1960f3abed23a0c418c3effa3
Michał Walenciak <Kicer86@gmail.com>
2015-05-03 13:37:22 +0200

    gui: utils: InfoGenerator: reading count of photos in each of containers

--- src/gui/desktop/utils/info_generator.cpp
+++ src/gui/desktop/utils/info_generator.cpp
@@ -23,6 +23,35 @@
 
 #include "signal_filter.hpp"
 
+namespace std
+{
+    template<typename T, typename... Args>
+    std::unique_ptr<T> make_unique(Args... args)
+    {
+        return std::unique_ptr<T>( new T(args...) );
+    }
+}
+
+namespace
+{
+    struct CollectionPhotos: Database::AGetPhotosCount
+    {
+        virtual void got(int c)
+        {
+
+        }
+    };
+
+
+    struct StagedAreaPhotos: Database::AGetPhotosCount
+    {
+        virtual void got(int c)
+        {
+
+        }
+    };
+}
+
 
 InfoGenerator::InfoGenerator(QObject* parent_object): QObject(parent_object), m_database(nullptr), m_signalFiler(new SignalFilter(this))
 {
@@ -48,5 +77,51 @@ void InfoGenerator::set(Database::IDatabase* database)
 
 void InfoGenerator::dbChanged()
 {
+    auto stagedAreaPhotos = std::make_unique<StagedAreaPhotos>();
+    auto collectionPhotos = std::make_unique<CollectionPhotos>();
 
+    auto stagedAreaPhotosFilter = std::make_shared<Database::FilterPhotosWithFlags>();
+    auto collectionPhotosFilter = std::make_shared<Database::FilterPhotosWithFlags>();
+
+    stagedAreaPhotosFilter->flags[IPhotoInfo::FlagsE::StagingArea] = 1;
+    collectionPhotosFilter->flags[IPhotoInfo::FlagsE::StagingArea] = 0;
+
+    m_database->exec(std::move(stagedAreaPhotos), {stagedAreaPhotosFilter});
+    m_database->exec(std::move(collectionPhotos), {collectionPhotosFilter});
+
+
+    /*
+    QString infoText;
+
+    if (m_currentPrj.get() == nullptr)
+        infoText = tr("No photo collection is opened.\n\n"
+        "Use 'open' action form 'Photo collection' menu to choose one\n"
+        "or 'new' action and create new collection.");
+
+    const bool photos_in_staging_area = m_stagedImagesModel->isEmpty() == false;
+    const bool photos_in_images_area  = m_imagesModel->isEmpty() == false;
+    const bool photos_collector_works = m_photosCollector->isWorking();
+
+    const bool state_photos_for_review = photos_in_images_area == false && (photos_in_staging_area || photos_collector_works);
+
+    if (infoText.isEmpty() && state_photos_for_review)
+        infoText = tr("%1.\n\n"
+        "All new photos are added to special area where they can be reviewed before they will be added to collection.\n"
+        "To se those photos choose %2 and then %3\n")
+        .arg(photos_collector_works? tr("Photos are being loaded"): tr("Photos waiting for review"))
+        .arg(ui->menuWindows->title())
+        .arg(m_views[1]->getName());
+
+    if (infoText.isEmpty() && m_imagesModel->isEmpty() == 0)
+        infoText = tr("There are no photos in your collection.\n\nAdd some by choosing 'Add photos' action from 'Photos' menu.");
+
+    if (infoText.isEmpty() == false)
+        ui->infoWidget->setText(infoText);
+
+    if (infoText.isEmpty() && ui->infoWidget->isVisible())
+        ui->infoWidget->hide();
+
+    if (infoText.isEmpty() == false && ui->infoWidget->isHidden())
+        ui->infoWidget->show();
+    */
 }
