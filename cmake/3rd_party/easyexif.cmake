1f58c61054dc5201faa5d4dd4ee8905c8d97161f
Michał Walenciak <Kicer86@gmail.com>
2015-05-05 10:45:48 +0200

    easyexif: nicer way of PIC turn on

--- cmake/3rd_party/easyexif.cmake
+++ cmake/3rd_party/easyexif.cmake
@@ -1,12 +1,7 @@
 
 function(addEasyExif)
 
-    add_library(easyexif STATIC ${CMAKE_SOURCE_DIR}/3rd_party/easyexif/exif.cpp)
-
-    if(MSVC)
-
-    else()
-        set_target_properties(easyexif PROPERTIES COMPILE_FLAGS -fPIC)    #use fPIC as easyexif will be linked against dynamic objects
-    endif()
+    add_library(easyexif STATIC ${CMAKE_SOURCE_DIR}/3rd_party/easyexif/exif.cpp)       
+    set_target_properties(easyexif PROPERTIES POSITION_INDEPENDENT_CODE ON)
 
 endfunction(addEasyExif)
