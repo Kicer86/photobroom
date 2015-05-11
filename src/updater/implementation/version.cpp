501151f62143cbb77eb647f5cb370365a08a6cf1
Michał Walenciak <Kicer86@gmail.com>
2015-05-11 22:40:47 +0200

    updater: introducing Version class

---
+++ src/updater/implementation/version.cpp
@@ -0,0 +1,37 @@
+/*
+ * Release version
+ * Copyright (C) 2015  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "version.hpp"
+
+Version::Version(): m_version()
+{
+
+}
+
+
+Version::~Version()
+{
+
+}
+
+
+bool Version::operator==(const Version& other) const
+{
+    return m_version == other.m_version;
+}
