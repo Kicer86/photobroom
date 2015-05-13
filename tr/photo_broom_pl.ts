e91f35c758094f9365b6871fc4266b82a5b6df91
Michał Walenciak <Kicer86@gmail.com>
2015-05-13 18:13:06 +0200

    tr: updating ts file

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -132,7 +132,7 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
         <translation type="vanished">Kolekcja zdjęć</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="+160"/>
+        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="+196"/>
         <source>Photos</source>
         <translation>Zdjęcia</translation>
     </message>
@@ -207,17 +207,28 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
         <translation>&amp;Pomoc</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="-62"/>
+        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="-78"/>
         <source>New version</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+0"/>
+        <location line="+1"/>
         <source>New version of PhotoBroom is available &lt;a href=&quot;%1&quot;&gt;here&lt;/a&gt;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+69"/>
+        <location line="+7"/>
+        <source>Internet connection problem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Could not check if there is new version of PhotoBroom.
+Please check your internet connection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+76"/>
         <source>Staged photos</source>
         <translation>Zdjęcia do zatwierdzenia</translation>
     </message>
