02d1c59e1b34d2b30ea3a7ec64aa2473c05431cf
Michał Walenciak <Kicer86@gmail.com>
2015-07-11 13:31:45 +0200

    simplifying threads usage

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -392,7 +392,7 @@ Aby dodać zdjęcia, wybierz akcję &quot;Dodaj zdjęcia&quot; z menu &quot;Zdj�
 <context>
     <name>PhotosAnalyzerImpl</name>
     <message>
-        <location filename="../src/database/database_tools/implementation/photos_analyzer.cpp" line="+227"/>
+        <location filename="../src/database/database_tools/implementation/photos_analyzer.cpp" line="+222"/>
         <source>Loading photos data...</source>
         <translation>Wczytywanie danych ze zdjęć...</translation>
     </message>
