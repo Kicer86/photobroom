b34ca8b8ea77ffea1d67b1335e7d7e65bccac3bd
Michał Walenciak <Kicer86@gmail.com>
2015-07-19 22:07:08 +0200

    gui: ui: introducing PhotosAddDialog

---
+++ src/gui/desktop/ui/photos_add_dialog.hpp
@@ -0,0 +1,35 @@
+/*
+ * Dialog for photos adding.
+ * Copyright (C) 2015  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PHOTOSADDDIALOG_HPP
+#define PHOTOSADDDIALOG_HPP
+
+#include <qt/QtWidgets/QDialog>
+
+class PhotosAddDialog : public QDialog
+{
+    public:
+        PhotosAddDialog();
+        PhotosAddDialog(const PhotosAddDialog &) = delete;
+        ~PhotosAddDialog();
+
+        PhotosAddDialog& operator=(const PhotosAddDialog &) = delete;
+};
+
+#endif // PHOTOSADDDIALOG_H
