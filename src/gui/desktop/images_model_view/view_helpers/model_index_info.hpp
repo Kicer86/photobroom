ad38f68a32c8daa28921c09853323f987e280897
Michał Walenciak <Kicer86@gmail.com>
2015-08-15 21:20:15 +0200

    code cleanup

--- src/gui/desktop/images_model_view/view_helpers/model_index_info.hpp
+++ src/gui/desktop/images_model_view/view_helpers/model_index_info.hpp
@@ -33,9 +33,9 @@ struct ModelIndexInfo
         void setSize(const QSize &);
         void setRect(const QRect& r);
         void setOverallSize(const QSize& r);
+
         const QRect getRect() const;
         const QSize& getOverallSize() const;
-
         const QPoint& getPosition() const;
         const QSize& getSize() const;
 
