9dedbc1223f56d310c13867aa25807142269961d
Michał Walenciak <Kicer86@gmail.com>
2015-08-30 13:08:39 +0200

    gui: introducing new config keys

--- src/gui/desktop/config_keys.hpp
+++ src/gui/desktop/config_keys.hpp
@@ -7,5 +7,10 @@ namespace ViewConfigKeys
     const char* const itemsSpacing = "view::spacing";
 }
 
+namespace UpdateConfigKeys
+{
+    const char* const updateFrequency = "updater::frequency";
+    const char* const updateEnabled   = "updater::enabled";
+}
 
 #endif // CONFIG_KEYS_HPP
