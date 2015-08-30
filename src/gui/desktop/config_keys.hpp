7be872f06065de3f49d9999106579ff69a85ba9d
Michał Walenciak <Kicer86@gmail.com>
2015-08-30 13:32:54 +0200

    gui: config_keys: new key

--- src/gui/desktop/config_keys.hpp
+++ src/gui/desktop/config_keys.hpp
@@ -11,6 +11,7 @@ namespace UpdateConfigKeys
 {
     const char* const updateFrequency = "updater::frequency";
     const char* const updateEnabled   = "updater::enabled";
+    const char* const lastCheck       = "updater::last_check";
 }
 
 #endif // CONFIG_KEYS_HPP
