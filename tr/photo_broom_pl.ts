43c8dc94f5b3f9ba3a82612dd86088b393411797
Michał Walenciak <Kicer86@gmail.com>
2015-12-09 23:14:06 +0100

    tr: ts file update

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -40,7 +40,7 @@
 <context>
     <name>IdxDataManager</name>
     <message>
-        <location filename="../src/gui/desktop/models/model_helpers/idx_data_manager.cpp" line="+910"/>
+        <location filename="../src/gui/desktop/models/model_helpers/idx_data_manager.cpp" line="+918"/>
         <source>Unlabeled</source>
         <translation>Nieopisane</translation>
     </message>
