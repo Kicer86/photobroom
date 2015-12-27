d145a6c5bd8ea8eac2a445a87be502cc1bc9a28d
Michał Walenciak <Kicer86@gmail.com>
2015-12-27 23:36:33 +0100

    cmake: paths cleanups in breakpad's finder

--- cmake/FindBreakpad.cmake
+++ cmake/FindBreakpad.cmake
@@ -5,25 +5,25 @@ if(EXISTS ${CMAKE_SOURCE_DIR}/3rd_party/breakpad)
 
     if (NOT TARGET breakpad)
 
-        add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/3rd_party/breakpad/src/libbreakpad.a
+        add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/3rd_party/breakpad/install/usr/lib/libbreakpad.a
 
-                           COMMAND ${CMAKE_SOURCE_DIR}/3rd_party/breakpad/configure
+                           COMMAND ${CMAKE_SOURCE_DIR}/3rd_party/breakpad/configure --prefix=/usr
                            COMMAND make
-                           COMMAND make install DESTDIR=${CMAKE_BINARY_DIR}/3rd_party/breakpad/usr
+                           COMMAND make install DESTDIR=${CMAKE_BINARY_DIR}/3rd_party/breakpad/install
 
                            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/3rd_party/breakpad
                           )
 
         add_custom_target(breakpad_build
-                          DEPENDS ${CMAKE_BINARY_DIR}/3rd_party/breakpad/src/libbreakpad.a)
+                          DEPENDS ${CMAKE_BINARY_DIR}/3rd_party/breakpad/install/usr/lib/libbreakpad.a)
 
         add_library(breakpad STATIC IMPORTED)
-        set_target_properties(breakpad PROPERTIES IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/3rd_party/breakpad/src/libbreakpad.a)
+        set_target_properties(breakpad PROPERTIES IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/3rd_party/breakpad/install/usr/lib/libbreakpad.a)
         add_dependencies(breakpad breakpad_build)
 
     endif()
 
-    set(BREAKPAD_INCLUDE_DIR ${CMAKE_BINARY_DIR}/3rd_party/breakpad/usr/include)
+    set(BREAKPAD_INCLUDE_DIR ${CMAKE_BINARY_DIR}/3rd_party/breakpad/install/usr/include)
 
 endif()
 
