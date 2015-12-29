f28f87a3be96b7b0f78bbe1495622b48cedf09a3
Michał Walenciak <Kicer86@gmail.com>
2015-12-29 23:27:33 +0100

    system: code format

--- src/system/implementation/crash_catcher_win32.cpp
+++ src/system/implementation/crash_catcher_win32.cpp
@@ -18,13 +18,14 @@
 namespace
 {
 
-    void   LogStackFrames(void* FaultAddress, char *eNextBP)
+    void LogStackFrames(void* FaultAddress, char* eNextBP)
     {
-        char *p, *pBP;
+        char* p, *pBP;
         unsigned i, x, BpPassed;
         static int  CurrentlyInTheStackDump = 0;
 
         BpPassed = (eNextBP != nullptr);
+
         if (eNextBP != nullptr)
         {
             asm
@@ -37,26 +38,27 @@ namespace
             printf("\n  Fault Occurred At $ADDRESS:%08LX\n", (int)FaultAddress);
 
         // prevent infinite loops
-        for(i = 0; eNextBP && i < 100; i++)
+        for (i = 0; eNextBP && i < 100; i++)
         {
             pBP = eNextBP;           // keep current BasePointer
-            eNextBP = *(char **)pBP; // dereference next BP
+            eNextBP = *(char**)pBP;  // dereference next BP
             p = pBP + 8;
             // Write 20 Bytes of potential arguments
             printf("         with ");
 
-            for(x = 0; p < eNextBP && x < 20; p++, x++)
-                printf("%02X ", *(unsigned char *)p);
+            for (x = 0; p < eNextBP && x < 20; p++, x++)
+                printf("%02X ", *(unsigned char*)p);
 
             printf("\n\n");
 
-            if(i == 1 && ! BpPassed)
-               printf("*************************************\n"
-                                 "         Fault Occurred Here:\n");
-            // Write the backjump address
-            printf("*** %2d called from $ADDRESS:%08LX\n", i, *(char **)(pBP + 4));
+            if (i == 1 && ! BpPassed)
+                printf("*************************************\n"
+                       "         Fault Occurred Here:\n");
 
-            if(*(char **)(pBP + 4) == NULL)
+            // Write the backjump address
+            printf("*** %2d called from $ADDRESS:%08LX\n", i, *(char**)(pBP + 4));
+
+            if (*(char**)(pBP + 4) == NULL)
                 break;
         }
 
@@ -70,31 +72,31 @@ namespace
         crashReport << std::endl;
 
         unsigned int   i;
-        void         * stack[ 100 ];
+        void*          stack[ 100 ];
         unsigned short frames;
-        SYMBOL_INFO  * symbol;
+        SYMBOL_INFO*   symbol;
         HANDLE         process;
 
         process = GetCurrentProcess();
 
-        SymInitialize( process, NULL, TRUE );
+        SymInitialize(process, NULL, TRUE);
 
-        frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
-        symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
+        frames               = CaptureStackBackTrace(0, 100, stack, NULL);
+        symbol               = (SYMBOL_INFO*)calloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char), 1);
         symbol->MaxNameLen   = 255;
-        symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
+        symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
 
-        for( i = 0; i < frames; i++ )
+        for (i = 0; i < frames; i++)
         {
-            SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
+            SymFromAddr(process, (DWORD64)(stack[ i ]), 0, symbol);
 
             crashReport << frames - i - 1 << ": " << symbol->Name << " - " << std::hex << symbol->Address << std::endl;
         }
 
-        free( symbol );
+        free(symbol);
 
         LogStackFrames(ExInfo->ExceptionRecord->ExceptionAddress,
-                       (char *)ExInfo->ContextRecord->Ebp);
+                       (char*)ExInfo->ContextRecord->Ebp);
 
         CrashCatcher::saveOutput(crashReport.str());
     }
@@ -102,7 +104,7 @@ namespace
 
     LONG WINAPI sig_handler(EXCEPTION_POINTERS* ExceptionInfo)
     {
-        switch(ExceptionInfo->ExceptionRecord->ExceptionCode)
+        switch (ExceptionInfo->ExceptionRecord->ExceptionCode)
         {
             case EXCEPTION_ACCESS_VIOLATION:
             case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
@@ -115,21 +117,25 @@ namespace
             case EXCEPTION_FLT_STACK_CHECK:
             case EXCEPTION_FLT_UNDERFLOW:
             case EXCEPTION_ILLEGAL_INSTRUCTION:
-            //case EXCEPTION_IN_PAGE_ERROR:
+
+                //case EXCEPTION_IN_PAGE_ERROR:
             case EXCEPTION_INT_DIVIDE_BY_ZERO:
             case EXCEPTION_INT_OVERFLOW:
-            //case EXCEPTION_INVALID_DISPOSITION:
+
+                //case EXCEPTION_INVALID_DISPOSITION:
             case EXCEPTION_NONCONTINUABLE_EXCEPTION:
             case EXCEPTION_PRIV_INSTRUCTION:
-            //case EXCEPTION_SINGLE_STEP:
+
+                //case EXCEPTION_SINGLE_STEP:
             case EXCEPTION_STACK_OVERFLOW:
                 bt(ExceptionInfo);
                 exit(1);
-            break;
+                break;
 
-            default:
-              fputs("Error: Unrecognized Exception\n", stderr);
-              break;
+            default
+                    :
+                fputs("Error: Unrecognized Exception\n", stderr);
+                break;
         }
 
         return EXCEPTION_EXECUTE_HANDLER;
@@ -141,6 +147,6 @@ void CrashCatcher::internal_init()
 {
     SetUnhandledExceptionFilter(sig_handler);
 
-    int *a = nullptr;
+    int* a = nullptr;
     volatile int b = *a;
 }
