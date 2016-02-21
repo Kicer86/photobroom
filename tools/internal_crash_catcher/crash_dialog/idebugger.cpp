0ab23d6053c951d42b137dfe891fce5aa8b80993
Michał Walenciak <Kicer86@gmail.com>
2016-02-21 21:35:57 +0100

    tools: crash catcher: introducing IDebugger

---
+++ tools/internal_crash_catcher/crash_dialog/idebugger.cpp
@@ -0,0 +1,8 @@
+
+#include "idebugger.hpp"
+
+
+IDebugger::~IDebugger()
+{
+
+}
---
+++ tools/internal_crash_catcher/crash_dialog/idebugger.hpp
@@ -0,0 +1,13 @@
+
+#ifndef IDEBUGGER_HPP
+#define IDEBUGGER_HPP
+
+
+struct IDebugger
+{
+    virtual ~IDebugger();
+
+};
+
+#endif // IDEBUGGER_HPP
+
