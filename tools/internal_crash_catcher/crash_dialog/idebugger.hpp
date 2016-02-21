db7b0ee45342960401d85dff769a20883a5786b3
Michał Walenciak <Kicer86@gmail.com>
2016-02-21 21:40:37 +0100

    tools: crash catcher: initial interface definition for IDebugger

--- tools/internal_crash_catcher/crash_dialog/idebugger.hpp
+++ tools/internal_crash_catcher/crash_dialog/idebugger.hpp
@@ -2,11 +2,15 @@
 #ifndef IDEBUGGER_HPP
 #define IDEBUGGER_HPP
 
+#include <string>
+#include <vector>
 
 struct IDebugger
 {
     virtual ~IDebugger();
 
+    virtual bool attach(qint64 pid, qint64 tid) = 0;
+    virtual std::vector<std::string> getBackTrace() = 0;
 };
 
 #endif // IDEBUGGER_HPP
