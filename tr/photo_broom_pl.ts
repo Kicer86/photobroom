895fd4652b850cb453736daa30843145dbaac27f
Michał Walenciak <Kicer86@gmail.com>
2016-04-09 21:39:35 +0200

    tr: ts update

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -32,7 +32,7 @@
 <context>
     <name>CollectionDirScanDialog</name>
     <message>
-        <location filename="../src/gui/desktop/widgets/collection_dir_scan_dialog.cpp" line="+112"/>
+        <location filename="../src/gui/desktop/widgets/collection_dir_scan_dialog.cpp" line="+118"/>
         <source>Collection scan in progress</source>
         <translation type="unfinished"></translation>
     </message>
