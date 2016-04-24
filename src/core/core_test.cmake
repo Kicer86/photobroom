67e885dad7faa016e51c641db361451972d6e582
Michał Walenciak <Kicer86@gmail.com>
2016-04-24 21:09:05 +0200

    moving core tests to core

--- src/core/core_test.cmake
+++ src/core/core_test.cmake
@@ -4,18 +4,14 @@ include(${CMAKE_SOURCE_DIR}/cmake/functions.cmake)
 find_package(Threads REQUIRED)
 find_package(GMock REQUIRED)
 find_package(GTest REQUIRED)
-find_package(Qt5Core REQUIRED)
-find_package(Qt5Gui REQUIRED)
 
-
-set(BASE_DIR ${CMAKE_SOURCE_DIR}/src/core)
-setupTestEnvironmentFor(core ${BASE_DIR})
+include_directories(SYSTEM ${GMOCK_INCLUDE_DIRS} ${GTEST_INCLUDE_DIRS})
 
 include_directories($<TARGET_PROPERTY:core,INTERFACE_INCLUDE_DIRECTORIES>)
 
 set(SRC
-        tree_tests.cpp
-        ts_multi_head_queue_tests.cpp
+        unit_tests/tree_tests.cpp
+        unit_tests/ts_multi_head_queue_tests.cpp
     )
 
 add_executable(core_tests ${SRC})
@@ -24,4 +20,3 @@ target_link_libraries(core_tests PRIVATE ${GMOCK_MAIN_LIBRARY} ${GMOCK_LIBRARY} 
 enableCodeCoverage(core_tests)
 
 add_test(core core_tests)
-
