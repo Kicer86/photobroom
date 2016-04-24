946c12918e42fb61a164d86e3d73123fc66284a2
Michał Walenciak <Kicer86@gmail.com>
2016-04-24 21:26:17 +0200

    gui: tests: try not to crash when there is no display :)

--- src/gui/unit_tests/main.cpp
+++ src/gui/unit_tests/main.cpp
@@ -5,6 +5,14 @@
 
 int main(int argc, char** argv)
 {
+#ifdef Q_OS_LINUX
+    // gui cannot be tested when there is no graphical environment
+
+    const char* env_p = std::getenv("DISPLAY");
+    if (strcmp(env_p, "") == 0)
+        return 0;
+#endif
+
     QGuiApplication app(argc, argv);
     ::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
