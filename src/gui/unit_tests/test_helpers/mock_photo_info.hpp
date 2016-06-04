a6b16046700743f0e7201680f2cdcac45d9f6f83
Michał Walenciak <Kicer86@gmail.com>
2016-06-04 08:31:53 +0200

    gui: unit tests: resurrecting MockPhotoInfo

--- src/gui/unit_tests/test_helpers/mock_photo_info.hpp
+++ src/gui/unit_tests/test_helpers/mock_photo_info.hpp
@@ -10,32 +10,31 @@
 // trick with proxies: http://stackoverflow.com/questions/7616475/can-google-mock-a-method-with-a-smart-pointer-return-type
 struct MockPhotoInfo: IPhotoInfo
 {
+    MOCK_CONST_METHOD0(data, Photo::Data());
     MOCK_CONST_METHOD0(getPath, const QString());
     MOCK_CONST_METHOD0(getTags, const Tag::TagsList());
-    MOCK_CONST_METHOD0(getThumbnail, const QImage());
+    MOCK_CONST_METHOD0(getGeometry, const QSize());
     MOCK_CONST_METHOD0(getSha256, const Photo::Sha256sum());
     MOCK_CONST_METHOD0(getID, Photo::Id());
 
     MOCK_CONST_METHOD0(isFullyInitialized, bool());
     MOCK_CONST_METHOD0(isSha256Loaded, bool());
-    MOCK_CONST_METHOD0(isThumbnailLoaded, bool());
+    MOCK_CONST_METHOD0(isGeometryLoaded, bool());
     MOCK_CONST_METHOD0(isExifDataLoaded, bool());
 
     MOCK_METHOD1(registerObserver, void(IObserver *));
     MOCK_METHOD1(unregisterObserver, void(IObserver *));
 
-    MOCK_METHOD1(initSha256, void(const Photo::Sha256sum &));
-    MOCK_METHOD1(initThumbnail, void(const QImage &));
-    MOCK_METHOD1(initID, void(const Photo::Id &));
-
+    MOCK_METHOD1(setSha256, void(const Photo::Sha256sum &));
     MOCK_METHOD1(setTags, void(const Tag::TagsList &));
+    MOCK_METHOD2(setTag, void(const TagNameInfo &, const TagValue &));
+    MOCK_METHOD1(setGeometry, void(const QSize &));
 
-    MOCK_METHOD1(markStagingArea, void(bool));
-    MOCK_METHOD1(markExifDataLoaded, void(bool));
-    MOCK_CONST_METHOD0(getFlags, Photo::FlagsE());
+    MOCK_METHOD2(markFlag, void(Photo::FlagsE, int));
+    MOCK_CONST_METHOD1(getFlag, int(Photo::FlagsE));
 
-    //proxies
-    MOCK_METHOD0(accessTagsProxy, Tag::TagsList*());
+    MOCK_METHOD0(invalidate, void());
+    MOCK_METHOD0(isValid, bool());
 
     //data
     std::mutex m_tagAccessMutex;
