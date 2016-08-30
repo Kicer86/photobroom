ec2db75ed1a89e383d02b58bd3b9886e10a47b37
Michał Walenciak <Kicer86@gmail.com>
2016-08-30 23:43:28 +0200

    core: Logger: introducing LoggerStream

--- src/core/ilogger.hpp
+++ src/core/ilogger.hpp
@@ -2,7 +2,9 @@
 #ifndef ILOG_HPP
 #define ILOG_HPP
 
-class QString;
+#include <sstream>
+
+#include <QString>
 
 struct ILogger
 {
@@ -22,6 +24,29 @@ struct ILogger
     virtual void warning(const QString &) = 0;
     virtual void error(const QString &) = 0;
     virtual void debug(const QString &) = 0;
+};
+
+
+template<ILogger::Severity severity>
+class LoggerStream: std::stringbuf, public std::ostream
+{
+    public:
+        LoggerStream(ILogger* logger): std::stringbuf(), std::ostream(this), m_logger(logger)
+        {
+
+        }
+
+        LoggerStream(const LoggerStream<severity> &) = delete;
+        LoggerStream& operator=(const LoggerStream<severity> &) = delete;
+
+        ~LoggerStream()
+        {
+            std::string str = std::stringbuf::str();
+            m_logger->log(severity, str.c_str());
+        }
+
+    private:
+        ILogger* m_logger;
 };
 
 #endif
