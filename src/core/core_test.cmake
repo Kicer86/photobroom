b5545fc003cb3546067c7019fef7e10bbccf6607
Michał Walenciak <Kicer86@gmail.com>
2016-09-13 21:56:42 +0200

    cmake: core: cleanups

--- src/core/core_test.cmake
+++ src/core/core_test.cmake
@@ -14,7 +14,14 @@ set(SRC
 
 add_executable(core_tests ${SRC})
 
-target_link_libraries(core_tests PRIVATE ${GMOCK_MAIN_LIBRARY} ${GMOCK_LIBRARY} ${CMAKE_THREAD_LIBS_INIT})
+target_link_libraries(core_tests 
+                        PRIVATE 
+                            ${GMOCK_MAIN_LIBRARY} 
+                            ${GMOCK_LIBRARY} 
+                            ${CMAKE_THREAD_LIBS_INIT}
+)
+
+
 enableCodeCoverage(core_tests)
 
 add_test(core core_tests)
