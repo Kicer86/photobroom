251c112f0f72b4dea7e28a24fb746d054039f13f
Michał Walenciak <Kicer86@gmail.com>
2016-12-02 23:22:07 +0100

    code format

--- src/gui/desktop/ui/photos_grouping_dialog.hpp
+++ src/gui/desktop/ui/photos_grouping_dialog.hpp
@@ -10,14 +10,14 @@ class PhotosGroupingDialog;
 
 class PhotosGroupingDialog : public QDialog
 {
-    Q_OBJECT
+        Q_OBJECT
 
-public:
-    explicit PhotosGroupingDialog(QWidget *parent = 0);
-    ~PhotosGroupingDialog();
+    public:
+        explicit PhotosGroupingDialog(QWidget *parent = 0);
+        ~PhotosGroupingDialog();
 
-private:
-    Ui::PhotosGroupingDialog *ui;
+    private:
+        Ui::PhotosGroupingDialog *ui;
 };
 
 #endif // PHOTOS_GROUPING_DIALOG_HPP
