7f6adf24892e50a0349d8b4066c98d3c800fc172
Michał Walenciak <Kicer86@gmail.com>
2016-12-27 10:02:57 +0100

    appveyot: trying to fix cmake search PATH

--- appveyor.bat
+++ appveyor.bat
@@ -9,7 +9,7 @@ if "%platform%"=="Win32" (
     set qt_arch=msvc2015_64
 )
 
-set PATH="C:\Qt\5.6\%qt_arch%\bin;C:\Program Files\CMake\bin;%PATH%"
+set PATH=C:\Qt\5.6\%qt_arch%\bin;C:\Program Files\CMake\bin;%PATH%
 set CMAKE_PREFIX_PATH=C:/Qt/5.6/%qt_arch%;C:\Libraries\boost;c:/projects/install
 set GTEST_PATH=c:\projects\googletest
 
