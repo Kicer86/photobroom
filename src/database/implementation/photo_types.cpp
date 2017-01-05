e0f1aa5785af44cab347fdfca9643af3b6572838
Michał Walenciak <Kicer86@gmail.com>
2017-01-05 23:23:00 +0100

    database: Photo types: switching to generic id

--- src/database/implementation/photo_types.cpp
+++ src/database/implementation/photo_types.cpp
@@ -4,39 +4,4 @@
 namespace Photo
 {
 
-    Id::Id(): m_value(-1), m_valid(false)
-    {
-
-    }
-
-
-    Id::Id(Id::type v): m_value(v), m_valid(true)
-    {
-
-    }
-
-
-    bool Id::operator!() const
-    {
-        return !m_valid;
-    }
-
-
-    Id::operator Id::type() const
-    {
-        return m_value;
-    }
-
-
-    bool Id::valid() const
-    {
-        return m_valid;
-    }
-
-
-    Id::type Id::value() const
-    {
-        return m_value;
-    }
-    
 }
