703d10d4843f6652b845b63b5702bc1f13b6dc37
Michał Walenciak <Kicer86@gmail.com>
2017-01-15 22:35:22 +0100

    core: ptr_iterator - using concepts

--- src/core/ptr_iterator.hpp
+++ src/core/ptr_iterator.hpp
@@ -8,7 +8,68 @@
 #ifndef PTR_ITERATOR_HPP
 #define PTR_ITERATOR_HPP
 
+#include <type_traits>
+
+
 template<typename T>
+concept bool SmartPointer()
+{
+    return requires(T p)
+    {
+        { p.operator*() };
+        { p.get() };
+        { std::is_pointer<decltype(*p)>::value == true };
+    };
+}
+
+
+template<typename T>
+concept bool Iterator()
+{
+    return requires(T i)
+    {
+        typename T::iterator_category;
+        typename T::value_type;
+        typename T::difference_type;
+        typename T::pointer;
+        typename T::reference;
+
+        { i.operator*() };
+        { i.operator->() };
+    };
+}
+
+
+template<typename T>
+concept bool Container()
+{
+    return requires(T p)
+    {
+        typename T::const_iterator;
+
+        requires Iterator<typename T::const_iterator>();
+        requires Iterator<typename T::iterator>();
+
+        { p.begin() };
+        { p.end()   };
+
+    };
+}
+
+
+template<typename T>
+concept bool SmartPointerContainer()
+{
+    return requires(T p)
+    {
+        requires Container<T>();
+
+        { *p.begin() } -> SmartPointer;
+    };
+}
+
+
+template<SmartPointerContainer T>
 class ptr_iterator: public T::const_iterator
 {
     public:
