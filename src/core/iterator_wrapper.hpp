ac109fb018d5cacbd5ba67b1bc24b90ea55d54da
Michał Walenciak <Kicer86@gmail.com>
2017-01-29 13:59:37 +0100

    core: base iterator_wrapper implementation

--- src/core/iterator_wrapper.hpp
+++ src/core/iterator_wrapper.hpp
@@ -1,1 +1,25 @@
- 
+
+#ifndef ITERATOR_WRAPPER_HPP
+#define ITERATOR_WRAPPER_HPP
+
+template<typename R, typename B, typename T>
+struct iterator_wrapper: B
+{
+    iterator_wrapper()
+    {
+    }
+
+    ~iterator_wrapper()
+    {
+    }
+
+    R operator*() const
+    {
+        return m_operation(this);
+    }
+
+    private:
+        T m_operation;
+};
+
+#endif
