a9028b7a1b9e66d71103c1ee7a7b80246dad2349
Michał Walenciak <Kicer86@gmail.com>
2017-03-12 22:47:21 +0100

    system: add icons' path to path.hpp

--- src/system/paths_win.hpp.cmake
+++ src/system/paths_win.hpp.cmake
@@ -4,4 +4,5 @@ namespace Paths
     const char* const plugins = "@BASE_PATH@@PATH_PLUGINS@";
     const char* const translations = "@BASE_PATH@@PATH_TR@";
     const char* const libraries = "@BASE_PATH@@PATH_LIBS@";
+    const char* const icons = "@BASE_PATH@@PATH_ICONS@";
 }
