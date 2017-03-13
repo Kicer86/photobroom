a6f107b2677a6090a325dbeb6fc1945c8d2d7644
Michał Walenciak <Kicer86@gmail.com>
2017-03-13 22:23:17 +0100

    cmake linux: install desktop file

--- cmake/system/linux.cmake
+++ cmake/system/linux.cmake
@@ -1,3 +1,6 @@
 
 # install main icon in public area
 install(FILES ${CMAKE_SOURCE_DIR}/images/photo_broom.svg DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/scalable/apps)
+
+#install desktop file
+install(FILES ${CMAKE_SOURCE_DIR}/src/env/photo_broom.desktop DESTINATION ${CMAKE_INSTALL_PREFIX}/share/applications)
