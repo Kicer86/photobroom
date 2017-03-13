289fd2720b95b77529eb267f6569f120b59d3536
Michał Walenciak <Kicer86@gmail.com>
2017-03-13 22:32:16 +0100

    moving desktop file to resources/linux

--- cmake/system/linux.cmake
+++ cmake/system/linux.cmake
@@ -3,4 +3,4 @@
 install(FILES ${CMAKE_SOURCE_DIR}/images/photo_broom.svg DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/scalable/apps)
 
 #install desktop file
-install(FILES ${CMAKE_SOURCE_DIR}/src/env/photo_broom.desktop DESTINATION ${CMAKE_INSTALL_PREFIX}/share/applications)
+install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/linux/photo_broom.desktop DESTINATION ${CMAKE_INSTALL_PREFIX}/share/applications)
