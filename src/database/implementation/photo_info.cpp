8ccd858460b12796b88c2e3254304fe5fb45c11c
Michał Walenciak <Kicer86@gmail.com>
2017-03-22 21:30:26 +0100

    database: removing 'new' usage

--- src/database/implementation/photo_info.cpp
+++ src/database/implementation/photo_info.cpp
@@ -32,8 +32,9 @@ struct PhotoInfo::Data
 };
 
 
-PhotoInfo::PhotoInfo(const Photo::Data& data): m_data(new Data)
+PhotoInfo::PhotoInfo(const Photo::Data& data): m_data(nullptr)
 {
+    m_data = std::make_unique<Data>();
     *m_data->m_data.lock() = data;
 }
 
