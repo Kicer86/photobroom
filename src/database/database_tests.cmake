11e2eac3a6a7fe984c43ac28c94ddf388660c950
Michał Walenciak <Kicer86@gmail.com>
2017-04-16 23:14:06 +0200

    database: switching to new test framework

--- src/database/database_tests.cmake
+++ src/database/database_tests.cmake
@@ -1,60 +1,55 @@
 
+include(${CMAKE_SOURCE_DIR}/cmake/functions.cmake)
+
 find_package(GMock REQUIRED)
 find_package(GTest REQUIRED)
 find_package(Threads REQUIRED)
 find_package(Qt5Sql REQUIRED)
 
-set(SRC
-        backends/sql_backends/generic_sql_query_constructor.cpp
-        backends/sql_backends/sql_action_query_generator.cpp
-        backends/sql_backends/sql_filter_query_generator.cpp
-        backends/sql_backends/query_structs.cpp
-        implementation/action.cpp
-        implementation/filter.cpp
-        implementation/photo_data.cpp
-        implementation/photo_info.cpp
-        implementation/photo_types.cpp
 
-        # sql tests:
-        unit_tests/generic_sql_query_constructor_tests.cpp
-        unit_tests/sql_filter_query_generator_tests.cpp
-        unit_tests/sql_action_query_generator_tests.cpp
+addTestTarget(database
+                SOURCES
+                    backends/sql_backends/generic_sql_query_constructor.cpp
+                    backends/sql_backends/sql_action_query_generator.cpp
+                    backends/sql_backends/sql_filter_query_generator.cpp
+                    backends/sql_backends/query_structs.cpp
+                    implementation/action.cpp
+                    implementation/filter.cpp
+                    implementation/photo_data.cpp
+                    implementation/photo_info.cpp
+                    implementation/photo_types.cpp
 
-        # tools tests:
+                    # sql tests:
+                    unit_tests/generic_sql_query_constructor_tests.cpp
+                    unit_tests/sql_filter_query_generator_tests.cpp
+                    unit_tests/sql_action_query_generator_tests.cpp
 
-        unit_tests/tag_info_collector_tests.cpp
+                    # tools tests:
 
-        # main()
-        unit_tests/main.cpp
-    )
+                    unit_tests/tag_info_collector_tests.cpp
 
-add_executable(database_tests ${SRC})
+                    # main()
+                    unit_tests/main.cpp
 
-target_link_libraries(database_tests
-                        PRIVATE
-                            core
-                            database
-                            Qt5::Core
-                            Qt5::Gui
-                            Qt5::Sql
-                            ${GMOCK_LIBRARY}
-                            ${CMAKE_THREAD_LIBS_INIT}
+                LIBRARIES
+                    PRIVATE
+                        core
+                        database
+                        Qt5::Core
+                        Qt5::Gui
+                        Qt5::Sql
+                        ${GMOCK_LIBRARY}
+                        ${CMAKE_THREAD_LIBS_INIT}
+
+                INCLUDES
+                    SYSTEM PRIVATE
+                        ${GMOCK_INCLUDE_DIRS}
+                        ${GTEST_INCLUDE_DIRS}
+                        ${Qt5Sql_INCLUDE_DIRS}
+
+                    PRIVATE
+                        backends/sql_backends
+                        ${CMAKE_SOURCE_DIR}/src
+                        ${CMAKE_CURRENT_SOURCE_DIR}
+                        ${CMAKE_CURRENT_BINARY_DIR}/backends/sql_backends
 )
-
-target_include_directories(database_tests
-                           SYSTEM PRIVATE
-                                ${GMOCK_INCLUDE_DIRS}
-                                ${GTEST_INCLUDE_DIRS}
-                                ${Qt5Sql_INCLUDE_DIRS}
-
-                           PRIVATE
-                                backends/sql_backends
-                                ${CMAKE_SOURCE_DIR}/src
-                                ${CMAKE_CURRENT_SOURCE_DIR}
-                                ${CMAKE_CURRENT_BINARY_DIR}/backends/sql_backends
-)
-
-
-enableCodeCoverage(database_tests)
-
-add_test(database database_tests)
