55f2257d016d2906bba5f2581c8eee578c6db669
Michał Walenciak <Kicer86@gmail.com>
2017-04-16 23:10:53 +0200

    photos_crawler: switching to new test framework

--- src/photos_crawler/photos_crawler_test.cmake
+++ src/photos_crawler/photos_crawler_test.cmake
@@ -5,23 +5,28 @@ find_package(Threads REQUIRED)
 find_package(GMock REQUIRED)
 find_package(GTest REQUIRED)
 
-include_directories(SYSTEM ${GMOCK_INCLUDE_DIRS} ${GTEST_INCLUDE_DIRS})
 
-set(SRC
-        default_analyzers/ext_defaultanalyzer.cpp
-        default_analyzers/file_analyzer.cpp
-        implementation/ianalyzer.cpp
-        implementation/ifile_system_scanner.cpp
-        implementation/photo_crawler.cpp
+addTestTarget(photos_crawler
+                SOURCES
+                    default_analyzers/ext_defaultanalyzer.cpp
+                    default_analyzers/file_analyzer.cpp
+                    implementation/ianalyzer.cpp
+                    implementation/ifile_system_scanner.cpp
+                    implementation/photo_crawler.cpp
 
-        unit_tests/analyzerTests.cpp
-        unit_tests/photo_crawler_tests.cpp
-        unit_tests/photo_crawler_builder_tests.cpp
-    )
+                    unit_tests/analyzerTests.cpp
+                    unit_tests/photo_crawler_tests.cpp
+                    unit_tests/photo_crawler_builder_tests.cpp
 
-add_executable(photos_crawler_tests ${SRC})
+                LIBRARIES
+                    PRIVATE
+                    Qt5::Core
+                    ${GMOCK_MAIN_LIBRARY}
+                    ${GMOCK_LIBRARY}
+                    ${CMAKE_THREAD_LIBS_INIT}
 
-target_link_libraries(photos_crawler_tests PRIVATE Qt5::Core ${GMOCK_MAIN_LIBRARY} ${GMOCK_LIBRARY} ${CMAKE_THREAD_LIBS_INIT})
-enableCodeCoverage(photos_crawler_tests)
-
-add_test(photos_crawler photos_crawler_tests)
+                INCLUDES
+                    SYSTEM PRIVATE
+                        ${GMOCK_INCLUDE_DIRS}
+                        ${GTEST_INCLUDE_DIRS}
+)
