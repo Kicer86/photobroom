a3111d1ca7eb5b2322f75d0e1505514a715199f1
Michał Walenciak <Kicer86@gmail.com>
2017-05-28 09:49:53 +0200

    translations update

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -124,7 +124,7 @@ Po zakończeniu odczytu zdjęcia można przejrzeć klikając w tabkę &quot;Nowe
 <context>
     <name>IdxDataManager</name>
     <message>
-        <location filename="../src/gui/desktop/models/model_helpers/idx_data_manager.cpp" line="+909"/>
+        <location filename="../src/gui/desktop/models/model_helpers/idx_data_manager.cpp" line="+911"/>
         <source>Unlabeled</source>
         <translation>Nieopisane</translation>
     </message>
