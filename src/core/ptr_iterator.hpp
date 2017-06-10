7b1989b73dade956a9fe464baee6f6754786c505
Michał Walenciak <Kicer86@gmail.com>
2017-06-10 22:59:48 +0200

    core: introducing generic concepts and containers utils

--- src/core/ptr_iterator.hpp
+++ src/core/ptr_iterator.hpp
@@ -11,57 +11,9 @@
 #include <type_traits>
 
 #include "iterator_wrapper.hpp"
+#include "generic_concepts.hpp"
 
 #ifdef CONCEPTS_SUPPORTED
-
-template<typename T>
-concept bool SmartPointer()
-{
-    return requires(T p)
-    {
-        typename T::pointer;
-
-        { p.operator*() };
-        { p.get() };
-        { std::is_pointer<decltype(*p)>::value == true };
-    };
-}
-
-
-template<typename T>
-concept bool Iterator()
-{
-    return requires(T i)
-    {
-        typename T::iterator_category;
-        typename T::value_type;
-        typename T::difference_type;
-        typename T::pointer;
-        typename T::reference;
-
-        { i.operator*() };
-        { i.operator->() };
-    };
-}
-
-
-template<typename T>
-concept bool Container()
-{
-    return requires(T p)
-    {
-        typename T::const_iterator;
-        typename T::value_type;
-
-        requires Iterator<typename T::const_iterator>();
-        requires Iterator<typename T::iterator>();
-
-        { p.begin() };
-        { p.end()   };
-
-    };
-}
-
 
 template<typename T>
 concept bool SmartPointerContainer()
