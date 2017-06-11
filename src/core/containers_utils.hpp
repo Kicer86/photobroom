9a2fe6b21de4c108d23b902d8e991acca6220afe
Michał Walenciak <Kicer86@gmail.com>
2017-06-11 22:04:27 +0200

    core: simplifying containers utils
    always use begin() and rbegin(). It's easier an more flexible

--- src/core/containers_utils.hpp
+++ src/core/containers_utils.hpp
@@ -11,23 +11,11 @@
 template<Container T>
 const typename T::value_type& front(const T& container)
 {
-    return container.front();
-}
-
-template<typename T, typename S, typename A>
-const T& front(const std::set<T, S, A>& container)
-{
     return *container.begin();
 }
 
 template<Container T>
 const typename T::value_type& back(const T& container)
-{
-    return container.back();
-}
-
-template<typename T, typename S, typename A>
-const T& back(const std::set<T, S, A>& container)
 {
     return *container.rbegin();
 }
