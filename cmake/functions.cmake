08b592704972efb3b400dc10cd48811aab98173c
Michał Walenciak <Kicer86@gmail.com>
2017-08-10 20:16:57 +0200

    make sanitized tests build optional

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -3,6 +3,8 @@
 #addTestTarget(`target` SOURCES source files TEST_LIBRARY gtest|gmock)
 #function will add executable with tests and will register it for ctest.
 
+option(ENABLE_SANITIZERS_FOR_TESTS "Enables build of tests with sanitizers turned on" OFF)
+
 macro(addTestTarget target)
 
     #get sources
@@ -14,76 +16,94 @@ macro(addTestTarget target)
 
     #add test executables
     add_executable(${test_bin}_base ${T_SOURCES})
-    add_executable(${test_bin}_addr ${T_SOURCES})
-    add_executable(${test_bin}_thread ${T_SOURCES})
-    add_executable(${test_bin}_leak ${T_SOURCES})
-    add_executable(${test_bin}_ub ${T_SOURCES})
 
-    # setup proper flags
-    addFlags(${test_bin}_addr COMPILE_FLAGS "-fsanitize=address")
-    addFlags(${test_bin}_addr LINK_FLAGS "-fsanitize=address")
+    if(ENABLE_SANITIZERS_FOR_TESTS)
+        add_executable(${test_bin}_addr ${T_SOURCES})
+        add_executable(${test_bin}_thread ${T_SOURCES})
+        add_executable(${test_bin}_leak ${T_SOURCES})
+        add_executable(${test_bin}_ub ${T_SOURCES})
+    endif()
 
-    addFlags(${test_bin}_thread COMPILE_FLAGS "-fsanitize=thread")
-    addFlags(${test_bin}_thread LINK_FLAGS "-fsanitize=thread")
+    if(ENABLE_SANITIZERS_FOR_TESTS)
+        # setup proper flags for sanitizers
+        addFlags(${test_bin}_addr COMPILE_FLAGS "-fsanitize=address")
+        addFlags(${test_bin}_addr LINK_FLAGS "-fsanitize=address")
 
-    addFlags(${test_bin}_leak COMPILE_FLAGS "-fsanitize=leak")
-    addFlags(${test_bin}_leak LINK_FLAGS "-fsanitize=leak")
+        addFlags(${test_bin}_thread COMPILE_FLAGS "-fsanitize=thread")
+        addFlags(${test_bin}_thread LINK_FLAGS "-fsanitize=thread")
 
-    addFlags(${test_bin}_ub COMPILE_FLAGS "-fsanitize=undefined -fno-sanitize-recover=all"
-                                          "-fsanitize-undefined-trap-on-error"
-                                          "-fsanitize=shift "
-                                          "-fsanitize=integer-divide-by-zero "
-                                          "-fsanitize=unreachable "
-                                          "-fsanitize=vla-bound "
-                                          "-fsanitize=null "
-                                          "-fsanitize=return "
-                                          "-fsanitize=signed-integer-overflow "
-                                          "-fsanitize=bounds "
-                                          "-fsanitize=bounds-strict "
-                                          "-fsanitize=alignment "
-                                          "-fsanitize=object-size "
-                                          "-fsanitize=float-divide-by-zero "
-                                          "-fsanitize=float-cast-overflow "
-                                          "-fsanitize=nonnull-attribute "
-                                          "-fsanitize=returns-nonnull-attribute "
-                                          "-fsanitize=bool "
-                                          "-fsanitize=enum "
-                                          "-fsanitize=vptr"
-    )
+        addFlags(${test_bin}_leak COMPILE_FLAGS "-fsanitize=leak")
+        addFlags(${test_bin}_leak LINK_FLAGS "-fsanitize=leak")
 
-    addFlags(${test_bin}_ub LINK_FLAGS "-fsanitize=undefined -fno-sanitize-recover=all")
+        addFlags(${test_bin}_ub COMPILE_FLAGS "-fsanitize=undefined -fno-sanitize-recover=all"
+                                            "-fsanitize-undefined-trap-on-error"
+                                            "-fsanitize=shift "
+                                            "-fsanitize=integer-divide-by-zero "
+                                            "-fsanitize=unreachable "
+                                            "-fsanitize=vla-bound "
+                                            "-fsanitize=null "
+                                            "-fsanitize=return "
+                                            "-fsanitize=signed-integer-overflow "
+                                            "-fsanitize=bounds "
+                                            "-fsanitize=bounds-strict "
+                                            "-fsanitize=alignment "
+                                            "-fsanitize=object-size "
+                                            "-fsanitize=float-divide-by-zero "
+                                            "-fsanitize=float-cast-overflow "
+                                            "-fsanitize=nonnull-attribute "
+                                            "-fsanitize=returns-nonnull-attribute "
+                                            "-fsanitize=bool "
+                                            "-fsanitize=enum "
+                                            "-fsanitize=vptr"
+        )
+
+        addFlags(${test_bin}_ub LINK_FLAGS "-fsanitize=undefined -fno-sanitize-recover=all")
+    endif()
 
     #link against proper libraries
     target_link_libraries(${test_bin}_base PRIVATE ${T_LIBRARIES})
-    target_link_libraries(${test_bin}_addr PRIVATE ${T_LIBRARIES})
-    target_link_libraries(${test_bin}_thread PRIVATE ${T_LIBRARIES})
-    target_link_libraries(${test_bin}_leak PRIVATE ${T_LIBRARIES})
-    target_link_libraries(${test_bin}_ub PRIVATE ${T_LIBRARIES})
+
+    if(ENABLE_SANITIZERS_FOR_TESTS)
+        target_link_libraries(${test_bin}_addr PRIVATE ${T_LIBRARIES})
+        target_link_libraries(${test_bin}_thread PRIVATE ${T_LIBRARIES})
+        target_link_libraries(${test_bin}_leak PRIVATE ${T_LIBRARIES})
+        target_link_libraries(${test_bin}_ub PRIVATE ${T_LIBRARIES})
+    endif()
 
     #include dirs
     target_include_directories(${test_bin}_base ${T_INCLUDES})
-    target_include_directories(${test_bin}_addr ${T_INCLUDES})
-    target_include_directories(${test_bin}_thread ${T_INCLUDES})
-    target_include_directories(${test_bin}_leak ${T_INCLUDES})
-    target_include_directories(${test_bin}_ub ${T_INCLUDES})
+
+    if(ENABLE_SANITIZERS_FOR_TESTS)
+        target_include_directories(${test_bin}_addr ${T_INCLUDES})
+        target_include_directories(${test_bin}_thread ${T_INCLUDES})
+        target_include_directories(${test_bin}_leak ${T_INCLUDES})
+        target_include_directories(${test_bin}_ub ${T_INCLUDES})
+    endif()
 
     #enable code coverage
     enableCodeCoverage(${test_bin}_base)
 
     #add tests
     add_test(${target}_base ${test_bin}_base)
-    add_test(${target}_addr ${test_bin}_addr)
-    add_test(${target}_thread ${test_bin}_thread)
-    add_test(${target}_leak ${test_bin}_leak)
-    add_test(${target}_ub ${test_bin}_ub)
+    set(test_binaries ${test_bin}_base)
+
+    if(ENABLE_SANITIZERS_FOR_TESTS)
+        add_test(${target}_addr ${test_bin}_addr)
+        add_test(${target}_thread ${test_bin}_thread)
+        add_test(${target}_leak ${test_bin}_leak)
+        add_test(${target}_ub ${test_bin}_ub)
+
+        list(APPEND test_binaries
+            ${test_bin}_addr
+            ${test_bin}_thread
+            ${test_bin}_leak
+            ${test_bin}_ub
+        )
+    endif()
 
     add_custom_target(${test_bin}
                         DEPENDS
-                            ${test_bin}_base
-                            ${test_bin}_addr
-                            ${test_bin}_thread
-                            ${test_bin}_leak
-                            ${test_bin}_ub
+                            ${test_binaries}
     )
 
 endmacro(addTestTarget)
