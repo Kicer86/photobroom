c6cc8f0d5d1f4030cdbce548653caa73eff8932b
Michał Walenciak <Kicer86@gmail.com>
2017-08-15 22:32:51 +0200

    core: exporting MediaTypes

--- src/core/media_types.hpp
+++ src/core/media_types.hpp
@@ -5,14 +5,15 @@
 
 #include <vector>
 
+#include "core_export.h"
 
 namespace MediaTypes
 {
     // regexps for supported media files
-    extern const std::vector<const char *> images_extensions;
-    extern const std::vector<const char *> movies_extensions;
+    CORE_EXPORT extern const std::vector<const char *> images_extensions;
+    CORE_EXPORT extern const std::vector<const char *> movies_extensions;
 
-    extern const std::vector<const char *> all_extensions;
+    CORE_EXPORT extern const std::vector<const char *> all_extensions;
 }
 
 #endif
