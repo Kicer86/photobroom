82b27edfec45aeed2e686fe1b969e1946c926e52
Michał Walenciak <Kicer86@gmail.com>
2017-08-15 22:33:23 +0200

    PhotoCrawlerBuilder: switching to global list of supported extensions

--- src/photos_crawler/implementation/photo_crawler_builder.cpp
+++ src/photos_crawler/implementation/photo_crawler_builder.cpp
@@ -1,6 +1,8 @@
 
 #include "photo_crawler_builder.hpp"
 
+#include <core/media_types.hpp>
+
 #include "photo_crawler.hpp"
 #include "default_filesystem_scanners/filesystemscanner.hpp"
 #include "default_analyzers/file_analyzer.hpp"
@@ -19,11 +21,8 @@ std::unique_ptr<IAnalyzer> PhotoCrawlerBuilder::buildFullFileAnalyzer()
     auto analyzer = std::make_unique<FileAnalyzer>();
 
     //add subanalyzers
-    analyzer->registerAnalyzer( std::make_unique<FileExtensionAnalyzer>("jpe?g") );
-    analyzer->registerAnalyzer( std::make_unique<FileExtensionAnalyzer>("png") );
-    analyzer->registerAnalyzer( std::make_unique<FileExtensionAnalyzer>("gif") );
-    analyzer->registerAnalyzer( std::make_unique<FileExtensionAnalyzer>("tiff?") );
-    analyzer->registerAnalyzer( std::make_unique<FileExtensionAnalyzer>("bmp") );
+    for(const char* ext: MediaTypes::all_extensions)
+        analyzer->registerAnalyzer( std::make_unique<FileExtensionAnalyzer>(ext) );
 
     // TODO: added due to bug in clang: http://stackoverflow.com/questions/36752678/clang-returning-stdunique-ptr-with-type-conversion
     return std::move(analyzer);
