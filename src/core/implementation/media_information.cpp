ac450c2a546c9cf77a1e8ec1fcbbbb8667008ae7
Michał Walenciak <Kicer86@gmail.com>
2017-08-19 09:13:47 +0200

    introducing MediaInformation

---
+++ src/core/implementation/media_information.cpp
@@ -0,0 +1,31 @@
+/*
+ * Toolkit for reading media file size
+ * Copyright (C) 2017  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "media_information.hpp"
+
+MediaInformation::MediaInformation()
+{
+
+}
+
+
+QSize MediaInformation::size(const QString &) const
+{
+    return QSize();
+}
---
+++ src/core/media_information.hpp
@@ -0,0 +1,41 @@
+/*
+ * Toolkit for reading media file size
+ * Copyright (C) 2017  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MEDIAINFORMATION_HPP
+#define MEDIAINFORMATION_HPP
+
+#include "imedia_information.hpp"
+
+
+class MediaInformation : public IMediaInformation
+{
+    public:
+        MediaInformation();
+        MediaInformation(const MediaInformation &) = delete;
+        MediaInformation(MediaInformation &&) = delete;
+
+        MediaInformation& operator=(const MediaInformation &) = delete;
+        MediaInformation& operator=(MediaInformation &&) = delete;
+
+        virtual ~MediaInformation() = default;
+
+        virtual QSize size(const QString &) const;
+};
+
+#endif // MEDIAINFORMATION_HPP
