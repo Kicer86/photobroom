3a34898a4b56d6c4ac1d736c6cab90e127bd7ad3
Michał Walenciak <Kicer86@gmail.com>
2017-10-31 12:18:53 +0100

    test if parents are being considered for QModelIndex comparison

--- src/core/implementation/qmodelindex_comparator.cpp
+++ src/core/implementation/qmodelindex_comparator.cpp
@@ -4,10 +4,60 @@
 #include <QModelIndex>
 
 
+namespace
+{
+    std::vector<QModelIndex> getHierarchyOf(const QModelIndex& item)
+    {
+        std::vector<QModelIndex> result;
+
+        if (item.isValid())
+        {
+            const QModelIndex parent = item.parent();
+            result = getHierarchyOf(parent);
+            result.push_back(parent);
+        }
+
+        return result;
+    }
+
+    bool isLess(const QModelIndex& lhs, const QModelIndex& rhs)
+    {
+        return  lhs.row() <  rhs.row()
+            || (lhs.row() == rhs.row() && (lhs.column() <  rhs.column()
+                                       || (lhs.column() == rhs.column() && (lhs.internalId() <  rhs.internalId()
+                                                                        || (lhs.internalId() == rhs.internalId() && lhs.model() < rhs.model() )))));
+    }
+}
+
+
 bool QModelIndexComparator::operator()(const QModelIndex& lhs, const QModelIndex& rhs) const
 {
-    return  lhs.row() <  rhs.row()
-        || (lhs.row() == rhs.row() && (lhs.column() <  rhs.column()
-                                    || (lhs.column() == rhs.column() && (lhs.internalId() <  rhs.internalId()
-                                                                    || (lhs.internalId() == rhs.internalId() && lhs.model() < rhs.model() )))));
+    std::vector<QModelIndex> lhs_hierarchy = getHierarchyOf(lhs);
+    std::vector<QModelIndex> rhs_hierarchy = getHierarchyOf(rhs);
+
+    lhs_hierarchy.push_back(lhs);
+    rhs_hierarchy.push_back(rhs);
+
+    const std::size_t s = std::min(lhs_hierarchy.size(), rhs_hierarchy.size());
+
+    bool result = false;
+    for (std::size_t i = 0; i < s; i++)
+    {
+        const QModelIndex& lhs_item = lhs_hierarchy[i];
+        const QModelIndex& rhs_item = rhs_hierarchy[i];
+
+        if (isLess(lhs_item, rhs_item))
+        {
+            result = true;
+            break;
+        }
+
+        if (isLess(rhs_item, lhs_item))
+        {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
 }
--- src/core/unit_tests/qmodelindex_comparator_tests.cpp
+++ src/core/unit_tests/qmodelindex_comparator_tests.cpp
@@ -5,15 +5,53 @@
 
 #include "unit_tests_utils/mock_qabstractitemmodel.hpp"
 
+using testing::_;
+using testing::NiceMock;
+using testing::Return;
 
 TEST(QModelIndexComparatorTest, childrenOfSameParent)
+{
+    NiceMock<MockQAbstractItemModel> model;
+    QModelIndexComparator comparator;
+
+    const QModelIndex topParent;     // parent for c1 and c2
+    const QModelIndex c1 = model.constructIndex(1, 0);
+    const QModelIndex c2 = model.constructIndex(11, 0);
+
+    ON_CALL(model, parent(_))
+        .WillByDefault(Return(topParent));
+
+    EXPECT_TRUE(comparator(c1, c2));    // c1 is less than c2
+    EXPECT_FALSE(comparator(c2, c1));   // c2 is not less than c1
+}
+
+
+TEST(QModelIndexComparatorTest, childrenOfDifferentParents)
 {
     MockQAbstractItemModel model;
     QModelIndexComparator comparator;
 
+    const QModelIndex top;
+
+    const QModelIndex p1 = model.constructIndex(5, 0);
+    const QModelIndex p2 = model.constructIndex(7, 0);
+
     const QModelIndex c1 = model.constructIndex(1, 0);
     const QModelIndex c2 = model.constructIndex(11, 0);
 
-    EXPECT_TRUE(comparator(c1, c2));    // c1 is less than c2
-    EXPECT_FALSE(comparator(c2, c1));   // c2 is not less than c1
+    ON_CALL(model, parent(_))
+        .WillByDefault(Return(top));
+
+    ON_CALL(model, parent(c1))
+        .WillByDefault(Return(p2));
+
+    ON_CALL(model, parent(c2))
+        .WillByDefault(Return(p1));
+
+    // c1 is children of p2
+    // c2 is children of p1
+    // p1 is first, so its child should win
+
+    EXPECT_TRUE(comparator(c2, c1));    // c2 is less than c1
+    EXPECT_FALSE(comparator(c1, c2));   // c1 is not less than c2
 }
