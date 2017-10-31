20a9fdec5cdcb06cedbdbdd03bc930cbeb338155
Michał Walenciak <Kicer86@gmail.com>
2017-10-31 14:24:51 +0100

    introducing QModelIndexSelector::listAllBetween

--- src/core/implementation/qmodelindex_selector.cpp
+++ src/core/implementation/qmodelindex_selector.cpp
@@ -1,1 +1,9 @@
- 
+
+#include "qmodelindex_selector.hpp"
+
+std::vector<QModelIndex> QModelIndexSelector::listAllBetween(const QModelIndex &, const QModelIndex &)
+{
+    std::vector<QModelIndex> result;
+
+    return result;
+}
--- src/core/qmodelindex_selector.hpp
+++ src/core/qmodelindex_selector.hpp
@@ -1,1 +1,16 @@
- 
+
+
+#ifndef QMODELINDEX_SELECTOR_HPP
+#define QMODELINDEX_SELECTOR_HPP
+
+#include <QModelIndex>
+
+#include "core_export.h"
+
+namespace QModelIndexSelector
+{
+    CORE_EXPORT std::vector<QModelIndex> listAllBetween(const QModelIndex &, const QModelIndex &);
+};
+
+#endif // QMODELINDEX_SELECTOR_HPP
+
