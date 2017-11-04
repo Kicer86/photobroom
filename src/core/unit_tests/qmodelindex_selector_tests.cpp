d4acc44fa16f667f19ed6a4f3c11a87cf5814dd9
Michał Walenciak <Kicer86@gmail.com>
2017-11-04 12:36:58 +0100

    introducing test for items in different parents

--- src/core/unit_tests/qmodelindex_selector_tests.cpp
+++ src/core/unit_tests/qmodelindex_selector_tests.cpp
@@ -12,23 +12,14 @@ using testing::Return;
 
 TEST(QModelIndexSelectorTest, childrenOfSameParent)
 {
-    NiceMock<MockQAbstractItemModel> model;
+    NiceMock<StubQAbstractItemModel> model;
 
     const QModelIndex top;
-    const QModelIndex c1 = model.constructIndex(1, 0);
-    const QModelIndex c2 = model.constructIndex(3, 0);
-
-    ON_CALL(model, parent(_))
-        .WillByDefault(Return(top));
-
-    ON_CALL(model, index(_, _, _))
-        .WillByDefault(Invoke([&model](int r, int c, const QModelIndex &)
-        {
-            return model.constructIndex(r, c, nullptr);
-        }));
+    const QModelIndex c1 = model.addItem(1, 0, top);
+                           model.addItem(2, 0, top);
+    const QModelIndex c2 = model.addItem(3, 0, top);
 
     const auto items = QModelIndexSelector::listAllBetween(c1, c2);
-
     const std::size_t s = items.size();
 
     ASSERT_EQ(s, 3);
@@ -37,4 +28,27 @@ TEST(QModelIndexSelectorTest, childrenOfSameParent)
         EXPECT_EQ(items[i].row(), i + 1);
         EXPECT_EQ(items[i].column(), 0);
     }
+}
+
+
+
+TEST(QModelIndexSelectorTest, childrenOfDifferentParents)
+{
+    NiceMock<StubQAbstractItemModel> model;
+
+    const QModelIndex top;
+    const QModelIndex p1 =  model.addItem(1, 0, top);
+    const QModelIndex p2 =  model.addItem(2, 0, top);
+
+                            model.addItem(0, 0, p1);
+    const QModelIndex c11 = model.addItem(1, 0, p1);
+    const QModelIndex c12 = model.addItem(2, 0, p1);
+
+    const QModelIndex c20 = model.addItem(0, 0, p2);
+    const QModelIndex c21 = model.addItem(1, 0, p2);
+    const QModelIndex c22 = model.addItem(2, 0, p2);
+
+    const auto items = QModelIndexSelector::listAllBetween(c11, c22);
+
+    EXPECT_EQ(items, std::vector<QModelIndex>({c11, c12, p2, c20, c21, c22}));
 }
