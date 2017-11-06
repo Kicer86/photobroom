dc4b971453056b9e3edde1b20602a14cca129a77
Michał Walenciak <Kicer86@gmail.com>
2017-11-06 22:43:45 +0100

    extending model to run more tests

--- src/core/unit_tests/qmodelindex_selector_tests.cpp
+++ src/core/unit_tests/qmodelindex_selector_tests.cpp
@@ -16,6 +16,7 @@ struct QModelIndexSelectorTest: testing::Test
     {
         p1 =  model.addItem(1, 0, top);
         p2 =  model.addItem(2, 0, top);
+        p3 =  model.addItem(3, 0, top);
 
         c10 = model.addItem(0, 0, p1);
         c11 = model.addItem(1, 0, p1);
@@ -24,6 +25,22 @@ struct QModelIndexSelectorTest: testing::Test
         c20 = model.addItem(0, 0, p2);
         c21 = model.addItem(1, 0, p2);
         c22 = model.addItem(2, 0, p2);
+
+        c30 = model.addItem(0, 0, p3);
+        c31 = model.addItem(1, 0, p3);
+        c32 = model.addItem(2, 0, p3);
+
+        c300 = model.addItem(0, 0, c30);
+        c301 = model.addItem(1, 0, c30);
+        c302 = model.addItem(2, 0, c30);
+
+        c310 = model.addItem(0, 0, c31);
+        c311 = model.addItem(1, 0, c31);
+        c312 = model.addItem(2, 0, c31);
+
+        c320 = model.addItem(0, 0, c32);
+        c321 = model.addItem(1, 0, c32);
+        c322 = model.addItem(2, 0, c32);
     }
 
     ~QModelIndexSelectorTest()
@@ -36,6 +53,7 @@ struct QModelIndexSelectorTest: testing::Test
     QModelIndex top;
     QModelIndex p1;
     QModelIndex p2;
+    QModelIndex p3;
 
     QModelIndex c10;
     QModelIndex c11;
@@ -44,6 +62,22 @@ struct QModelIndexSelectorTest: testing::Test
     QModelIndex c20;
     QModelIndex c21;
     QModelIndex c22;
+
+    QModelIndex c30;
+    QModelIndex c31;
+    QModelIndex c32;
+
+    QModelIndex c300;
+    QModelIndex c301;
+    QModelIndex c302;
+
+    QModelIndex c310;
+    QModelIndex c311;
+    QModelIndex c312;
+
+    QModelIndex c320;
+    QModelIndex c321;
+    QModelIndex c322;
 };
 
 
