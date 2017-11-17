Kicer86/photobroom

commit 3a1e6b0add2f262db81f3c811b9a930a24037e27

AnimationGenerator: reorganization + progress for stabilization

src/gui/desktop/ui/photos_grouping_dialog.cpp
@@ -2,9 +2,12 @@
 #include "photos_grouping_dialog.hpp"
 
 #include <QFileInfo>
+#include <QMovie>
 #include <QProcess>
+#include <QProgressBar>
 
 #include <core/iexif_reader.hpp>
+#include <core/down_cast.hpp>
 
 #include "ui_photos_grouping_dialog.h"
 
@@ -15,6 +18,7 @@ PhotosGroupingDialog::PhotosGroupingDialog(const std::vector<IPhotoInfo::Ptr>& p
     QDialog(parent),
     m_model(),
     m_animationGenerator(),
+    m_movie(),
     m_sortProxy(),
     m_representativeFile(),
     ui(new Ui::PhotosGroupingDialog),
@@ -35,13 +39,12 @@ PhotosGroupingDialog::PhotosGroupingDialog(const std::vector<IPhotoInfo::Ptr>& p
     ui->photosList->resizeColumnsToContents();
     ui->buttonBox->button(QDialogButtonBox::Ok)->setDisabled(true);
 
-    using namespace std::placeholders;
-    auto callback = std::bind(&PhotosGroupingDialog::updatePreview, this, _1, _2);
-
-    m_animationGenerator = std::make_unique<AnimationGenerator>(m_executor, callback);
+    m_animationGenerator = std::make_unique<AnimationGenerator>(m_executor);
 
     connect(ui->applyButton, &QPushButton::clicked, this, &PhotosGroupingDialog::makeAnimation);
-    connect(ui->previewScaleSlider, &QSlider::sliderMoved, m_animationGenerator.get(), &AnimationGenerator::scalePreview);
+    connect(m_animationGenerator.get(), &AnimationGenerator::operation, this, &PhotosGroupingDialog::generationTitle);
+    connect(m_animationGenerator.get(), &AnimationGenerator::progress,  this, &PhotosGroupingDialog::generationProgress);
+    connect(m_animationGenerator.get(), &AnimationGenerator::finished,  this, &PhotosGroupingDialog::generationDone);
 }
 
 
@@ -57,15 +60,43 @@ QString PhotosGroupingDialog::getRepresentative() const
 }
 
 
-void PhotosGroupingDialog::updatePreview(QWidget* preview, const QString& location)
+void PhotosGroupingDialog::generationTitle(const QString& title)
 {
-    if (preview != nullptr)
+    QWidget* progress_widget = ui->resultPreview->widget();
+    QProgressBar* progress = down_cast<QProgressBar *>(progress_widget);
+
+    progress->setFormat(title + " %p%");
+}
+
+
+void PhotosGroupingDialog::generationProgress(int v)
+{
+    QWidget* progress_widget = ui->resultPreview->widget();
+    QProgressBar* progress = down_cast<QProgressBar *>(progress_widget);
+
+    if (v == -1)
+        progress->setMaximum(0);
+    else
     {
-        ui->resultPreview->setWidget(preview);
-        m_representativeFile = location;
+        progress->setMaximum(100);
+        progress->setValue(v);
     }
+}
+
+
+void PhotosGroupingDialog::generationDone(const QString& location)
+{
+    m_representativeFile = location;
 
     ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(m_representativeFile.isEmpty() == false);
+
+    m_movie = std::make_unique<QMovie>(location);
+    QLabel* label = new QLabel;
+
+    label->setMovie(m_movie.get());
+    m_movie->start();
+
+    ui->resultPreview->setWidget(label);
 }
 
 
@@ -85,7 +116,11 @@ void PhotosGroupingDialog::makeAnimation()
     generator_data.delay = ui->delaySpinBox->value();
     generator_data.stabilize = ui->stabilizationCheckBox->isChecked();
 
-    m_animationGenerator->generatePreviewWidget(generator_data);
+    QProgressBar* bar = new QProgressBar(this);
+    bar->setFormat("%p%");
+
+    ui->resultPreview->setWidget(bar);
+    m_animationGenerator->generate(generator_data);
 }
 
 

src/gui/desktop/ui/photos_grouping_dialog.hpp
@@ -60,13 +60,16 @@ class PhotosGroupingDialog: public QDialog
     private:
         QStandardItemModel m_model;
         std::unique_ptr<AnimationGenerator> m_animationGenerator;
+        std::unique_ptr<QMovie> m_movie;
         SortingProxy m_sortProxy;
         QString m_representativeFile;
         Ui::PhotosGroupingDialog *ui;
         IExifReader* m_exifReader;
         ITaskExecutor* m_executor;
 
-        void updatePreview(QWidget *, const QString &);
+        void generationTitle(const QString &);
+        void generationProgress(int);
+        void generationDone(const QString &);
         void typeChanged();
         void makeAnimation();
         void fillModel(const std::vector<IPhotoInfo::Ptr> &);

tr/photo_broom_pl.ts
@@ -2,6 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="pl_PL">
 <context>
+    <name>AnimationGenerator</name>
+    <message>
+        <location filename="../src/gui/desktop/utils/grouppers/animation_generator.cpp" line="+126"/>
+        <source>Stabilizing photos</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>BaseTags</name>
     <message>
         <location filename="../src/core/implementation/base_tags.cpp" line="+67"/>
@@ -572,7 +580,7 @@ Kod błędu: %1</translation>
         <translation>Podgląd:</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/photos_grouping_dialog.cpp" line="+107"/>
+        <location filename="../src/gui/desktop/ui/photos_grouping_dialog.cpp" line="+142"/>
         <source>photo path</source>
         <translation>ścieżka zdjęcia</translation>
     </message>

src/gui/desktop/utils/CMakeLists.txt
@@ -38,6 +38,7 @@ set(UTILS_FILES_TO_MOC
     photos_collector.hpp
     selection_extractor.hpp
     signal_filter.hpp
+    grouppers/animation_generator.hpp
 )
 
 source_group(files REGULAR_EXPRESSION .*gui.* )

src/gui/desktop/utils/grouppers/animation_generator.cpp
@@ -24,10 +24,8 @@
 #include <QDirIterator>
 #include <QFile>
 #include <QFileInfo>
-#include <QMovie>
 #include <QLabel>
 #include <QProcess>
-#include <QProgressBar>
 #include <QRegExp>
 #include <QTextStream>
 
@@ -65,108 +63,37 @@ namespace
 
         return status;
     }
-
-    struct GifGenerator: ITaskExecutor::ITask
-    {
-        GifGenerator(const AnimationGenerator::Data& data, const QString& location, const std::function<void(const QString &)>& doneCallback):
-            m_data(data),
-            m_location(location),
-            m_doneCallback(doneCallback)
-        {
-        }
-
-        std::string name() const override
-        {
-            return "GifGenerator";
-        }
-
-        void perform() override
-        {
-            // stabilize?
-            QStringList images_to_be_used;
-
-            if (m_data.stabilize)
-            {
-                // https://groups.google.com/forum/#!topic/hugin-ptx/gqodoTgAjbI
-                // http://wiki.panotools.org/Panorama_scripting_in_a_nutshell
-                // http://wiki.panotools.org/Align_image_stack
-
-                // generate aligned files
-                const QString output_prefix = System::getTempFilePath() + "_";
-
-                int photos_left = m_data.photos.size();
-
-                auto align_image_stack_output_analizer = [&photos_left](QIODevice& device)
-                {
-                    QRegExp cp_regExp("Creating control points between.*");
-
-                    while(device.bytesAvailable() > 0)
-                    {
-                        const QByteArray line_raw = device.readLine();
-                        const QString line(line_raw);
-
-                        if (cp_regExp.exactMatch(line))
-                        {
-                            photos_left--;
-
-                        }
-                    }
-                };
-
-                execute("align_image_stack",
-                        align_image_stack_output_analizer,
-                        "-C",
-                        "-v",                              // for align_image_stack_output_analizer
-                        "--use-given-order",
-                        "-d", "-i", "-x", "-y", "-z",
-                        "-s", "0",
-                        "-a", output_prefix,
-                        m_data.photos);
-
-                const QFileInfo output_prefix_info(output_prefix);
-                QDirIterator filesIterator(output_prefix_info.absolutePath(), {output_prefix_info.fileName() + "*"}, QDir::Files);
-
-                while(filesIterator.hasNext())
-                    images_to_be_used.push_back(filesIterator.next());
-
-                std::sort(images_to_be_used.begin(), images_to_be_used.end());
-            }
-            else
-                images_to_be_used = m_data.photos;
-
-            // generate gif
-            const int last_photo_delay = (m_data.delay / 1000.0) * 100 + (1 / m_data.fps * 100);
-            const QStringList all_but_last = images_to_be_used.mid(0, images_to_be_used.size() - 1);
-            const QString last = images_to_be_used.last();
-
-            execute("convert",
-                    nullptr,
-                    "-delay", QString::number(1/m_data.fps * 100),   // convert fps to 1/100th of a second
-                    all_but_last,
-                    "-delay", QString::number(last_photo_delay),
-                    last,
-                    "-auto-orient",
-                    "-loop", "0",
-                    "-resize", QString::number(m_data.scale) + "%",
-                    m_location);
-
-            m_doneCallback(m_location);
-        }
-
-        AnimationGenerator::Data m_data;
-        QString m_location;
-        std::function<void(const QString &)> m_doneCallback;
-    };
 }
+
+struct GifGenerator: ITaskExecutor::ITask
+{
+    GifGenerator(AnimationGenerator* generator): m_generator(generator)
+    {
+    }
+
+
+    GifGenerator(const GifGenerator &) = delete;
+    GifGenerator& operator=(const GifGenerator &) = delete;
+
+    std::string name() const override
+    {
+        return "GifGenerator";
+    }
+
+    void perform() override
+    {
+        m_generator->perform();
+    }
+
+    AnimationGenerator* m_generator;
+};
 
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
-AnimationGenerator::AnimationGenerator(ITaskExecutor* executor, const std::function<void(QWidget *, const QString &)>& callback):
-    m_callback(callback),
-    m_movie(),
-    m_baseSize(),
+AnimationGenerator::AnimationGenerator(ITaskExecutor* executor):
+    m_data(),
     m_executor(executor)
 {
 }
@@ -178,53 +105,91 @@ AnimationGenerator::~AnimationGenerator()
 }
 
 
-void AnimationGenerator::generatePreviewWidget(const Data& data)
+void AnimationGenerator::generate(const Data& data)
 {
-    if (m_movie.get() != nullptr)
-        m_movie->stop();
+    m_data = data;
 
-    m_baseSize = QSize();
-
-    const QString location = System::getTempFilePath() + ".gif";
-
-    using namespace std::placeholders;
-    std::function<void(const QString &)> doneFun = std::bind(&AnimationGenerator::done, this, _1);
-    auto doneCallback = make_cross_thread_function(this, doneFun);
-
-    auto task = std::make_unique<GifGenerator>(data, location, doneCallback);
+    auto task = std::make_unique<GifGenerator>(this);
     m_executor->add(std::move(task));
 
-    QProgressBar* progress = new QProgressBar;
-    progress->setRange(0, 0);
-
-    m_callback(progress, QString());
+    emit progress(-1);
 }
 
 
-void AnimationGenerator::scalePreview(double scale)
+void AnimationGenerator::perform()
 {
-    if (m_movie.get() != nullptr)
+    // stabilize?
+    QStringList images_to_be_used;
+
+    if (m_data.stabilize)
     {
-        if (m_baseSize.isValid() == false)
-            m_baseSize = m_movie->frameRect().size();
+        emit operation(tr("Stabilizing photos"));
+        emit progress(0);
+        // https://groups.google.com/forum/#!topic/hugin-ptx/gqodoTgAjbI
+        // http://wiki.panotools.org/Panorama_scripting_in_a_nutshell
+        // http://wiki.panotools.org/Align_image_stack
 
-        const double scaleFactor = scale/100;
-        QSize size = m_baseSize;
-        size.rheight() *= scaleFactor;
-        size.rwidth() *= scaleFactor;
+        // generate aligned files
+        const QString output_prefix = System::getTempFilePath() + "_";
 
-        m_movie->setScaledSize(size);
+        int photos_count = m_data.photos.size();
+        int photos_done = 0;
+
+        auto align_image_stack_output_analizer = [&photos_done, &photos_count, this](QIODevice& device)
+        {
+            QRegExp cp_regExp("Creating control points between.*");
+
+            while(device.bytesAvailable() > 0)
+            {
+                const QByteArray line_raw = device.readLine();
+                const QString line(line_raw);
+
+                if (cp_regExp.exactMatch(line))
+                {
+                    photos_done++;
+
+                    emit progress(photos_done * 100 / (photos_count - 1));   // there will be n-1 control points groups
+                }
+            }
+        };
+
+        execute("align_image_stack",
+                align_image_stack_output_analizer,
+                "-C",
+                "-v",                              // for align_image_stack_output_analizer
+                "--use-given-order",
+                "-d", "-i", "-x", "-y", "-z",
+                "-s", "0",
+                "-a", output_prefix,
+                m_data.photos);
+
+        const QFileInfo output_prefix_info(output_prefix);
+        QDirIterator filesIterator(output_prefix_info.absolutePath(), {output_prefix_info.fileName() + "*"}, QDir::Files);
+
+        while(filesIterator.hasNext())
+            images_to_be_used.push_back(filesIterator.next());
+
+        std::sort(images_to_be_used.begin(), images_to_be_used.end());
     }
-}
+    else
+        images_to_be_used = m_data.photos;
 
+    // generate gif
+    const int last_photo_delay = (m_data.delay / 1000.0) * 100 + (1 / m_data.fps * 100);
+    const QStringList all_but_last = images_to_be_used.mid(0, images_to_be_used.size() - 1);
+    const QString last = images_to_be_used.last();
+    const QString location = System::getTempFilePath() + ".gif";
 
-void AnimationGenerator::done(const QString& location)
-{
-    m_movie = std::make_unique<QMovie>(location);
-    QLabel* label = new QLabel;
+    execute("convert",
+            nullptr,
+            "-delay", QString::number(1/m_data.fps * 100),   // convert fps to 1/100th of a second
+            all_but_last,
+            "-delay", QString::number(last_photo_delay),
+            last,
+            "-auto-orient",
+            "-loop", "0",
+            "-resize", QString::number(m_data.scale) + "%",
+            location);
 
-    label->setMovie(m_movie.get());
-    m_movie->start();
-
-    m_callback(label, location);
+    emit finished(location);
 }

src/gui/desktop/utils/grouppers/animation_generator.hpp
@@ -25,36 +25,43 @@
 
 #include <core/itask_executor.hpp>
 
-class QMovie;
 
-
-struct AnimationGenerator: QObject
+class AnimationGenerator: public QObject
 {
-    struct Data
-    {
-        double fps;
-        double delay;
-        double scale;
-        QStringList photos;
-        bool stabilize;
+        Q_OBJECT
 
-        Data(): fps(0.0), delay(0.0), scale(0.0), photos(), stabilize(false) {}
-    };
+    public:
+        struct Data
+        {
+            double fps;
+            double delay;
+            double scale;
+            QStringList photos;
+            bool stabilize;
 
-    AnimationGenerator(ITaskExecutor* executor, const std::function<void(QWidget *, const QString &)>& callback);
-    AnimationGenerator(const AnimationGenerator &) = delete;
-    ~AnimationGenerator();
+            Data(): fps(0.0), delay(0.0), scale(0.0), photos(), stabilize(false) {}
+        };
 
-    AnimationGenerator& operator=(const AnimationGenerator &) = delete;
+        AnimationGenerator(ITaskExecutor* executor);
+        AnimationGenerator(const AnimationGenerator &) = delete;
+        ~AnimationGenerator();
 
-    void generatePreviewWidget(const Data& data);
-    void scalePreview(double scale);
-    void done(const QString& location);
+        AnimationGenerator& operator=(const AnimationGenerator &) = delete;
 
-    std::function<void(QWidget *, const QString &)> m_callback;
-    std::unique_ptr<QMovie> m_movie;
-    QSize m_baseSize;
-    ITaskExecutor* m_executor;
+        void generate(const Data& data);
+
+    signals:
+        void operation(const QString &);
+        void progress(int);
+        void finished(const QString &);
+
+    private:
+        Data m_data;
+        ITaskExecutor* m_executor;
+
+        friend class GifGenerator;
+
+        void perform();
 };
 
 

commit 1f07880e2d31110bf380ec697278f32778d8b422

restoring previewScaleSlider's functionality
regression; broken in 3a1e6b0add2f262db81f3c811b9a930a24037e27

src/gui/desktop/ui/photos_grouping_dialog.cpp
@@ -186,6 +186,7 @@ void PhotosGroupingDialog::makeAnimation()
         auto animation_task = std::make_unique<AnimationGenerator>(generator_data, m_logger);
 
         connect(this, &PhotosGroupingDialog::cancel, animation_task.get(), &AnimationGenerator::cancel);
+        connect(ui->previewScaleSlider, &QSlider::sliderMoved,        this, &PhotosGroupingDialog::scalePreview);
         connect(animation_task.get(), &AnimationGenerator::operation, this, &PhotosGroupingDialog::generationTitle);
         connect(animation_task.get(), &AnimationGenerator::progress,  this, &PhotosGroupingDialog::generationProgress);
         connect(animation_task.get(), &AnimationGenerator::finished,  this, &PhotosGroupingDialog::generationDone);
@@ -240,4 +241,23 @@ QStringList PhotosGroupingDialog::getPhotos() const
     }
 
     return result;
+}
+
+
+void PhotosGroupingDialog::scalePreview()
+{
+    if (m_movie.get() != nullptr)
+    {
+        if (m_baseSize.isValid() == false)
+            m_baseSize = m_movie->frameRect().size();
+
+        const int scale = ui->previewScaleSlider->value();
+
+        const double scaleFactor = scale/100.0;
+        QSize size = m_baseSize;
+        size.rheight() *= scaleFactor;
+        size.rwidth() *= scaleFactor;
+
+        m_movie->setScaledSize(size);
+    }
 }

src/gui/desktop/ui/photos_grouping_dialog.hpp
@@ -69,6 +69,7 @@ class PhotosGroupingDialog: public QDialog
         std::unique_ptr<QMovie> m_movie;
         SortingProxy m_sortProxy;
         QString m_representativeFile;
+        QSize m_baseSize;
         Ui::PhotosGroupingDialog *ui;
         IExifReader* m_exifReader;
         IConfiguration* m_config;
@@ -86,6 +87,7 @@ class PhotosGroupingDialog: public QDialog
         void fillModel(const std::vector<IPhotoInfo::Ptr> &);
 
         QStringList getPhotos() const;
+        void scalePreview();
 
     // internal signals:
     signals:

tr/photo_broom_pl.ts
@@ -647,7 +647,7 @@ Proszę przejść do ustawień aplikacji i ustawić ścieżkę do programu &quot
 Odwiedź stronę http://hugin.sourceforge.net/ aby go pobrać.</translation>
     </message>
     <message>
-        <location line="+16"/>
+        <location line="+17"/>
         <source>Cancel generation</source>
         <translation>Przerwij</translation>
     </message>