94c32b29e4f4dc4b9de3c7222e761e0f45bfeb75
Michał Walenciak <Kicer86@gmail.com>
2017-11-26 19:14:25 +0100

    extracting big text block to .cmake

--- tools/dependencies/jsoncpp.py
+++ tools/dependencies/jsoncpp.py
@@ -1,15 +1,8 @@
 
-
 def build_jsoncpp(cmake_lists):
 
-    cmake_lists.write(
-        "ExternalProject_Add(jsoncpp                                       \n"
-        "GIT_REPOSITORY https://github.com/open-source-parsers/jsoncpp.git \n"
-        "GIT_TAG 9234cbbc90d1f6c70dd5a90b4d533779e45c820c                  \n"
-        "PREFIX ${PROJECT_BINARY_DIR}                                      \n"
-        "CMAKE_ARGS ${COMMON_OPTIONS} -DCMAKE_POSITION_INDEPENDENT_CODE=ON \n"
-        ")                                                                 \n"
-        )
+    cmake_lists_section = open('jsoncpp.cmake').read()
+    cmake_lists.write(cmake_lists_section)
 
     return 0
 
