27e2a5b9217bcc9521dd8dce8b6e5db63ae49943
Michał Walenciak <Kicer86@gmail.com>
2017-12-16 21:56:10 +0100

    introducing constants for image files

---
+++ src/gui/images/images.hpp
@@ -0,0 +1,14 @@
+
+#ifndef IMAGES_HPP
+#define IMAGES_HPP
+
+namespace Images
+{
+    constexpr const char* const add     = ":/gui/add.svg";
+    constexpr const char* const checked = ":/gui/checked.svg";
+    constexpr const char* const clock   = ":/gui/clock.svg";
+    constexpr const char* const error   = ":/gui/error.svg";
+    constexpr const char* const video   = ":/gui/video.svg";
+}
+
+#endif
