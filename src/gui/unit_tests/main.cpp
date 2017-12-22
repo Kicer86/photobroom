746f4ed38bc0e1d7a715c5d5985928f66f0486ac
Michał Walenciak <Kicer86@gmail.com>
2017-12-22 18:10:04 +0100

    Revert "Do not run unit tests for gui when there is no display"
    This reverts commit 99ff10efe2c429db198bc506b6f3507e09feca6b.

--- src/gui/unit_tests/main.cpp
+++ src/gui/unit_tests/main.cpp
@@ -5,16 +5,7 @@
 
 int main(int argc, char** argv)
 {
-    int result = 0;
-    QScreen* screen = QGuiApplication::primaryScreen();
-
-    if (screen != nullptr)
-    {
-        QGuiApplication app(argc, argv);
-        ::testing::InitGoogleMock(&argc, argv);
-
-        result = RUN_ALL_TESTS();
-    }
-
-    return result;
+    QGuiApplication app(argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
 }
