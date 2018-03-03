41cc4d83aa8be1e28b2e7bbf146039bb294033fe
Michał Walenciak <Kicer86@gmail.com>
2018-03-03 10:22:30 +0100

    more RAII + const

--- src/face_recognition/face_recognition.cpp
+++ src/face_recognition/face_recognition.cpp
@@ -92,7 +92,9 @@ namespace
         PyErr_Fetch(&ptype, &pvalue, &ptraceback);
         PyErr_NormalizeException(&ptype, &pvalue, &ptraceback);
 
-        std::string value_str = ObjectToString(pvalue);
+        PyObjPtr type(ptype), value(pvalue), traceback(ptraceback);
+
+        const std::string value_str = ObjectToString(pvalue);
 
         /* See if we can get a full traceback */
         PyObjPtr module_name( PyUnicode_FromString("traceback") );
@@ -103,7 +105,7 @@ namespace
             PyObjPtr pyth_func( PyObject_GetAttrString(pyth_module.get(), "format_exception") );
             if (pyth_func && PyCallable_Check(pyth_func.get()))
             {
-                PyObjPtr pyth_val( PyObject_CallFunctionObjArgs(pyth_func.get(), ptype, pvalue, ptraceback, NULL) );
+                PyObjPtr pyth_val( PyObject_CallFunctionObjArgs(pyth_func.get(), type.get(), value.get(), traceback.get(), NULL) );
 
                 std::string traceback_str = ObjectToString(pyth_val.get());
             }
