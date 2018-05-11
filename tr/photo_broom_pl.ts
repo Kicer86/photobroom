7f09f11224a83147c7c0e310259474dcfbe5d4d6
Michał Walenciak <Kicer86@gmail.com>
2018-05-11 23:04:58 +0200

    ts update

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -127,7 +127,7 @@ Po zakończeniu odczytu zdjęcia można przejrzeć klikając w tabkę &quot;Nowe
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+46"/>
+        <location line="+52"/>
         <source>Assi&amp;gned people</source>
         <translation type="unfinished"></translation>
     </message>
