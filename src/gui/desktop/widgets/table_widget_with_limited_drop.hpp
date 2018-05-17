fcba58f85d84e24b88e780cd1ab382c858014baf
Michał Walenciak <Kicer86@gmail.com>
2018-05-17 18:14:47 +0200

    introducing TableWidgetWithLimitedDrop

---
+++ src/gui/desktop/widgets/table_widget_with_limited_drop.hpp
@@ -0,0 +1,28 @@
+/*
+ * QTableWidget with limited drop
+ * Copyright (C) 2018  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TABLEWIDGETWITHLIMITEDDROP_HPP
+#define TABLEWIDGETWITHLIMITEDDROP_HPP
+
+#include <QTableWidget>
+
+class TableWidgetWithLimitedDrop : public QTableWidget
+{
+};
+
+#endif // TABLEWIDGETWITHLIMITEDDROP_H
