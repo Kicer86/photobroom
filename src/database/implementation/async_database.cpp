e597a66464bc35735bc40e68875afe7f136f9a7d
Michał Walenciak <Kicer86@gmail.com>
2018-06-04 22:59:07 +0200

    execute db tasks immediately

--- src/database/implementation/async_database.cpp
+++ src/database/implementation/async_database.cpp
@@ -515,7 +515,13 @@ namespace Database
         void addTask(std::unique_ptr<IThreadTask>&& task)
         {
             assert(m_working);
-            m_executor.addTask(std::move(task));
+
+            // When task comes from from db's thread execute it immediately.
+            // This simplifies some client's codes (when operating inside of performCustomAction)
+            if (std::this_thread::get_id() == m_thread.get_id())
+                task->execute(&m_executor);
+            else
+                m_executor.addTask(std::move(task));
         }
 
         void stopExecutor()
