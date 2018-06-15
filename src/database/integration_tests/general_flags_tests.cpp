a42ba6740ef7e6062ae7bc3e49e0edc47e967b42
Michał Walenciak <Kicer86@gmail.com>
2018-06-15 22:43:39 +0200

    photos storage test

--- src/database/integration_tests/general_flags_tests.cpp
+++ src/database/integration_tests/general_flags_tests.cpp
@@ -24,8 +24,27 @@ TEST_F(GeneralFlagsTest, flagsIntroduction)
     {
         db->performCustomAction([](Database::IBackendOperator* op)
         {
+            // store 2 photos
+            Photo::DataDelta pd1, pd2;
+            pd1.data[Photo::Field::Path] = QString("photo1.jpeg");
+            pd2.data[Photo::Field::Path] = QString("photo2.jpeg");
 
+            std::vector<Photo::Id> ids;
+            std::vector<Photo::DataDelta> photos = { pd1, pd2 };
+            op->addPhotos(photos);
+
+            ids.push_back(photos.front().id);
+            ids.push_back(photos.back().id);
+
+            op->set(ids[0], "test1", 1);
+            op->set(ids[0], "test2", 2);
+            op->set(ids[1], "test3", 3);
+            op->set(ids[1], "test4", 4);
+
+            EXPECT_EQ(op->get(ids[0], "test2"), 2);
+            EXPECT_EQ(op->get(ids[0], "test1"), 1);
+            EXPECT_EQ(op->get(ids[1], "test4"), 4);
+            EXPECT_EQ(op->get(ids[1], "test3"), 3);
         });
     });
 }
-
