6793a071bd619a69c84541caa7e3263940a3ce3b
Michał Walenciak <Kicer86@gmail.com>
2018-06-21 19:48:44 +0200

    fixing problems with installation (broken paths)

--- cmake_private/system/linux.cmake
+++ cmake_private/system/linux.cmake
@@ -1,6 +1,6 @@
 
 # install main icon in public area
-install(FILES ${CMAKE_SOURCE_DIR}/images/photo_broom.svg DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/scalable/apps)
+install(FILES ${CMAKE_SOURCE_DIR}/images/photo_broom.svg DESTINATION share/icons/hicolor/scalable/apps)
 
 #install desktop file
-install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/linux/photo_broom.desktop DESTINATION ${CMAKE_INSTALL_PREFIX}/share/applications)
+install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/linux/photo_broom.desktop DESTINATION share/applications)
--- external/dlib.cmake
+++
@@ -1,7 +0,0 @@
-
-file(GLOB libraries
-     LIST_DIRECTORIES false
-     "${PROJECT_BINARY_DIR}/dlib/dlib.cpython-*"
-)
-
-file(INSTALL ${libraries} DESTINATION ${CMAKE_INSTALL_PREFIX}/${PATH_SCRIPTS})
