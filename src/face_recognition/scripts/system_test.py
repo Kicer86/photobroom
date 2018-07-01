84118f6b663bdec14cf53b9656c9f545c316b0f6
Michał Walenciak <Kicer86@gmail.com>
2018-07-01 19:02:33 +0200

    dropping debug messages

--- src/face_recognition/scripts/system_test.py
+++ src/face_recognition/scripts/system_test.py
@@ -8,16 +8,10 @@ def detect_required_modules():
     missing = []
 
     for module in modules:
-        message = "Checking for " + module + " module - ";
         mod_spec = importlib.util.find_spec(module)
         found = mod_spec is not None
 
-        if found:
-            message += "found"
-        else:
-            message += "not found"
+        if not found:
             missing.append(module)
-
-        print(message)
 
     return missing
