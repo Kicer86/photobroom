16411b1996ab1349fb3d8e9b3dddc658c1d369c8
Michał Walenciak <Kicer86@gmail.com>
2018-07-07 23:03:46 +0200

    marking currently used functions as deprecated

--- src/system/system.hpp
+++ src/system/system.hpp
@@ -10,9 +10,9 @@
 struct SYSTEM_EXPORT System
 {
     static QString getApplicationConfigDir();
-    static QString getApplicationTempDir();
-    static QString getTempFilePath();
-    static QString getTempFilePatternFor(const QString& extension);  // just xyz without a dot
+    [[deprecated]] static QString getApplicationTempDir();
+    [[deprecated]] static QString getTempFilePath();
+    [[deprecated]] static QString getTempFilePatternFor(const QString& extension);  // just xyz without a dot
     static std::string findProgram(const std::string& name);         // TODO: is is really necessary?
     static std::string userName();
 };
