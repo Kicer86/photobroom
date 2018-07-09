75433296ddeeabc60e8b2835ca452bdd008665c0
Michał Walenciak <Kicer86@gmail.com>
2018-07-09 23:06:45 +0200

    comments

--- src/system/system.hpp
+++ src/system/system.hpp
@@ -25,7 +25,7 @@ struct SYSTEM_EXPORT System
     static std::string findProgram(const std::string& name);         // TODO: is is really necessary?
     static std::string userName();
     static std::unique_ptr<ITmpDir> getTmpDir(const QString& utility);       // Returns dir in tmp dir for given utility.
-    static QString getTmpFile(ITmpDir *, const QString& fileExt);
+    static QString getTmpFile(ITmpDir *, const QString& fileExt);            // Returns unique file name with given extension in given tmp dir
 };
 
 
