40101b8eb43fa79f80ba41080f395cf6c75889d5
Michał Walenciak <Kicer86@gmail.com>
2018-07-18 22:40:03 +0200

    code extraction

--- src/core/implementation/media_types.cpp
+++ src/core/implementation/media_types.cpp
@@ -16,42 +16,34 @@ namespace
     std::mutex img_regex_mutex;
     std::mutex anim_img_regex_mutex;
     std::mutex vid_regex_mutex;
+
+    bool is(std::mutex& mutex, const QRegExp& regex, const QString& file_path)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+
+        const QFileInfo path(file_path);
+        const QString ext = path.suffix();
+        const bool matches = regex.exactMatch(ext);
+
+        return matches;
+    }
 }
 
 
 namespace MediaTypes
 {
-
     bool isImageFile(const QString& file_path)
     {
-        std::lock_guard<std::mutex> lock(img_regex_mutex);
-
-        const QFileInfo path(file_path);
-        const QString ext = path.suffix();
-        const bool matches = img_regex.exactMatch(ext);
-
-        return matches;
+        return is(img_regex_mutex, img_regex, file_path);
     }
 
     bool isAnimatedImageFile(const QString& file_path)
     {
-        std::lock_guard<std::mutex> lock(anim_img_regex_mutex);
-
-        const QFileInfo path(file_path);
-        const QString ext = path.suffix();
-        const bool matches = anim_img_regex.exactMatch(ext);
-
-        return matches;
+        return is(anim_img_regex_mutex, anim_img_regex, file_path);
     }
 
     bool isVideoFile(const QString& file_path)
     {
-        std::lock_guard<std::mutex> lock(vid_regex_mutex);
-
-        const QFileInfo path(file_path);
-        const QString ext = path.suffix();
-        const bool matches = vid_regex.exactMatch(ext);
-
-        return matches;
+        return is(vid_regex_mutex, vid_regex, file_path);
     }
 }
