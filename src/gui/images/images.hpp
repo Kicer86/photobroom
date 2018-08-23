b16621253a7b9999cb47e7c64213d3a08e3c5b5b
Michał Walenciak <Kicer86@gmail.com>
2018-08-23 22:26:23 +0200

    introducing GIF and HDR images

--- src/gui/images/images.hpp
+++ src/gui/images/images.hpp
@@ -9,6 +9,8 @@ namespace Images
     constexpr const char* const clock   = ":/gui/clock.svg";
     constexpr const char* const error   = ":/gui/error.svg";
     constexpr const char* const video   = ":/gui/video.svg";
+    constexpr const char* const hdr     = ":/gui/hdr.svg";
+    constexpr const char* const gif     = ":/gui/gif.svg";
 }
 
 #endif
