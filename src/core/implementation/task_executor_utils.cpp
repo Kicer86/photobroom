62b250b1ad82de4108ecfc5911a35f99f11ddfcb
Michał Walenciak <Kicer86@gmail.com>
2018-08-26 20:30:48 +0200

    initial implementation of TasksQueue

--- src/core/implementation/task_executor_utils.cpp
+++ src/core/implementation/task_executor_utils.cpp
@@ -1,2 +1,56 @@
 
 #include "task_executor_utils.hpp"
+
+
+TasksQueue::TasksQueue(ITaskExecutor* executor):
+    m_tasksMutex(),
+    m_waitingTasks(),
+    m_tasksExecutor(executor),
+    m_maxTasks(16),
+    m_executingTasks(0)
+{
+
+}
+
+
+void TasksQueue::clean()
+{
+    std::lock_guard<std::mutex> guard(m_tasksMutex);
+    m_waitingTasks.clear();
+}
+
+
+void TasksQueue::try_to_fire()
+{
+    assert(m_tasksMutex.try_lock() == false);  // here we expect mutex to be locked
+
+    // Do not put all waiting tasks to executor.
+    while (m_maxTasks > m_executingTasks && m_waitingTasks.empty() == false)
+    {
+        fire();
+    }
+}
+
+
+void TasksQueue::fire()
+{
+    assert(m_tasksMutex.try_lock() == false);  // here we expect mutex to be locked
+    assert(m_waitingTasks.empty() == false);
+
+    auto task = std::move(m_waitingTasks.front());
+    m_waitingTasks.pop_front();
+
+    m_tasksExecutor->add(std::move(task));
+    m_executingTasks++;
+}
+
+
+void TasksQueue::task_finished()
+{
+    std::lock_guard<std::mutex> guard(m_tasksMutex);
+
+    assert(m_executingTasks > 0);
+    m_executingTasks--;
+
+    try_to_fire();
+}
--- src/core/task_executor_utils.hpp
+++ src/core/task_executor_utils.hpp
@@ -2,7 +2,10 @@
 #ifndef TASK_EXECUTOR_UTILS
 #define TASK_EXECUTOR_UTILS
 
+#include <mutex>
 #include <future>
+
+#include "itask_executor.hpp"
 
 template<typename T, typename E>
 struct ExecutorTraits
@@ -29,5 +32,73 @@ auto evaluate(E* executor, const T& task)
 
     return result;
 }
+
+
+// Helper class.
+// A subqueue for ITaskExecutor.
+// Its purpose is to have a queue of tasks to be executed by executor
+// but controled by client ( can be clean()ed )
+class TasksQueue
+{
+    public:
+
+        TasksQueue(ITaskExecutor *);
+
+        template<typename T>
+        void addTask(T&& callable)
+        {
+            std::lock_guard<std::mutex> guard(m_tasksMutex);
+
+            auto task = std::make_unique<IntTask<T>>(callable, this);
+            m_waitingTasks.push_back(task);
+
+            try_to_fire();
+        }
+
+        void clean();
+
+    private:
+        template<typename T> friend struct IntTask;
+
+        template<typename T>
+        struct IntTask: ITaskExecutor::ITask
+        {
+            IntTask(T&& callable, TasksQueue* queue):
+                m_callable(callable),
+                m_queue(queue)
+            {
+            }
+
+            void notify()
+            {
+                // tell TasksQueue job is done
+                m_queue->task_finished();
+            }
+
+            void perform() override
+            {
+                m_callable();     // client's code
+                notify();         // internal jobs
+            }
+
+            std::string name() override
+            {
+                return "TasksQueue::IntTask";
+            }
+
+            T m_callable;
+            TasksQueue* m_queue;
+        };
+
+        std::mutex m_tasksMutex;
+        std::deque<std::unique_ptr<ITaskExecutor::ITask>> m_waitingTasks;
+        ITaskExecutor* m_tasksExecutor;
+        int m_maxTasks;
+        int m_executingTasks;
+
+        void try_to_fire();
+        void fire();
+        void task_finished();
+};
 
 #endif
