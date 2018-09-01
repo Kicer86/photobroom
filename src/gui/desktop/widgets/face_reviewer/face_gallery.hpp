9cb3a098f2f372c0b00159453294cbd6a4081a24
Michał Walenciak <Kicer86@gmail.com>
2018-09-01 08:02:26 +0200

    introducing FaceGallery

---
+++ src/gui/desktop/widgets/face_reviewer/face_gallery.hpp
@@ -0,0 +1,28 @@
+/*
+ * Widget displaying faces in horizontal line
+ * Copyright (C) 2018  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FACEGALLERY_HPP
+#define FACEGALLERY_HPP
+
+#include <QWidget>
+
+class FaceGallery: public QWidget
+{
+};
+
+#endif // FACEGALLERY_HPP
