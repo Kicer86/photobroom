b95271e40200a556d4602f43058d93e10c552140
Michał Walenciak <Kicer86@gmail.com>
2018-09-18 21:27:43 +0200

    initial implementation taken from ThumbnailGenerator::FromImageTask

--- src/core/image_tools.hpp
+++ src/core/image_tools.hpp
@@ -2,6 +2,18 @@
 #ifndef IMAGE_TOOLS_HPP
 #define IMAGE_TOOLS_HPP
 
+#include <QImage>
 
+struct IExifReader;
+
+namespace Image
+{
+    QImage normalized(const QString &,
+                      IExifReader *);      // returns QImage rotated acordingly to exif data
+
+    void normalize(const QString& src,
+                   const QString& dst,
+                   IExifReader *);         // save 'src' file as 'dst' with rotation data applied
+}
 
 #endif
