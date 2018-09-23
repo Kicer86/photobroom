272629455fd7dd5089751e959c4a924f2dddee13
Michał Walenciak <Kicer86@gmail.com>
2018-09-23 07:17:49 +0200

    introducing PeopleListModel

---
+++ src/gui/desktop/utils/people_list_model.hpp
@@ -0,0 +1,30 @@
+/*
+ * Model keeping list of all people names
+ * Copyright (C) 2018  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PEOPLELISTMODEL_HPP
+#define PEOPLELISTMODEL_HPP
+
+#include <QAbstractListModel>
+
+
+class PeopleListModel: public QAbstractListModel
+{
+
+};
+
+#endif // PEOPLELISTMODEL_HPP
