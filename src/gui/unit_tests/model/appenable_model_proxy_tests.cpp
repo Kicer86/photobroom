943616c21d3bb2170db65970611f2462a8d69bf2
Michał Walenciak <Kicer86@gmail.com>
2018-10-12 21:17:56 +0200

    some initial tests

--- src/gui/unit_tests/model/appenable_model_proxy_tests.cpp
+++ src/gui/unit_tests/model/appenable_model_proxy_tests.cpp
@@ -1,5 +1,8 @@
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <QStandardItemModel>
 
 #include "widgets/tag_editor/helpers/appendable_model_proxy.hpp"
 
@@ -13,7 +16,27 @@ using ::testing::Return;
 TEST(AppendableModelProxyTest, IsConstructable)
 {
     EXPECT_NO_THROW({
-        AppendableModelProxy proxy;
+        AppendableModelProxy proxy(0, nullptr);
     });
 }
 
+
+TEST(AppendableModelProxyTest, NoModelNoData)
+{
+    AppendableModelProxy proxy(2, nullptr);
+
+    EXPECT_EQ(proxy.rowCount(QModelIndex()), 0);
+    EXPECT_EQ(proxy.columnCount(QModelIndex()), 0);
+}
+
+
+TEST(AppendableModelProxyTest, EmptyModelAppendableRow)
+{
+    QStandardItemModel model;
+
+    AppendableModelProxy proxy(2, nullptr);
+    proxy.setSourceModel(&model);
+
+    EXPECT_EQ(proxy.rowCount(QModelIndex()), 1);
+    EXPECT_EQ(proxy.columnCount(QModelIndex()), 2);
+}
