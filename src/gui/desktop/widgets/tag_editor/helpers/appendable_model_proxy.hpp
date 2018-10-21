aa3d5796045020c4d84dc4e49803f8cc17259182
Michał Walenciak <Kicer86@gmail.com>
2018-10-21 22:35:23 +0200

    default values for arugments

--- src/gui/desktop/widgets/tag_editor/helpers/appendable_model_proxy.hpp
+++ src/gui/desktop/widgets/tag_editor/helpers/appendable_model_proxy.hpp
@@ -32,10 +32,10 @@ class AppendableModelProxy: public QAbstractItemModel
         bool appendingEnabled() const;
 
         //QAbstractItemModel overrides:
-        int columnCount(const QModelIndex& parent) const override;
-        int rowCount(const QModelIndex& parent) const override;
+        int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+        int rowCount(const QModelIndex& parent = QModelIndex()) const override;
         QModelIndex parent(const QModelIndex& child) const override;
-        QModelIndex index(int row, int column, const QModelIndex& parent) const override;
+        QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const override;
         Qt::ItemFlags flags(const QModelIndex &index) const override;
         QVariant data(const QModelIndex& index, int role) const override;
         QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
