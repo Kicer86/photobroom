91d13b6b7d58ebcbbae7f1e7fb3570ecc8acb424
Michał Walenciak <Kicer86@gmail.com>
2018-11-14 20:59:53 +0100

    pybind11 possible build fix

--- tools/dependencies/pybind11.cmake
+++ tools/dependencies/pybind11.cmake
@@ -1,6 +1,7 @@
 
 ExternalProject_Add(pybind11
     GIT_REPOSITORY https://github.com/pybind/pybind11.git
+    GIT_TAG v2.2.4
     PREFIX ${PROJECT_BINARY_DIR}
     CMAKE_ARGS ${COMMON_OPTIONS} -DPYBIND11_TEST=OFF
 )
