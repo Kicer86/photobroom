c7a043a748342d70a19d4a0ca4ad7e339b414d71
Michał Walenciak <Kicer86@gmail.com>
2019-01-03 19:49:22 +0100

    dropping deprecated functions

--- src/system/implementation/system.cpp
+++ src/system/implementation/system.cpp
@@ -72,34 +72,6 @@ QString System::getApplicationConfigDir()
 }
 
 
-QString System::getApplicationTempDir()
-{
-    static QTemporaryDir tmpDir;
-
-    return tmpDir.path();
-}
-
-
-QString System::getTempFilePath()
-{
-    static int v = 0;
-
-    const QString result = QString("%1/%2").arg(getApplicationTempDir()).arg(v++, 6, 16, QLatin1Char('0'));
-
-    return result;
-}
-
-
-QString System::getTempFilePatternFor(const QString& extension)
-{
-    const QString tmp_name = QDir::tempPath() + "/" +
-                             QCoreApplication::applicationName() +
-                             "-XXXXXX." + extension;
-
-    return tmp_name;
-}
-
-
 std::string System::findProgram(const std::string& name)
 {
     const QString path = QStandardPaths::findExecutable(name.c_str());
--- src/system/system.hpp
+++ src/system/system.hpp
@@ -19,9 +19,7 @@ struct ITmpDir
 struct SYSTEM_EXPORT System
 {
     static QString getApplicationConfigDir();
-    [[deprecated]] static QString getApplicationTempDir();
-    [[deprecated]] static QString getTempFilePath();
-    [[deprecated]] static QString getTempFilePatternFor(const QString& extension);  // just xyz without a dot
+
     static std::string findProgram(const std::string& name);         // TODO: is is really necessary?
     static std::string userName();
 
