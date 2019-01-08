924468ef6eba87ae7c7247fc64d8a50956edded4
Michał Walenciak <Kicer86@gmail.com>
2019-01-08 17:37:28 +0100

    hide base
    Due to visual studio's implementation of algorithms
    iterator_wrapper cannot inherite from base iterator
    as visual will detect it and will use some implementation
    specific calls to base iterator ommiting operator*()

--- src/core/iterator_wrapper.hpp
+++ src/core/iterator_wrapper.hpp
@@ -3,33 +3,48 @@
 #define ITERATOR_WRAPPER_HPP
 
 template<typename R, typename B, typename T>
-struct iterator_wrapper: B
+class iterator_wrapper: private B
 {
-    iterator_wrapper(): B(), m_operation()
-    {
+        typedef iterator_wrapper<R, B, T> iterator;
 
-    }
+    public:
+        iterator_wrapper(): B(), m_operation()
+        {
 
-    iterator_wrapper(const B& base): B(base), m_operation()
-    {
+        }
 
-    }
+        iterator_wrapper(const B& base): B(base), m_operation()
+        {
 
-    iterator_wrapper(const iterator_wrapper &) = default;
+        }
 
-    ~iterator_wrapper()
-    {
-    }
+        iterator_wrapper(const iterator_wrapper &) = default;
 
-    R operator*() const
-    {
-        return m_operation(*this);
-    }
+        ~iterator_wrapper()
+        {
+        }
 
-    R operator*() 
-    {
-        return m_operation(*this);
-    }
+        R operator*() const
+        {
+            return m_operation(*this);
+        }
+
+        R operator*()
+        {
+            return m_operation(*this);
+        }
+
+        iterator operator++()
+        {
+            return B::operator++();
+        }
+
+        template<typename O>
+        bool operator==(const O& other) const
+        {
+            const B* base = this;
+            return *base == other;
+        }
 
     private:
         T m_operation;
