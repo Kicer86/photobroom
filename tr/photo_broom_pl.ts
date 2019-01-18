e213bd3a5865e46fa8ada278654f8a2371bb5d18
Michał Walenciak <Kicer86@gmail.com>
2019-01-18 22:51:49 +0100

    ts update

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -122,7 +122,7 @@ Po zakończeniu odczytu zdjęcia można przejrzeć klikając w tabkę &quot;Nowe
         <translation>Znajdź lepsze</translation>
     </message>
     <message numerus="yes">
-        <location line="+20"/>
+        <location line="+19"/>
         <source>On %n photo(s)</source>
         <translation>
             <numerusform>Na %n zdjęciu</numerusform>
@@ -920,7 +920,7 @@ Podana lokalizacja może już zawierać zdjęcia - zostaną one dodane do kolekc
 <context>
     <name>TestSystem</name>
     <message>
-        <location filename="../src/gui/desktop/utils/people_operator.cpp" line="+478"/>
+        <location filename="../src/gui/desktop/utils/people_operator.cpp" line="+488"/>
         <source>Automatic face detection and people recognition functionality is disabled due to missing Python modules.
 
 To install them, use &apos;python -m pip install %1&apos; (on Windows)
