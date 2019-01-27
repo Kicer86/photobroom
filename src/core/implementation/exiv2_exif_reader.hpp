4ed18ce489d3a333a0642cb0cb24824a92b9b50f
Michał Walenciak <Kicer86@gmail.com>
2019-01-27 15:00:33 +0100

    recognice proper type of Exiv's ptr (by Michał S)

--- src/core/implementation/exiv2_exif_reader.hpp
+++ src/core/implementation/exiv2_exif_reader.hpp
@@ -9,6 +9,38 @@
 
 #include "aexif_reader.hpp"
 
+
+template<typename, typename = void>
+struct has_auto_ptr_type : std::false_type
+{
+    using ptr_type = int;
+};
+
+template<typename T>
+struct has_auto_ptr_type<T, std::void_t<typename T::AutoPtr>> : std::true_type
+{
+    using ptr_type = typename T::AutoPtr;
+};
+
+template<typename, typename = void>
+struct has_unique_ptr_type : std::false_type
+{
+    using ptr_type = int;
+};
+
+template<typename T>
+struct has_unique_ptr_type<T, std::void_t<typename T::UniquePtr>> : std::true_type
+{
+    using ptr_type = typename T::UniquePtr;
+};
+
+template <typename T>
+struct Exiv2Helper
+{
+    using Ptr = typename std::conditional<has_unique_ptr_type<T>::value, typename has_unique_ptr_type<T>::ptr_type, typename has_auto_ptr_type<T>::ptr_type>::type;
+};
+
+
 class Exiv2ExifReader: public AExifReader
 {
     public:
@@ -24,7 +56,7 @@ class Exiv2ExifReader: public AExifReader
         virtual void collect(const QString &) override;
         virtual std::string read(TagType) const override;
 
-        Exiv2::Image::UniquePtr m_exif_data;
+        Exiv2Helper<Exiv2::Image>::Ptr m_exif_data;
         QString m_path;
 };
 
