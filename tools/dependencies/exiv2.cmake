e4e8cbd5860a72e57945d98db404fd74760731a0
Michał Walenciak <Kicer86@gmail.com>
2019-02-24 14:05:00 +0100

    removing internal tool for dependencies management. Use conan

--- tools/dependencies/exiv2.cmake
+++
@@ -1,8 +0,0 @@
-
-ExternalProject_Add(exiv2
-    DEPENDS zlib expat
-    GIT_REPOSITORY https://github.com/Kicer86/exiv2.git
-    GIT_TAG unique_ptr
-    PREFIX ${PROJECT_BINARY_DIR}
-    CMAKE_ARGS ${COMMON_OPTIONS}
-)
--- tools/dependencies/expat.cmake
+++
@@ -1,7 +0,0 @@
-
-ExternalProject_Add(expat
-    GIT_REPOSITORY https://github.com/libexpat/libexpat
-    PREFIX ${PROJECT_BINARY_DIR}
-    CMAKE_ARGS ${COMMON_OPTIONS} -DBUILD_tests=FALSE -DCMAKE_DEBUG_POSTFIX=
-    SOURCE_SUBDIR expat
-)
