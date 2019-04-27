5381f8f35899a37bf30127b125a9e98278f1865b
Michał Walenciak <Kicer86@gmail.com>
2019-04-27 17:28:23 +0200

    introducing GroupsManager

---
+++ src/gui/desktop/utils/groups_manager.hpp
@@ -0,0 +1,29 @@
+/*
+ * High level groups management
+ * Copyright (C) 2019  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GROUPS_MANAGER_HPP
+#define GROUPS_MANAGER_HPP
+
+
+class GroupsManager
+{
+    public:
+        GroupsManager();
+};
+
+#endif // GROUPS_MANAGER_HPP
