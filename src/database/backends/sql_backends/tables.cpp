f941c4b2e33dab9948388a05ef812fdcb0fdf897
Michał Walenciak <Kicer86@gmail.com>
2019-05-28 22:18:35 +0200

    introducing photos_change_log table

--- src/database/backends/sql_backends/tables.cpp
+++ src/database/backends/sql_backends/tables.cpp
@@ -171,6 +171,18 @@ namespace Database
                             }
         );
 
+        TableDefinition
+        table_photos_change_log(TAB_PHOTOS_CHANGE_LOG,
+                            {
+                                { "id", "", ColDefinition::Purpose::ID },
+                                { "photo_id", "INTEGER NOT NULL"       },
+                                { "action", "INTEGER"                  },       // add/delete/modify
+                                { "field", "CHAR(64)"                  },       // tag? flag? person?
+                                { "data",  QString("VARCHAR(%1)").arg(ConfigConsts::Constraints::database_tag_value_len) },
+                                { "FOREIGN KEY(photo_id) REFERENCES " TAB_PHOTOS "(id)", ""  },
+                            }
+        );
+
         //all tables
         std::map<std::string, TableDefinition> tables =
         {
@@ -186,5 +198,6 @@ namespace Database
             { TAB_PEOPLE_NAMES,         table_people },
             { TAB_PEOPLE,               table_people_locations },
             { TAB_GENERAL_FLAGS,        table_general_flags },
+            { TAB_PHOTOS_CHANGE_LOG,    table_photos_change_log },
         };
 }
