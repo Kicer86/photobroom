d4e2944436eb2f112608913ccce0b209cc685827
Michał Walenciak <Kicer86@gmail.com>
2019-06-14 19:24:37 +0200

    simplifying PhotoInfo

--- src/database/implementation/photo_info.cpp
+++ src/database/implementation/photo_info.cpp
@@ -14,28 +14,12 @@
 #include "photo_data.hpp"
 #include "idatabase.hpp"
 
-struct PhotoInfo::Data
-{
-    Data():
-        m_data(),
-        m_valid(true)
-    {
-    }
-
-    Data(const Data &) = delete;
-    Data& operator=(const Data &) = delete;
-
-    ol::ThreadSafeResource<Photo::Data> m_data;
-    bool m_valid;
-};
-
 
 PhotoInfo::PhotoInfo(const Photo::Data& data, Database::IDatabase* storekeeper):
-    m_data(nullptr),
+    m_data(data),
     m_storekeeper(storekeeper)
 {
-    m_data = std::make_unique<Data>();
-    *m_data->m_data.lock() = data;
+
 }
 
 
@@ -47,25 +31,25 @@ PhotoInfo::~PhotoInfo()
 
 Photo::Data PhotoInfo::data() const
 {
-    return m_data->m_data.lock().get();
+    return m_data.lock().get();
 }
 
 
 QString PhotoInfo::getPath() const
 {
-    return m_data->m_data.lock()->path;
+    return m_data.lock()->path;
 }
 
 
 Tag::TagsList PhotoInfo::getTags() const
 {
-    return m_data->m_data.lock()->tags;
+    return m_data.lock()->tags;
 }
 
 
 QSize PhotoInfo::getGeometry() const
 {
-    return m_data->m_data.lock()->geometry;
+    return m_data.lock()->geometry;
 }
 
 
@@ -73,13 +57,13 @@ Photo::Sha256sum PhotoInfo::getSha256() const
 {
     assert(isSha256Loaded());
 
-    return m_data->m_data.lock()->sha256Sum;
+    return m_data.lock()->sha256Sum;
 }
 
 
 Photo::Id PhotoInfo::getID() const
 {
-    return m_data->m_data.lock()->id;
+    return m_data.lock()->id;
 }
 
 
@@ -109,7 +93,7 @@ bool PhotoInfo::isExifDataLoaded() const
 
 void PhotoInfo::setData(const Photo::Data& data)
 {
-    auto locked_data = m_data->m_data.lock();
+    auto locked_data = m_data.lock();
 
     *locked_data = data;
 }
@@ -119,7 +103,7 @@ void PhotoInfo::setSha256(const Photo::Sha256sum& sha256)
 {
     assert(isSha256Loaded() == false);
 
-    auto data = m_data->m_data.lock();
+    auto data = m_data.lock();
     data->sha256Sum = sha256;
     data->flags[Photo::FlagsE::Sha256Loaded] = 1;
 
@@ -134,7 +118,7 @@ void PhotoInfo::setGeometry(const QSize& geometry)
 {
     assert(isGeometryLoaded() == false);
 
-    auto data = m_data->m_data.lock();
+    auto data = m_data.lock();
     data->geometry = geometry;
     data->flags[Photo::FlagsE::GeometryLoaded] = 1;
 
@@ -147,7 +131,7 @@ void PhotoInfo::setGeometry(const QSize& geometry)
 
 void PhotoInfo::setTags(const Tag::TagsList& tags)
 {
-    auto data = m_data->m_data.lock();
+    auto data = m_data.lock();
 
     // copy only non empty tags to data->tags
     auto data_tags_ins = std::inserter(data->tags, data->tags.end());
@@ -164,7 +148,7 @@ void PhotoInfo::setTags(const Tag::TagsList& tags)
 
 void PhotoInfo::setTag(const TagNameInfo& name, const TagValue& value)
 {
-    auto data = m_data->m_data.lock();
+    auto data = m_data.lock();
 
     if (value.type() == TagValue::Type::Empty)
         data->tags.erase(name);
@@ -179,7 +163,7 @@ void PhotoInfo::setTag(const TagNameInfo& name, const TagValue& value)
 
 void PhotoInfo::setGroup(const GroupInfo& info)
 {
-    auto data = m_data->m_data.lock();
+    auto data = m_data.lock();
     data->groupInfo = info;
 
     Photo::DataDelta delta(data->id);
@@ -190,9 +174,9 @@ void PhotoInfo::setGroup(const GroupInfo& info)
 
 void PhotoInfo::markFlag(Photo::FlagsE flag, int v)
 {
-    m_data->m_data.lock()->flags[flag] = v;
+    m_data.lock()->flags[flag] = v;
 
-    auto data = m_data->m_data.lock();
+    auto data = m_data.lock();
     data->flags[flag] = v;
 
     Photo::DataDelta delta(data->id);
@@ -203,7 +187,7 @@ void PhotoInfo::markFlag(Photo::FlagsE flag, int v)
 
 int PhotoInfo::getFlag(Photo::FlagsE flag) const
 {
-    auto flags = m_data->m_data.lock()->flags;
+    auto flags = m_data.lock()->flags;
     auto it = flags.find(flag);
     const int result = it == flags.end()? 0: it->second;
 
@@ -213,11 +197,11 @@ int PhotoInfo::getFlag(Photo::FlagsE flag) const
 
 void PhotoInfo::invalidate()
 {
-    m_data->m_valid = false;
+    m_valid = false;
 }
 
 
 bool PhotoInfo::isValid()
 {
-    return m_data->m_valid;
+    return m_valid;
 }
