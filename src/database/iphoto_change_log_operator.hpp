0d7fadb092eff82dee31ce4975b021db19eda321
Michał Walenciak <Kicer86@gmail.com>
2019-06-15 22:08:18 +0200

    adding dumpChangeLog to interface

--- src/database/iphoto_change_log_operator.hpp
+++ src/database/iphoto_change_log_operator.hpp
@@ -2,10 +2,17 @@
 #ifndef IPHOTO_CHANGE_LOG_OPERATOR_HPP
 #define IPHOTO_CHANGE_LOG_OPERATOR_HPP
 
+
+#include <QStringList>
+
 namespace Database
 {
     struct IPhotoChangeLogOperator
     {
+        virtual ~IPhotoChangeLogOperator() = default;
+
+        // for debug / tests
+        virtual QStringList dumpChangeLog() = 0;
     };
 }
 
