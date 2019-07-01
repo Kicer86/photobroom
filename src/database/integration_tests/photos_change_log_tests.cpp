fceeba773d0bec3887882a3309e223e97ebea358
Michał Walenciak <Kicer86@gmail.com>
2019-07-01 19:30:08 +0200

    adding partial test for groups

--- src/database/integration_tests/photos_change_log_tests.cpp
+++ src/database/integration_tests/photos_change_log_tests.cpp
@@ -67,3 +67,50 @@ TEST_F(PhotosChangeLog, tagsManipulation)
     });
 }
 
+
+TEST_F(PhotosChangeLog, groupsManipulation)
+{
+    for_all_db_plugins([](Database::IDatabase* db)
+    {
+        db->exec([](Database::IBackend* op)
+        {
+            // add some photos
+            Photo::DataDelta pd0,pd1,pd2,pd3,pd4,pd5;
+
+            pd0.insert<Photo::Field::Path>("photo0.jpeg");
+            pd1.insert<Photo::Field::Path>("photo1.jpeg");
+            pd2.insert<Photo::Field::Path>("photo2.jpeg");
+            pd3.insert<Photo::Field::Path>("photo1.jpeg");
+            pd4.insert<Photo::Field::Path>("photo4.jpeg");
+            pd5.insert<Photo::Field::Path>("photo5.jpeg");
+
+            std::vector<Photo::DataDelta> photos = { pd0,pd1,pd2,pd3,pd4,pd5 };
+            ASSERT_TRUE(op->addPhotos(photos));
+
+            // create groups
+            const Group::Id gr1 = op->groupOperator()->addGroup(photos[0].getId(), Group::Animation);
+            const Group::Id gr2 = op->groupOperator()->addGroup(photos[3].getId(), Group::Animation);
+
+            // read photo structure
+            Photo::DataDelta data_delta1(photos[1].getId());
+            Photo::DataDelta data_delta2(photos[2].getId());
+
+            // add to group
+            const GroupInfo gr_info1 = { .group_id = gr1, .role = GroupInfo::Member };
+            data_delta1.insert<Photo::Field::GroupInfo>(gr_info1);
+            op->update(data_delta1);
+
+            const GroupInfo gr_info2 = { .group_id = gr1, .role = GroupInfo::Member };
+            data_delta2.insert<Photo::Field::GroupInfo>(gr_info2);
+            op->update(data_delta2);
+
+            // verify change log
+            const QStringList changeLog = op->photoChangeLogOperator()->dumpChangeLog();
+
+            ASSERT_EQ(changeLog.size(), 2);
+            EXPECT_EQ(changeLog[0], "photo id: 2. Group added. 1: 2");
+            EXPECT_EQ(changeLog[1], "photo id: 3. Group added. 1: 2");
+
+        });
+    });
+}
