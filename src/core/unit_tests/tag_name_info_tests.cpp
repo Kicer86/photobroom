c7d8628cf1e8d5b0ecbc604244100f94542e04ab
Michał Walenciak <Kicer86@gmail.com>
2019-07-21 15:44:00 +0200

    adding TagNameInfoTest tests

--- src/core/unit_tests/tag_name_info_tests.cpp
+++ src/core/unit_tests/tag_name_info_tests.cpp
@@ -2,11 +2,12 @@
 #include <gmock/gmock.h>
 
 #include "tag.hpp"
+#include "base_tags.hpp"
 
 
 TEST(TagNameInfoTest, emptyAfterConstruction)
 {
-    TagNameInfo info;
+    const TagNameInfo info;
 
     EXPECT_EQ(info.getName(), QString());
     EXPECT_EQ(info.getDisplayName(), QString());
@@ -14,6 +15,44 @@ TEST(TagNameInfoTest, emptyAfterConstruction)
     EXPECT_EQ(info.getTag(), BaseTagsList::Invalid);
 }
 
+
+TEST(TagNameInfoTest, copyOperation)
+{
+    const TagNameInfo info(BaseTagsList::Date);
+    const TagNameInfo info2(info);
+
+    TagNameInfo info3;
+    info3 = info;
+
+    EXPECT_EQ(info, info2);
+    EXPECT_EQ(info, info3);
+}
+
+
+TEST(TagNameInfoTest, compareOperation)
+{
+    const auto tags = BaseTags::getAll();
+    const std::size_t s = tags.size();
+
+    for (std::size_t i = 0; i < s; i++)
+        for (std::size_t j = 0; j < s; j++)
+        {
+            const TagNameInfo l(tags[i]);
+            const TagNameInfo r(tags[j]);
+
+            if (i == j)
+            {
+                EXPECT_TRUE(l == r);
+                EXPECT_FALSE(l < r);
+                EXPECT_FALSE(l > r);
+            }
+            else
+            {
+                EXPECT_FALSE(l == r);
+                EXPECT_NE(l < r, l > r);
+            }
+        }
+}
 
 typedef std::pair<BaseTagsList, TagNameInfo::Type> TagNameInfoExpectations;
 struct TagNameInfoTest2: testing::TestWithParam<TagNameInfoExpectations> {};
