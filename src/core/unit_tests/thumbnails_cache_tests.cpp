c3c95eb632fc6d0ea6f1fdbf7389680bcc8731b5
Michał Walenciak <Kicer86@gmail.com>
2019-08-31 13:19:49 +0200

    verify no invalid resuls are returnes

--- src/core/unit_tests/thumbnails_cache_tests.cpp
+++ src/core/unit_tests/thumbnails_cache_tests.cpp
@@ -7,3 +7,16 @@ TEST(ThumbnailsCacheTest, isConstructible)
 {
     ThumbnailsCache cache;
 }
+
+TEST(ThumbnailsCacheTest, returnsNothingWhenEmpty)
+{
+    ThumbnailsCache cache;
+
+    std::optional img1 = cache.find("", 0);
+    std::optional img2 = cache.find("/", 100);
+    std::optional img3 = cache.find(".", -100);
+
+    EXPECT_FALSE(img1.has_value());
+    EXPECT_FALSE(img2.has_value());
+    EXPECT_FALSE(img3.has_value());
+}
