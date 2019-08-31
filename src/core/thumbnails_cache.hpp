f2f36424d7ffe8dec10d4853569ad9686a360878
Michał Walenciak <Kicer86@gmail.com>
2019-08-31 13:10:16 +0200

    introducing simple cache for thumbnails

---
+++ src/core/thumbnails_cache.hpp
@@ -0,0 +1,31 @@
+/*
+ * Simple memory cache for thumbnails.
+ * Copyright (C) 2019  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef THUMBNAILS_CACHE_HPP
+#define THUMBNAILS_CACHE_HPP
+
+#include "ithumbnails_cache.hpp"
+
+
+class ThumbnailsCache : public IThumbnailsCache
+{
+    public:
+        void store(const QString&, int , const QImage& ) override;
+};
+
+#endif
