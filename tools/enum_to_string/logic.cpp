89fb3e05ddaab0ae5e7263fa7a74d393cc1b5ffa
Michał Walenciak <Kicer86@gmail.com>
2019-09-14 16:29:11 +0200

    handle whitespaces before word

--- tools/enum_to_string/logic.cpp
+++ tools/enum_to_string/logic.cpp
@@ -2,16 +2,38 @@
 #include <cstring>
 #include <fstream>
 
+namespace
+{
+    const char* const whitespaces = "\n\r\t ";
+}
+
 std::string read_word(std::istream& input)
 {
     std::string word;
 
+    // skip all whilespaces
     while(input.good())
     {
         const auto ch = input.get();
         if (ch != std::ifstream::traits_type::eof())
         {
-            if (strchr("\n \t", ch))
+            if (strchr(whitespaces, ch))
+                continue;
+            else
+            {
+                input.unget();
+                break;
+            }
+        }
+    }
+
+    // put all but whitespaces to 'word'
+    while(input.good())
+    {
+        const auto ch = input.get();
+        if (ch != std::ifstream::traits_type::eof())
+        {
+            if (strchr(whitespaces, ch))
                 break;
             else
                 word += static_cast<char>(ch);
