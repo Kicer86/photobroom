d39d1aa460ef887e1ba32ae166afac8274835afb
Michał Walenciak <Kicer86@gmail.com>
2019-09-28 11:47:23 +0200

    initial work with code switch to flex and bison

--- tools/enum_to_string/logic.cpp
+++ tools/enum_to_string/logic.cpp
@@ -3,6 +3,11 @@
 #include <fstream>
 #include <vector>
 
+
+#include "enum.tab.hh"
+#include "enum.yy.hh"
+
+
 struct Enum
 {
     std::string name;
@@ -10,100 +15,27 @@ struct Enum
 };
 
 
-namespace
-{
-    class EnumStateMachine
-    {
-        public:
-            void take(const std::string& word)
-            {
-                switch (m_state)
-                {
-                    case WaitingForEnum:
-                        if (word == "enum")
-                        {
-                            m_enums.emplace_back(Enum{});
-                            m_state = WaitingForClassOrName;
-                        }
-                        break;
-
-                    case WaitingForClassOrName:
-                        if (word == "class")
-                            break;
-
-                        m_enums.back().name = word;
-                        m_state = WaitingForBody;
-                        break;
-                }
-            }
-
-            const std::vector<Enum>& enums() const
-            {
-                return m_enums;
-            }
-
-        private:
-            enum
-            {
-                WaitingForEnum,
-                WaitingForClassOrName,
-                WaitingForBody,
-            } m_state = WaitingForEnum;
-
-            std::vector<Enum> m_enums;
-    };
-}
-
-
-std::string read_word(std::istream& input)
-{
-    std::string word;
-
-    // skip all whilespaces
-    while(input.good())
-    {
-        const auto ch = input.get();
-        if (ch != std::ifstream::traits_type::eof())
-        {
-            if (isspace(ch))
-                continue;
-            else
-            {
-                input.unget();
-                break;
-            }
-        }
-    }
-
-    // put all but whitespaces to 'word'
-    while(input.good())
-    {
-        const auto ch = input.get();
-        if (ch != std::ifstream::traits_type::eof())
-        {
-            if (isspace(ch))
-                break;
-            else
-                word += static_cast<char>(ch);
-        }
-    }
-
-    return word;
-}
-
-
 std::vector<Enum> find_enum(std::istream& data)
 {
-    EnumStateMachine state_machine;
+  YY_BUFFER_STATE bp;
 
-    while(data.good())
-    {
-        const std::string w = read_word(data);
+#if defined YYDEBUG && YYDEBUG==1
+  enum_debug=0;
+#endif
 
-        state_machine.take(w);
-    }
+  const std::string stream(std::istreambuf_iterator<char>(data), {});
 
-    return state_machine.enums();
+  yyscan_t scanner;
+  enum_lex_init_extra(nullptr, &scanner);
+
+  bp=enum__scan_string(stream.data(), scanner);
+  enum__switch_to_buffer(bp, scanner);
+  enum_parse(scanner);
+
+  enum__delete_buffer(bp, scanner);
+  enum_lex_destroy(scanner);
+
+  return {};
 }
 
 
--- tools/enum_to_string/unit_test.cpp
+++ tools/enum_to_string/unit_test.cpp
@@ -4,54 +4,6 @@
 
 #include "logic.cpp"
 
-TEST(WordReaderTest, aFewWords)
-{
-    std::stringstream str("a ab abc");
-
-    const std::string w1 = read_word(str);
-    const std::string w2 = read_word(str);
-    const std::string w3 = read_word(str);
-
-    EXPECT_EQ(w1, "a");
-    EXPECT_EQ(w2, "ab");
-    EXPECT_EQ(w3, "abc");
-}
-
-
-TEST(WordReaderTest, emptyInput)
-{
-    std::stringstream str;
-
-    const std::string w1 = read_word(str);
-
-    EXPECT_TRUE(w1.empty());
-}
-
-
-TEST(WordReaderTest, oneWord)
-{
-    std::stringstream str("qwerty");
-
-    const std::string w1 = read_word(str);
-
-    EXPECT_EQ(w1, "qwerty");
-}
-
-
-TEST(WordReaderTest, manyWhitespaces)
-{
-    std::stringstream str("   \n\tabc\t\t\r\ndef\r\n\r\n \t");
-
-    const std::string w1 = read_word(str);
-    const std::string w2 = read_word(str);
-
-    EXPECT_EQ(w1, "abc");
-    EXPECT_EQ(w2, "def");
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-
 
 TEST(EnumFinderTest, emptyEnum)
 {
@@ -75,3 +27,24 @@ TEST(EnumFinderTest, emptyEnumClass)
     EXPECT_EQ(r.front().name, "E");
     EXPECT_TRUE(r.front().entries.empty());
 }
+
+/*
+TEST(EnumFinderTest, enumWithEntries)
+{
+    std::stringstream str("enum E {a, b, c};");
+
+    auto r = find_enum(str);
+
+    ASSERT_EQ(r.size(), 1);
+
+    const Enum& e = r.front();
+    EXPECT_EQ(e.name, "E");
+
+    const auto& ents = e.entries;
+    ASSERT_EQ(ents.size(), 3);
+
+    EXPECT_EQ(ents[0], "a");
+    EXPECT_EQ(ents[1], "b");
+    EXPECT_EQ(ents[2], "c");
+}
+*/
