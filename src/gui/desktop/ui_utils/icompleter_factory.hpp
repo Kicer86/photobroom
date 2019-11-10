dc137e1e9e80579ecccca754c468fe387a9a32b4
Michał Walenciak <Kicer86@gmail.com>
2019-11-10 12:46:42 +0100

    docs update

--- src/gui/desktop/ui_utils/icompleter_factory.hpp
+++ src/gui/desktop/ui_utils/icompleter_factory.hpp
@@ -10,9 +10,22 @@ struct ICompleterFactory
 {
     virtual ~ICompleterFactory() {}
 
+    /**
+     * @brief create QCompleter for given type
+     * \return Pointer to QCompleter. It is client's responsibility to delete it
+     */
     virtual QCompleter* createCompleter(const TagTypes &) = 0;
+
+    /**
+     * @brief create QCompleter for given set of types
+     * \return Pointer to QCompleter. It is client's responsibility to delete it
+     */
     virtual QCompleter* createCompleter(const std::set<TagTypes> &) = 0;
 
+    /**
+     * @brief create QCompleter with people names
+     * \return Pointer to QCompleter. It is client's responsibility to delete it
+     */
     virtual QCompleter* createPeopleCompleter() = 0;
 };
 
