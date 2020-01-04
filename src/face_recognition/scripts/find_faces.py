086bd17061575fb17c9ab2f1f86cf55ee499ccfb
Michał Walenciak <Kicer86@gmail.com>
2020-01-04 15:10:55 +0100

    dropping useless script

--- src/face_recognition/scripts/find_faces.py
+++
@@ -1,9 +0,0 @@
-
-import face_recognition
-
-
-def find_faces(path):
-    image = face_recognition.load_image_file(path)
-    face_locations = face_recognition.face_locations(image)
-
-    return face_locations
