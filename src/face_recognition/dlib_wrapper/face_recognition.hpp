be05c859caf25f1925414aa261914c52dc41b43b
Michał Walenciak <Kicer86@gmail.com>
2020-01-05 22:13:36 +0100

    preparing files for face_recognition_model_v1

---
+++ src/face_recognition/dlib_wrapper/face_recognition.hpp
@@ -0,0 +1,29 @@
+/*
+ * Photo Broom - photos management tool.
+ * Copyright (C) 2020  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FACE_RECOGNITION_MODEL_V1_HPP
+#define FACE_RECOGNITION_MODEL_V1_HPP
+
+/**
+ * @todo write docs
+ */
+class face_recognition_model_v1
+{
+};
+
+#endif // FACE_RECOGNITION_MODEL_V1_HPP
