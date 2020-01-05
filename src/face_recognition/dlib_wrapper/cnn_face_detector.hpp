4a4632d9eeb8dda8a569778a428ead515dc55cd3
Michał Walenciak <Kicer86@gmail.com>
2020-01-05 22:11:38 +0100

    enclosing code in namespace

--- src/face_recognition/dlib_wrapper/cnn_face_detector.hpp
+++ src/face_recognition/dlib_wrapper/cnn_face_detector.hpp
@@ -29,29 +29,33 @@ class QImage;
  * @brief face detection tool
  * Based on https://github.com/davisking/dlib/blob/6b581d91f6b9b847a8163420630ef947e7cc88db/tools/python/src/cnn_face_detector.cpp
  */
-class cnn_face_detection_model_v1
+
+namespace dlib_api
 {
+    class cnn_face_detection_model_v1
+    {
 
-public:
+    public:
 
-    cnn_face_detection_model_v1(const std::string& model_filename);
+        cnn_face_detection_model_v1(const std::string& model_filename);
 
-    std::vector<dlib::mmod_rect> detect (
-        const QImage& qimage,
-        const int upsample_num_times
-    );
+        std::vector<dlib::mmod_rect> detect (
+            const QImage& qimage,
+            const int upsample_num_times
+        );
 
-private:
+    private:
 
-    template <long num_filters, typename SUBNET> using con5d = dlib::con<num_filters,5,5,2,2,SUBNET>;
-    template <long num_filters, typename SUBNET> using con5  = dlib::con<num_filters,5,5,1,1,SUBNET>;
+        template <long num_filters, typename SUBNET> using con5d = dlib::con<num_filters,5,5,2,2,SUBNET>;
+        template <long num_filters, typename SUBNET> using con5  = dlib::con<num_filters,5,5,1,1,SUBNET>;
 
-    template <typename SUBNET> using downsampler  = dlib::relu<dlib::affine<con5d<32, dlib::relu<dlib::affine<con5d<32, dlib::relu<dlib::affine<con5d<16,SUBNET>>>>>>>>>;
-    template <typename SUBNET> using rcon5  = dlib::relu<dlib::affine<con5<45,SUBNET>>>;
+        template <typename SUBNET> using downsampler  = dlib::relu<dlib::affine<con5d<32, dlib::relu<dlib::affine<con5d<32, dlib::relu<dlib::affine<con5d<16,SUBNET>>>>>>>>>;
+        template <typename SUBNET> using rcon5  = dlib::relu<dlib::affine<con5<45,SUBNET>>>;
 
-    using net_type = dlib::loss_mmod<dlib::con<1,9,9,1,1,rcon5<rcon5<rcon5<downsampler<dlib::input_rgb_image_pyramid<dlib::pyramid_down<6>>>>>>>>;
+        using net_type = dlib::loss_mmod<dlib::con<1,9,9,1,1,rcon5<rcon5<rcon5<downsampler<dlib::input_rgb_image_pyramid<dlib::pyramid_down<6>>>>>>>>;
 
-    net_type net;
-};
+        net_type net;
+    };
+}
 
 #endif // CNN_FACE_DETECTION_MODEL_V1_H
