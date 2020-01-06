7f0917fce0ca16549d3fc4b4dca004f2f4c34466
Michał Walenciak <Kicer86@gmail.com>
2020-01-06 10:35:55 +0100

    moving heavy code out of header file

--- src/face_recognition/dlib_wrapper/cnn_face_detector.cpp
+++ src/face_recognition/dlib_wrapper/cnn_face_detector.cpp
@@ -28,9 +28,30 @@
 namespace dlib_api
 {
 
-    cnn_face_detection_model_v1::cnn_face_detection_model_v1(const std::string& model_filename)
+    struct cnn_face_detection_model_v1::data
     {
-        dlib::deserialize(model_filename) >> net;
+        template <long num_filters, typename SUBNET> using con5d = dlib::con<num_filters,5,5,2,2,SUBNET>;
+        template <long num_filters, typename SUBNET> using con5  = dlib::con<num_filters,5,5,1,1,SUBNET>;
+
+        template <typename SUBNET> using downsampler  = dlib::relu<dlib::affine<con5d<32, dlib::relu<dlib::affine<con5d<32, dlib::relu<dlib::affine<con5d<16,SUBNET>>>>>>>>>;
+        template <typename SUBNET> using rcon5  = dlib::relu<dlib::affine<con5<45,SUBNET>>>;
+
+        using net_type = dlib::loss_mmod<dlib::con<1,9,9,1,1,rcon5<rcon5<rcon5<downsampler<dlib::input_rgb_image_pyramid<dlib::pyramid_down<6>>>>>>>>;
+
+        net_type net;
+    };
+
+
+    cnn_face_detection_model_v1::cnn_face_detection_model_v1(const std::string& model_filename)
+        : m_data(std::make_unique<data>())
+    {
+        dlib::deserialize(model_filename) >> m_data->net;
+    }
+
+
+    cnn_face_detection_model_v1::~cnn_face_detection_model_v1()
+    {
+
     }
 
 
@@ -49,7 +70,7 @@ namespace dlib_api
         for (int i = 0; i < upsample_num_times; i++)
             pyramid_up(image, pyr);
 
-        auto dets = net(image);
+        auto dets = m_data->net(image);
 
         // Scale the detection locations back to the original image size
         // if the image was upscaled.
--- src/face_recognition/dlib_wrapper/cnn_face_detector.hpp
+++ src/face_recognition/dlib_wrapper/cnn_face_detector.hpp
@@ -37,6 +37,7 @@ namespace dlib_api
     public:
 
         cnn_face_detection_model_v1(const std::string& model_filename);
+        ~cnn_face_detection_model_v1();
 
         std::vector<dlib::mmod_rect> detect (
             const QImage& qimage,
@@ -45,15 +46,8 @@ namespace dlib_api
 
     private:
 
-        template <long num_filters, typename SUBNET> using con5d = dlib::con<num_filters,5,5,2,2,SUBNET>;
-        template <long num_filters, typename SUBNET> using con5  = dlib::con<num_filters,5,5,1,1,SUBNET>;
-
-        template <typename SUBNET> using downsampler  = dlib::relu<dlib::affine<con5d<32, dlib::relu<dlib::affine<con5d<32, dlib::relu<dlib::affine<con5d<16,SUBNET>>>>>>>>>;
-        template <typename SUBNET> using rcon5  = dlib::relu<dlib::affine<con5<45,SUBNET>>>;
-
-        using net_type = dlib::loss_mmod<dlib::con<1,9,9,1,1,rcon5<rcon5<rcon5<downsampler<dlib::input_rgb_image_pyramid<dlib::pyramid_down<6>>>>>>>>;
-
-        net_type net;
+        struct data;
+        std::unique_ptr<data> m_data;
     };
 }
 
