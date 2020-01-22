bd7f49705e408f3103cf75edd604163d7774a913
Michał Walenciak <Kicer86@gmail.com>
2020-01-22 22:54:00 +0100

    testing distance between scaled faces

--- src/face_recognition/learning_tests/face_scaling.cpp
+++ src/face_recognition/learning_tests/face_scaling.cpp
@@ -13,16 +13,31 @@
 #define STRING(s) #s
 #define PATH(path) STRING(path)
 
-const QString face1Path = QString(PATH(FACES_DIR)) + "/face1.jpg";
-const QString face2Path = QString(PATH(FACES_DIR)) + "/face2.jpg";
-
-QImage downsize(const QImage& source, int factor)
+namespace
 {
-    const QSize size = source.size();
-    const qreal scaleFactor = std::sqrt(factor);
-    const QSize scaledSize = size / scaleFactor;
+    const QString face1Path = QString(PATH(FACES_DIR)) + "/face1.jpg";
+    const QString face2Path = QString(PATH(FACES_DIR)) + "/face2.jpg";
 
-    return source.scaled(scaledSize);
+    QImage downsize(const QImage& source, int factor)
+    {
+        const QSize size = source.size();
+        const qreal scaleFactor = std::sqrt(factor);
+        const QSize scaledSize = size / scaleFactor;
+
+        return source.scaled(scaledSize);
+    }
+
+
+    QImage extractFace(const QImage& photo)
+    {
+        QVector faces = dlib_api::face_locations(photo, 0, dlib_api::cnn);
+        assert(faces.size() == 1);
+
+        const QRect faceRect = faces.front();
+        const QImage faceImg = photo.copy(faceRect);
+
+        return faceImg;
+    }
 }
 
 
@@ -66,4 +81,25 @@ TEST(FaceScalingTest, faceDetectionForCnn)
         QVector faces = dlib_api::face_locations(small, 0, dlib_api::cnn);
         EXPECT_EQ(faces.size(), 0);
     }
+}
+
+
+TEST(FaceScalingTest, scaledFaceDistance)
+{
+    const QImage img(face1Path);
+    const QImage face = extractFace(img);
+    const auto face_encodings = dlib_api::face_encodings(face);
+
+    // downsize by factor of powers of 2 - minimal image size is 128x128, detected face size is left: 12, right: 90, top: 28, bottom: 106
+    for(int scale = 2; scale <= 64; scale *= 2)
+    {
+        const QImage small_image = downsize(img, scale);
+        const QImage small_face = extractFace(small_image);
+        const auto small_face_encodings = dlib_api::face_encodings(small_face);
+        const std::vector distances = dlib_api::face_distance( {small_face_encodings}, face_encodings );
+        const double distance = distances.front();
+
+        EXPECT_LT(distance, 0.25);
+    }
+
 }
