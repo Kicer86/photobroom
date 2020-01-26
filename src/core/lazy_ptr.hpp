761eb41ef26437d7493f4bb725bc7bf4af635ab6
Michał Walenciak <Kicer86@gmail.com>
2020-01-26 23:33:24 +0100

    handle non ptr constructors

--- src/core/lazy_ptr.hpp
+++ src/core/lazy_ptr.hpp
@@ -29,10 +29,33 @@ class lazy_ptr
         std::unique_ptr<T> m_object;
         C& m_constructor;
 
+        // used when C::operator() returns pointer
+        struct PtrCreator
+        {
+            T* operator()(C& constructor)
+            {
+                return constructor();
+            }
+        };
+
+        // used when C::operator() returns value
+        struct CopyCreator
+        {
+            T* operator()(C& constructor)
+            {
+                return new T(constructor());
+            }
+        };
+
         T* get()
         {
             if (m_object.get() == nullptr)
-                m_object.reset( m_constructor() );
+            {
+                typedef typename std::conditional<std::is_pointer<decltype(m_constructor())>::value, PtrCreator, CopyCreator>::type Creator;
+                Creator creator;
+
+                m_object.reset( creator(m_constructor) );
+            }
 
             return m_object.get();
         }
