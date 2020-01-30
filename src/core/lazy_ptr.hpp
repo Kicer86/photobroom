ebb2ee888e94bf52a723596960e9ecf426d45957
Michał Walenciak <Kicer86@gmail.com>
2020-01-30 22:46:45 +0100

    storage for m_constructor depends on copy_constructibility of C

--- src/core/lazy_ptr.hpp
+++ src/core/lazy_ptr.hpp
@@ -8,6 +8,12 @@ template<typename T, typename C>
 class lazy_ptr
 {
     public:
+        lazy_ptr()
+            : m_constructor()
+        {
+
+        }
+
         explicit lazy_ptr(C& constructor)
             : m_constructor(constructor)
         {
@@ -25,8 +31,10 @@ class lazy_ptr
         }
 
     private:
+        typedef typename std::conditional<std::is_copy_constructible<C>::value, C, C&>::type CType;
+
         std::unique_ptr<T> m_object;
-        C& m_constructor;
+        CType m_constructor;
 
         // used when C::operator() returns pointer
         struct PtrCreator
