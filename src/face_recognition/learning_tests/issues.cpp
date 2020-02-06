d43f770602206254ba4c889863e122334b85ff38
Michał Walenciak <Kicer86@gmail.com>
2020-02-06 21:19:22 +0100

    handle another problem with dlib

--- src/face_recognition/learning_tests/issues.cpp
+++ src/face_recognition/learning_tests/issues.cpp
@@ -28,3 +28,29 @@ TEST(DlibIssuesTest, outOfMemory)
         faceLocator.face_locations_cnn(img1, 0);       // but does not throw for yet another call
     });
 }
+
+
+TEST(DlibIssuesTest, outOfMemoryEvenWhenResourcesAreAvailable)
+{
+    const QImage img1(utils::photoPath(1));
+
+    {
+        dlib_api::FaceLocator faceLocator;
+        QVector facesCnn = faceLocator.face_locations_cnn(img1, 0);
+        ASSERT_EQ(facesCnn.size(), 1);
+
+        ASSERT_THROW({
+            for(int i = 1; i < 128; i++)
+                faceLocator.face_locations_cnn(img1, i);   // locate faces with 'i' getting higher and higher until cuda throws out of memory exception
+        }, dlib::cuda_error);
+    }
+
+    {
+        dlib_api::FaceLocator faceLocator;
+
+        EXPECT_THROW({
+            faceLocator.face_locations_cnn(img1, 0);       // another instance of FaceLocator so we should be fine here, but well, surprise
+        },
+        dlib::cuda_error);
+    }
+}
