3f22b79ea9fbca7d82a0722bdacce96e972e5ec1
Michał Walenciak <Kicer86@gmail.com>
2020-02-14 21:04:32 +0100

    tests for person recognition added

---
+++ src/face_recognition/learning_tests/person_recognition_tests.cpp
@@ -0,0 +1,58 @@
+
+#include <gtest/gtest.h>
+#include <QDirIterator>
+
+#include <unit_tests_utils/empty_logger.hpp>
+
+#include "utils.hpp"
+#include "face_recognition/dlib_wrapper/dlib_face_recognition_api.hpp"
+
+namespace
+{
+    dlib_api::FaceEncodings fetchFaceEncodings(dlib_api::FaceLocator& locator, dlib_api::FaceEncoder& encoder, const QString& path)
+    {
+        dlib_api::FaceEncodings result;
+        const QImage photo(path);
+
+        const auto location = locator.face_locations(photo);
+        if (location.size(), 1)
+        {
+            const QImage face = photo.copy(location.front());
+            const auto encodings = encoder.face_encodings(face);
+
+            result = encodings;
+        }
+
+        return result;
+    }
+
+}
+
+
+TEST(PersonRecognitionTest, GeorgeWBushRecognition)
+{
+    EmptyLogger logger;
+    dlib_api::FaceLocator locator(&logger);
+    dlib_api::FaceEncoder encoder;
+    QDirIterator di(utils::photoSetPath() + "/lfw/George_W_Bush", QDir::Files);
+
+    const QString referencePhotoPath = utils::photoSetPath() + "/lfw/George_W_Bush/George_W_Bush_0006.jpg";
+    const auto referenceFaceEncoding = fetchFaceEncodings(locator, encoder, referencePhotoPath);
+    ASSERT_FALSE(referenceFaceEncoding.empty());
+
+    std::vector<dlib_api::FaceEncodings> testedFacesEncodings;
+
+    while(di.hasNext())
+    {
+        const QString file(di.next());
+
+        const auto testedFaceEncoding = fetchFaceEncodings(locator, encoder, file);
+        if (testedFaceEncoding.empty() == false)
+            testedFacesEncodings.push_back(testedFaceEncoding);
+    }
+
+    const auto distances = dlib_api::face_distance(testedFacesEncodings, referenceFaceEncoding);
+
+    for(double distance: distances)
+        EXPECT_LE(distance, 0.6);
+}
