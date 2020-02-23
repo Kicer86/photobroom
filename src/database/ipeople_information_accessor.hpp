bfb3a1a428c6b7e3a56f87c9db66ff8c69912859
Michał Walenciak <Kicer86@gmail.com>
2020-02-23 14:46:02 +0100

    moving class into namespace

--- src/database/ipeople_information_accessor.hpp
+++ src/database/ipeople_information_accessor.hpp
@@ -4,20 +4,14 @@
 
 #include "person_data.hpp"
 
-class IPeopleInformationAccessor
+
+namespace Database
 {
-    public:
-        virtual ~IPeopleInformationAccessor() = default;
-
-        /// list all people names
-        virtual std::vector<PersonName>  listPeople() = 0;
-
-        /**
-         * \brief get person details
-         * \arg id person id
-         * \return PersonName struct
-         */
-        virtual PersonName               person(const Person::Id &) = 0;
-};
+    class IPeopleInformationAccessor
+    {
+        public:
+            virtual ~IPeopleInformationAccessor() = default;
+    };
+}
 
 #endif
