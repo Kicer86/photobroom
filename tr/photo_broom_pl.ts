92818abeff7b1ccf729ff0b6a7e081dfe3b432ce
Michał Walenciak <Kicer86@gmail.com>
2020-03-15 23:43:11 +0100

    ts update

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -449,7 +449,7 @@ Sprawdź proszę swoje połączenie z internetem.</translation>
         <translation>Nowe zdjęcia</translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="+149"/>
+        <location filename="../src/gui/desktop/ui/mainwindow.cpp" line="+148"/>
         <source>No collection opened</source>
         <translation>Nie otwarto kolekcji zdjęć</translation>
     </message>
