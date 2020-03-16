bca4e38d2cd9d4f6ee339bf367dbb4d00f88bb1d
Michał Walenciak <Kicer86@gmail.com>
2020-03-16 21:09:25 +0100

    generate qml accessor IThumbnailsManager

--- src/gui/desktop/quick_views/qml_setup.hpp
+++ src/gui/desktop/quick_views/qml_setup.hpp
@@ -3,7 +3,11 @@
 #define QML_SETUP_HPP
 
 #include "quick_views_export.h"
+#include "../ui_utils/qml_utils.hpp"
 
 QUICK_VIEWS_EXPORT void register_qml_types();
+
+struct IThumbnailsManager;
+INVOKABLE_WRAPPER_FOR_INTERFACE(IThumbnailsManager);
 
 #endif
