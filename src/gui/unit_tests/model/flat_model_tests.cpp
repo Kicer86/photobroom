a2cf8676b45cf8487e91953792d64e16175b718a
Michał Walenciak <Kicer86@gmail.com>
2020-04-09 23:24:01 +0200

    verifying single data fetch

--- src/gui/unit_tests/model/flat_model_tests.cpp
+++ src/gui/unit_tests/model/flat_model_tests.cpp
@@ -4,13 +4,27 @@
 
 #include "desktop/models/flat_model.hpp"
 #include "unit_tests_utils/mock_database.hpp"
+#include "unit_tests_utils/mock_backend.hpp"
 
+
+using testing::_;
+using testing::Invoke;
+using testing::Return;
 
 class FlatModelTest: public testing::Test
 {
-    protected:
+    public:
         FlatModel model;
         MockDatabase db;
+        MockBackend backend;
+
+        FlatModelTest()
+        {
+            ON_CALL(db, execute(_)).WillByDefault(Invoke([this](std::unique_ptr<Database::IDatabase::ITask>&& task)
+            {
+                task->run(&backend);
+            }));
+        }
 };
 
 
@@ -26,4 +40,23 @@ TEST_F(FlatModelTest, resetAfterDBSet)
     model.setDatabase(&db);
     EXPECT_EQ(model_about_to_be_reset.count(), 2);
     EXPECT_EQ(model_reset.count(), 2);
+}
+
+
+TEST_F(FlatModelTest, insertNotification)
+{
+    EXPECT_CALL(backend, getPhotos(_))
+        .WillOnce(Return(std::vector<Photo::Id>{Photo::Id(1), Photo::Id(2), Photo::Id(3)}));
+
+    QSignalSpy model_about_to_be_inserted(&model, &FlatModel::rowsAboutToBeInserted);
+    QSignalSpy model_inserted(&model, &FlatModel::rowsInserted);
+
+    model.setDatabase(&db);
+
+    EXPECT_EQ(model_about_to_be_inserted.count(), 1);
+    EXPECT_EQ(model_inserted.count(), 1);
+
+    // we expect 3 items were inserted on position 0, 1 and 2
+    EXPECT_EQ(model_inserted.at(0).at(1).toInt(), 0);       // first(0) instance of signal, second(1) argument  (first)
+    EXPECT_EQ(model_inserted.at(0).at(2).toInt(), 2);       // first(0) instance of signal, third(2) argument   (last)
 }
