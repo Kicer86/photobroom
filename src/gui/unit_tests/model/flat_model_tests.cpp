f6b127c4dafeff3b3f33b02b53a6db1655786d1c
Michał Walenciak <Kicer86@gmail.com>
2020-04-11 13:37:08 +0200

    test for replacement

--- src/gui/unit_tests/model/flat_model_tests.cpp
+++ src/gui/unit_tests/model/flat_model_tests.cpp
@@ -194,3 +194,35 @@ TEST_F(FlatModelTest, dataRemovedInTheMiddle)
     EXPECT_EQ(model_removed.at(0).at(1).toInt(), 1);       // first(0) instance of signal, second(1) argument  (first)
     EXPECT_EQ(model_removed.at(0).at(2).toInt(), 1);       // first(0) instance of signal, third(2) argument   (last)
 }
+
+
+TEST_F(FlatModelTest, dataReplacement)
+{
+    EXPECT_CALL(backend, getPhotos(_))
+        .WillOnce(Return(std::vector<Photo::Id>{Photo::Id(1), Photo::Id(2), Photo::Id(3)}))                 // first call after db set
+        .WillOnce(Return(std::vector<Photo::Id>{Photo::Id(1), Photo::Id(4), Photo::Id(3)}));                // second call after setting filters
+
+    QSignalSpy model_about_to_be_inserted(&model, &FlatModel::rowsAboutToBeInserted);
+    QSignalSpy model_inserted(&model, &FlatModel::rowsInserted);
+    QSignalSpy model_about_to_be_removed(&model, &FlatModel::rowsAboutToBeRemoved);
+    QSignalSpy model_removed(&model, &FlatModel::rowsRemoved);
+
+    model.setDatabase(&db);
+    model.setFilters({});       // setting filters should update set of photos
+
+    //
+    // 1 removal expected and 2 insertions - one after db set, second after filters set
+    EXPECT_EQ(model_about_to_be_removed.count(), 1);
+    EXPECT_EQ(model_removed.count(), 1);
+    EXPECT_EQ(model_about_to_be_inserted.count(), 2);
+    EXPECT_EQ(model_inserted.count(), 2);
+
+    // we expect one item to be removed and one to be insered
+    // There are two scenarios: either we insert first, then remove
+    // or oposite. Depending on order positions may be swapped.
+    // Current implementation will insert before removal
+    EXPECT_EQ(model_inserted.at(1).at(1).toInt(), 1);       // second(1) instance of signal, second(1) argument  (first)
+    EXPECT_EQ(model_inserted.at(1).at(2).toInt(), 1);       // second(1) instance of signal, third(2) argument   (last)
+    EXPECT_EQ(model_removed.at(0).at(1).toInt(), 2);       // first(0) instance of signal, second(1) argument  (first)
+    EXPECT_EQ(model_removed.at(0).at(2).toInt(), 2);       // first(0) instance of signal, third(2) argument   (last)
+}
