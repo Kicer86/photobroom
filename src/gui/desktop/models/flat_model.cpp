3dd4408a9492e78ec8e07847be26b0609bb5b5e1
Michał Walenciak <Kicer86@gmail.com>
2020-04-11 23:17:21 +0200

    using extraced code

--- src/gui/desktop/models/flat_model.cpp
+++ src/gui/desktop/models/flat_model.cpp
@@ -269,9 +269,7 @@ void FlatModel::fetchedPhotos(const std::vector<Photo::Id>& photos)
                     m_photos.erase(old_photos_it, last_old_it);    // everything in old sequence is not needed, remove it
                     endRemoveRows();
 
-                    beginInsertRows({}, pos, pos + std::distance(new_photos_it, last_new_it) - 1);
-                    m_photos.insert(old_photos_it, new_photos_it, last_new_it);   // append while new block of new items
-                    endInsertRows();
+                    insertPhotos(old_photos_it, new_photos_it, last_new_it);
 
                     last_old_it = m_photos.end();
                     old_photos_it = last_old_it;
