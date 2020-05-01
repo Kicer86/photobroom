8cb2ad098ef0c5eec34dc1940ec1f0733edda7b4
Michał Walenciak <Kicer86@gmail.com>
2020-05-01 18:59:54 +0200

    implementation translating QModelIndexes to Photo::Ids

--- src/gui/desktop/utils/selection_to_photoid_translator.cpp
+++ src/gui/desktop/utils/selection_to_photoid_translator.cpp
@@ -1,2 +1,44 @@
 
 #include "selection_to_photoid_translator.hpp"
+
+#include <QItemSelectionModel>
+
+#include "models/photo_properties.hpp"
+
+
+SelectionToPhotoIdTranslator::SelectionToPhotoIdTranslator(QItemSelectionModel* selectionModel, QObject* p)
+    : QObject(p)
+    , m_model(selectionModel->model())
+    , m_propertiesRole(-1)
+{
+    connect(selectionModel, &QItemSelectionModel::selectionChanged,
+            this, &SelectionToPhotoIdTranslator::translate);
+
+    const auto roles = m_model->roleNames();
+
+    // Find photo properties role. Model needs to provide it
+    const auto it = std::find_if(roles.begin(), roles.end(), [](const auto roleName){
+        return roleName == "photoProperties";
+    });
+
+    assert(it != roles.end());
+
+    m_propertiesRole = it.key();
+}
+
+
+void SelectionToPhotoIdTranslator::translate(const QItemSelection& selection) const
+{
+    const auto indexes = selection.indexes();
+    std::vector<Photo::Id> ids;
+
+    for(const QModelIndex& idx: indexes)
+    {
+        const QVariant rawProperties = idx.data(m_propertiesRole);
+        const PhotoProperties properties = rawProperties.value<PhotoProperties>();
+
+        ids.push_back(properties.m_id);
+    }
+
+    emit selectionChanged(ids);
+}
--- src/gui/desktop/utils/selection_to_photoid_translator.hpp
+++ src/gui/desktop/utils/selection_to_photoid_translator.hpp
@@ -4,9 +4,28 @@
 
 #include <QObject>
 
+#include <database/photo_types.hpp>
+
+class QAbstractItemModel;
+class QItemSelection;
+class QItemSelectionModel;
+
 
 class SelectionToPhotoIdTranslator: public QObject
 {
+        Q_OBJECT
+
+    public:
+        SelectionToPhotoIdTranslator(QItemSelectionModel *, QObject* = nullptr);
+
+    signals:
+        void selectionChanged(const std::vector<Photo::Id> &) const;
+
+    private:
+        QAbstractItemModel* m_model;
+        int m_propertiesRole;
+
+        void translate(const QItemSelection &) const;
 };
 
 #endif // SELECTIONTOPHOTOIDTRANSLATOR_HPP
