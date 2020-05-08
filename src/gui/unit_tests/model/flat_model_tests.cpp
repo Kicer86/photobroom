1adc22b27d455f82eaa3e8c27f347f417614514d
Michał Walenciak <Kicer86@gmail.com>
2020-05-08 23:46:13 +0200

    test full cleanup

--- src/gui/unit_tests/model/flat_model_tests.cpp
+++ src/gui/unit_tests/model/flat_model_tests.cpp
@@ -234,6 +234,33 @@ TEST_F(FlatModelTest, dataRemovedInTheMiddle)
 }
 
 
+TEST_F(FlatModelTest, removingAll)
+{
+    const auto initial_photos_set = std::vector<Photo::Id>{Photo::Id(1), Photo::Id(2), Photo::Id(3)};
+    const auto final_photos_set = std::vector<Photo::Id>{};
+
+    EXPECT_CALL(photoOperator, onPhotos(_, _))
+        .WillOnce(Return(initial_photos_set))                 // first call after db set
+        .WillOnce(Return(final_photos_set));                  // second call after setting filters
+
+    QSignalSpy model_about_to_be_removed(&model, &FlatModel::rowsAboutToBeRemoved);
+    QSignalSpy model_removed(&model, &FlatModel::rowsRemoved);
+
+    model.setDatabase(&db);
+    model.setFilters({});       // setting filters should update set of photos
+
+    // 1 removal expected
+    ASSERT_EQ(model_about_to_be_removed.count(), 1);
+    ASSERT_EQ(model_removed.count(), 1);
+
+    // we expect 3 items to be removed
+    EXPECT_EQ(model_removed.at(0).at(1).toInt(), 0);       // first(0) instance of signal, second(1) argument  (first)
+    EXPECT_EQ(model_removed.at(0).at(2).toInt(), 2);       // first(0) instance of signal, third(2) argument   (last)
+
+    EXPECT_EQ(final_photos_set, model.photos());
+}
+
+
 TEST_F(FlatModelTest, dataReplacementInTheMiddle)
 {
     const auto initial_photos_set = std::vector<Photo::Id>{Photo::Id(1), Photo::Id(2), Photo::Id(3)};
