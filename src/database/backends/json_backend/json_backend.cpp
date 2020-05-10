0e92d185b3106e9e959e2aefcbfcb56b9ab77647
Michał Walenciak <Kicer86@gmail.com>
2020-05-10 13:38:15 +0200

    json backend skeleton

---
+++ src/database/backends/json_backend/json_backend.cpp
@@ -0,0 +1,2 @@
+
+#include "json_backend.hpp"
---
+++ src/database/backends/json_backend/json_backend.hpp
@@ -0,0 +1,17 @@
+
+#ifndef JSONBACKEND_HPP
+#define JSONBACKEND_HPP
+
+#include "database/ibackend.hpp"
+
+namespace Database
+{
+    /**
+    * \brief json based backend
+    */
+    class JsonBackend : public IBackend
+    {
+    };
+}
+
+#endif // JSONBACKEND_HPP
