22da6ea1e596f1c6600ac0b8a52f36a93e044bac
Michał Walenciak <Kicer86@gmail.com>
2020-05-10 13:46:38 +0200

    preparing empty methods

--- src/database/backends/json_backend/json_backend.cpp
+++ src/database/backends/json_backend/json_backend.cpp
@@ -1,2 +1,85 @@
 
 #include "json_backend.hpp"
+
+
+namespace Database
+{
+
+    bool JsonBackend::addPhotos(std::vector<Photo::DataDelta>& photos)
+    {
+
+    }
+
+    bool JsonBackend::update(const Photo::DataDelta& delta)
+    {
+
+    }
+
+    std::vector<TagTypeInfo> JsonBackend::listTags()
+    {
+
+    }
+
+    std::vector<TagValue> JsonBackend::listTagValues(const TagTypes &, const std::vector<IFilter::Ptr> &)
+    {
+
+    }
+
+    Photo::Data JsonBackend::getPhoto(const Photo::Id &)
+    {
+
+    }
+
+    int JsonBackend::getPhotosCount(const std::vector<IFilter::Ptr> &)
+    {
+
+    }
+
+    void JsonBackend::set(const Photo::Id &id, const QString& name, int value)
+    {
+
+    }
+
+    std::optional<int> JsonBackend::get(const Photo::Id& id, const QString& name)
+    {
+
+    }
+
+    std::vector<Photo::Id> JsonBackend::markStagedAsReviewed()
+    {
+
+    }
+
+    BackendStatus JsonBackend::init(const ProjectInfo &)
+    {
+
+    }
+
+    void JsonBackend::closeConnections()
+    {
+
+    }
+
+    IGroupOperator& JsonBackend::groupOperator()
+    {
+
+    }
+
+    IPhotoOperator& JsonBackend::photoOperator()
+    {
+
+    }
+
+    IPhotoChangeLogOperator& JsonBackend::photoChangeLogOperator()
+    {
+
+    }
+
+
+    IPeopleInformationAccessor& JsonBackend::peopleInformationAccessor()
+    {
+
+    }
+
+
+}
--- src/database/backends/json_backend/json_backend.hpp
+++ src/database/backends/json_backend/json_backend.hpp
@@ -9,8 +9,25 @@ namespace Database
     /**
     * \brief json based backend
     */
-    class JsonBackend : public IBackend
+    class JsonBackend: public IBackend
     {
+        public:
+            // IBackend interface
+            bool addPhotos(std::vector<Photo::DataDelta>& photos) override;
+            bool update(const Photo::DataDelta& delta) override;
+            std::vector<TagTypeInfo> listTags() override;
+            std::vector<TagValue> listTagValues(const TagTypes &, const std::vector<IFilter::Ptr> &) override;
+            Photo::Data getPhoto(const Photo::Id &) override;
+            int getPhotosCount(const std::vector<IFilter::Ptr> &) override;
+            void set(const Photo::Id& id, const QString& name, int value) override;
+            std::optional<int> get(const Photo::Id& id, const QString& name) override;
+            std::vector<Photo::Id> markStagedAsReviewed() override;
+            BackendStatus init(const ProjectInfo &) override;
+            void closeConnections() override;
+            IGroupOperator& groupOperator() override;
+            IPhotoOperator& photoOperator() override;
+            IPhotoChangeLogOperator& photoChangeLogOperator() override;
+            IPeopleInformationAccessor& peopleInformationAccessor() override;
     };
 }
 
