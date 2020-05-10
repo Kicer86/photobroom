307b2ea8a5cb5206e84a71394bff4ba50f2cb9e4
Michał Walenciak <Kicer86@gmail.com>
2020-05-10 12:17:19 +0200

    preparations for models tests

--- src/gui/unit_tests/model/aphoto_info_model_tests.cpp
+++ src/gui/unit_tests/model/aphoto_info_model_tests.cpp
@@ -4,6 +4,26 @@
 #include <db_data_model.hpp>
 #include <flat_model.hpp>
 
+#include <unit_tests_utils/mock_database.hpp>
+
 // tests for implementations of APhotoInfoModel
+class APhotoInfoModelTest: testing::Test
+{
+    public:
+        APhotoInfoModelTest()
+        {
+            model1.setDatabase(&db);
+            model2.setDatabase(&db);
+        }
 
+        ~APhotoInfoModelTest()
+        {
+        }
 
+    private:
+        FlatModel model1;
+        DBDataModel model2;
+        MockDatabase db;
+
+        std::vector<APhotoInfoModel *> models = { &model1, &model2 };
+};
