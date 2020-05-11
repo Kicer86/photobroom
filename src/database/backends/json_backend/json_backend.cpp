96a1c23819a98d27df188615903b35167987905e
Michał Walenciak <Kicer86@gmail.com>
2020-05-11 22:54:35 +0200

    file format

--- src/database/backends/json_backend/json_backend.cpp
+++ src/database/backends/json_backend/json_backend.cpp
@@ -13,45 +13,54 @@ namespace Database
 
     }
 
+
     bool JsonBackend::update(const Photo::DataDelta& delta)
     {
 
     }
+
 
     std::vector<TagTypeInfo> JsonBackend::listTags()
     {
 
     }
 
+
     std::vector<TagValue> JsonBackend::listTagValues(const TagTypes &, const std::vector<IFilter::Ptr> &)
     {
 
     }
+
 
     Photo::Data JsonBackend::getPhoto(const Photo::Id &)
     {
 
     }
 
+
     int JsonBackend::getPhotosCount(const std::vector<IFilter::Ptr> &)
     {
 
     }
+
 
     void JsonBackend::set(const Photo::Id &id, const QString& name, int value)
     {
 
     }
 
+
     std::optional<int> JsonBackend::get(const Photo::Id& id, const QString& name)
     {
 
     }
 
+
     std::vector<Photo::Id> JsonBackend::markStagedAsReviewed()
     {
 
     }
+
 
     BackendStatus JsonBackend::init(const ProjectInfo& prjInfo)
     {
@@ -72,20 +81,24 @@ namespace Database
         return status;
     }
 
+
     void JsonBackend::closeConnections()
     {
 
     }
+
 
     IGroupOperator& JsonBackend::groupOperator()
     {
 
     }
 
+
     IPhotoOperator& JsonBackend::photoOperator()
     {
 
     }
+
 
     IPhotoChangeLogOperator& JsonBackend::photoChangeLogOperator()
     {
@@ -97,6 +110,5 @@ namespace Database
     {
 
     }
-
 
 }
