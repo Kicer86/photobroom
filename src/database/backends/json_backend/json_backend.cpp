06a1cd22b30774622626938fab549e08013f7e10
Michał Walenciak <Kicer86@gmail.com>
2020-05-17 10:15:35 +0200

    compilable version of JsonBackend

--- src/database/backends/json_backend/json_backend.cpp
+++ src/database/backends/json_backend/json_backend.cpp
@@ -10,13 +10,38 @@ namespace Database
 
     bool JsonBackend::addPhotos(std::vector<Photo::DataDelta>& photos)
     {
+        for (Photo::DataDelta& delta: photos)
+        {
+            assert(delta.getId().valid() == false);
 
+            const Photo::Id id(m_nextId);
+            delta.setId(id);
+
+            Photo::Data data;
+            data.apply(delta);
+
+            auto [it, i] = m_photos.insert(data);      // insert empty Data for given id
+
+            assert(i == true);
+
+            m_nextId++;
+        }
+
+        return true;
     }
 
 
     bool JsonBackend::update(const Photo::DataDelta& delta)
     {
+        auto it = m_photos.find(delta.getId());
 
+        Photo::Data data = *it;
+        data.apply(delta);
+
+        it = m_photos.erase(it);
+        m_photos.insert(it, data);
+
+        return true;
     }
 
 
@@ -32,9 +57,10 @@ namespace Database
     }
 
 
-    Photo::Data JsonBackend::getPhoto(const Photo::Id &)
+    Photo::Data JsonBackend::getPhoto(const Photo::Id& id)
     {
-
+        auto it = m_photos.find(id);
+        return it == m_photos.end()? Photo::Data(): *it;
     }
 
 
@@ -46,13 +72,25 @@ namespace Database
 
     void JsonBackend::set(const Photo::Id &id, const QString& name, int value)
     {
-
+        m_flags[id][name] = value;
     }
 
 
     std::optional<int> JsonBackend::get(const Photo::Id& id, const QString& name)
     {
+        std::optional<int> result;
 
+        auto it = m_flags.find(id);
+
+        if (it != m_flags.end())
+        {
+            auto f_it = it->second.find(name);
+
+            if (f_it != it->second.end())
+                result = f_it->second;
+        }
+
+        return result;
     }
 
 
@@ -68,15 +106,26 @@ namespace Database
 
         const QFileInfo db_file_info(prjInfo.databaseLocation);
 
-        if (prjInfo.backendName == "Json" &&
-            db_file_info.exists()         &&
-            db_file_info.isReadable()     &&
-            db_file_info.isWritable())
+        if (prjInfo.backendName != "Json")
+            status = StatusCodes::OpenFailed;
+        else if (db_file_info.exists())
         {
+            if (db_file_info.isReadable()  &&
+                db_file_info.isWritable())
+            {
 
+            }
+            else
+                status = StatusCodes::OpenFailed;
         }
         else
-            status = StatusCodes::OpenFailed;
+        {
+            QFile db_file(prjInfo.databaseLocation);
+            const bool open_status = db_file.open(QIODevice::NewOnly | QIODevice::ReadWrite);
+
+            if (open_status == false)
+                status = StatusCodes::OpenFailed;
+        }
 
         return status;
     }
@@ -102,13 +151,114 @@ namespace Database
 
     IPhotoChangeLogOperator& JsonBackend::photoChangeLogOperator()
     {
-
+        return *this;
     }
 
 
     IPeopleInformationAccessor& JsonBackend::peopleInformationAccessor()
     {
+        return *this;
+    }
+
+
+    std::vector<PersonName> JsonBackend::listPeople()
+    {
+        std::vector<PersonName> result;
+        result.reserve(m_peopleNames.size());
+
+        std::copy(m_peopleNames.cbegin(), m_peopleNames.cend(), std::back_inserter(result));
+
+        return result;
+    }
+
+
+    std::vector<PersonInfo> JsonBackend::listPeople(const Photo::Id& id)
+    {
+        auto it = m_people.find(id);
+        return it == m_people.end()? std::vector<PersonInfo>(): it->second;
+    }
+
+
+    PersonName JsonBackend::person(const Person::Id& id)
+    {
+        auto it = m_peopleNames.find(id);
+        return it == m_peopleNames.end()? PersonName(): *it;
+
 
     }
 
+
+    std::vector<PersonFingerprint> JsonBackend::fingerprintsFor(const Person::Id &)
+    {
+
+    }
+
+
+    std::map<PersonInfo::Id, PersonFingerprint> JsonBackend::fingerprintsFor(const std::vector<PersonInfo::Id>& id)
+    {
+
+    }
+
+
+    Person::Id JsonBackend::store(const PersonName &pn)
+    {
+        Person::Id id = pn.id();
+
+        if (id.valid())
+        {
+            auto it = m_peopleNames.find(id);
+            if (it == m_peopleNames.end())
+                id = Person::Id();
+            else
+            {
+                it = m_peopleNames.erase(it);
+                m_peopleNames.insert(it, pn);
+            }
+        }
+        else
+        {
+            id = Person::Id(m_nextPerson++);
+            const PersonName personWithId(id, pn.name());
+
+            m_peopleNames.insert(personWithId);
+        }
+
+        return id;
+    }
+
+
+    PersonInfo::Id JsonBackend::store(const PersonInfo& pi)
+    {
+
+    }
+
+
+    PersonFingerprint::Id JsonBackend::store(const PersonFingerprint &)
+    {
+
+    }
+
+
+    void JsonBackend::storeDifference(const Photo::Data &, const Photo::DataDelta &)
+    {
+
+    }
+
+
+    void JsonBackend::groupCreated(const Group::Id &, const Group::Type &, const Photo::Id& representative)
+    {
+
+    }
+
+
+    void JsonBackend::groupDeleted(const Group::Id &, const Photo::Id& representative, const std::vector<Photo::Id>& members)
+    {
+
+    }
+
+
+    QStringList JsonBackend::dumpChangeLog()
+    {
+        return {};
+    }
 }
--- src/database/backends/json_backend/json_backend.hpp
+++ src/database/backends/json_backend/json_backend.hpp
@@ -3,13 +3,17 @@
 #define JSONBACKEND_HPP
 
 #include "database/ibackend.hpp"
+#include "database/iphoto_change_log_operator.hpp"
+
 
 namespace Database
 {
     /**
     * \brief json based backend
     */
-    class JsonBackend: public IBackend
+    class JsonBackend: public IBackend,
+                              IPeopleInformationAccessor,
+                              IPhotoChangeLogOperator
     {
         public:
             // IBackend interface
@@ -28,6 +32,64 @@ namespace Database
             IPhotoOperator& photoOperator() override;
             IPhotoChangeLogOperator& photoChangeLogOperator() override;
             IPeopleInformationAccessor& peopleInformationAccessor() override;
+
+        private:
+            // IPeopleInformationAccessor interface
+            std::vector<PersonName> listPeople() override;
+            std::vector<PersonInfo> listPeople(const Photo::Id &) override;
+            PersonName person(const Person::Id &) override;
+            std::vector<PersonFingerprint> fingerprintsFor(const Person::Id &) override;
+            std::map<PersonInfo::Id, PersonFingerprint> fingerprintsFor(const std::vector<PersonInfo::Id>& id) override;
+            Person::Id store(const PersonName& pn) override;
+            PersonInfo::Id store(const PersonInfo& pi) override;
+            PersonFingerprint::Id store(const PersonFingerprint &) override;
+
+            // IPhotoChangeLogOperator interface
+            void storeDifference(const Photo::Data &, const Photo::DataDelta &) override;
+            void groupCreated(const Group::Id &, const Group::Type &, const Photo::Id& representative) override;
+            void groupDeleted(const Group::Id &, const Photo::Id &representative, const std::vector<Photo::Id>& members) override;
+            QStringList dumpChangeLog() override;
+
+            typedef std::map<QString, int> Flags;
+
+            static Photo::Id getIdFor(const Photo::Data& d)
+            {
+                return d.id;
+            }
+
+            static Person::Id getIdFor(const PersonName& pn)
+            {
+                return pn.id();
+            }
+
+            template<typename T, typename IdT>
+            struct IdComparer
+            {
+                bool operator()(const T& lhs, const T& rhs) const
+                {
+                    return getIdFor(lhs) < getIdFor(rhs);
+                }
+
+                bool operator()(const IdT& lhs, const T& rhs) const
+                {
+                    return lhs < getIdFor(rhs);
+                }
+
+                bool operator()(const T& lhs, const IdT& rhs) const
+                {
+                    return getIdFor(lhs) < rhs;
+                }
+
+                using is_transparent = void;
+            };
+
+            std::map<Photo::Id, Flags> m_flags;
+            std::map<Photo::Id, std::vector<PersonInfo>> m_people;
+            std::set<Photo::Data, IdComparer<Photo::Data, Photo::Id>> m_photos;
+            std::set<PersonName, IdComparer<PersonName, Person::Id>> m_peopleNames;
+
+            int m_nextId = 0;
+            int m_nextPerson = 0;
     };
 }
 
