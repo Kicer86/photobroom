ff43b0dc2c1d1ceeb74936f465d928643996df4f
Michał Walenciak <Kicer86@gmail.com>
2020-05-23 09:59:22 +0200

    introducing APhotoChangeLogOperator for common stuff of change loggers

---
+++ src/database/aphoto_change_log_operator.hpp
@@ -0,0 +1,15 @@
+
+#ifndef APHOTOCHANGELOGOPERATOR_HPP
+#define APHOTOCHANGELOGOPERATOR_HPP
+
+#include "iphoto_change_log_operator.hpp"
+
+
+namespace Database
+{
+    class APhotoChangeLogOperator: public IPhotoChangeLogOperator
+    {
+    };
+}
+
+#endif // APHOTOCHANGELOGOPERATOR_HPP
---
+++ src/database/implementation/aphoto_change_log_operator.cpp
@@ -0,0 +1,2 @@
+
+#include "aphoto_change_log_operator.hpp"
