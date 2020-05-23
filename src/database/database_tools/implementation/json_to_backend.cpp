20b2a9b6fe5c0b9c7070ea6189149bf3bd63bfca
Michał Walenciak <Kicer86@gmail.com>
2020-05-23 13:21:12 +0200

    introducing JsonToBackend

---
+++ src/database/database_tools/implementation/json_to_backend.cpp
@@ -0,0 +1,2 @@
+
+#include "../json_to_backend.hpp"
---
+++ src/database/database_tools/json_to_backend.hpp
@@ -0,0 +1,11 @@
+#ifndef JSONTOBACKEND_HPP
+#define JSONTOBACKEND_HPP
+
+/**
+ * @brief Read json and store it in backend
+ */
+class JsonToBackend
+{
+};
+
+#endif
