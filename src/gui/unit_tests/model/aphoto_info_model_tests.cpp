a6ebc2b72b87a83e5969627096bda75f8e236b89
Michał Walenciak <Kicer86@gmail.com>
2020-05-29 00:14:21 +0200

    test for empty model

--- src/gui/unit_tests/model/aphoto_info_model_tests.cpp
+++ src/gui/unit_tests/model/aphoto_info_model_tests.cpp
@@ -1,29 +1,50 @@
 
 #include <gmock/gmock.h>
 
-#include <db_data_model.hpp>
-#include <flat_model.hpp>
-
+#include <database/backends/memory_backend/memory_backend.hpp>
 #include <unit_tests_utils/mock_database.hpp>
 
+#include "db_data_model.hpp"
+#include "flat_model.hpp"
+
+
+using testing::Invoke;
+using testing::Return;
+using testing::NiceMock;
+
 // tests for implementations of APhotoInfoModel
-class APhotoInfoModelTest: testing::Test
+template<typename T>
+class APhotoInfoModelTest: public testing::Test
 {
     public:
         APhotoInfoModelTest()
         {
-            model1.setDatabase(&db);
-            model2.setDatabase(&db);
+            ON_CALL(db, execute).WillByDefault(Invoke([this](std::unique_ptr<Database::IDatabase::ITask>&& task)
+            {
+                task->run(&backend);
+            }));
+
+            ON_CALL(db, backend).WillByDefault(Return(&backend));
+
+            model.setDatabase(&db);
         }
 
         ~APhotoInfoModelTest()
         {
         }
 
-    private:
-        FlatModel model1;
-        DBDataModel model2;
-        MockDatabase db;
+        T model;
 
-        std::vector<APhotoInfoModel *> models = { &model1, &model2 };
+    private:
+        NiceMock<Database::MemoryBackend> backend;
+        NiceMock<MockDatabase> db;
 };
+
+using ModelTypes = testing::Types<FlatModel, DBDataModel>;
+TYPED_TEST_SUITE(APhotoInfoModelTest, ModelTypes);
+
+
+TYPED_TEST(APhotoInfoModelTest, noEntriesWhenEmptyDatabase)
+{
+    EXPECT_EQ(this->model.rowCount({}), 0);
+}
