8277ac6cfb327c8d64cbf61302d2213628a39de9
Michał Walenciak <Kicer86@gmail.com>
2020-08-03 15:06:47 +0200

    falling back to c++17

--- src/database/actions.hpp
+++ src/database/actions.hpp
@@ -18,7 +18,12 @@ namespace Database::Actions
         }
 
         SortByTag(const SortByTag &) = default;
-        bool operator<=>(const SortByTag &) const = default;
+
+        bool operator==(const SortByTag& other) const
+        {
+            return tag == other.tag &&
+                   sort_order == other.sort_order;
+        }
 
         const TagTypes tag;
         const Qt::SortOrder sort_order;
@@ -32,9 +37,12 @@ namespace Database::Actions
         {
         }
 
-        const Qt::SortOrder sort_order;
+        bool operator==(const SortByTimestamp& other) const
+        {
+            return sort_order == other.sort_order;
+        }
 
-        bool operator<=>(const SortByTimestamp &) const = default;
+        const Qt::SortOrder sort_order;
     };
 }
 
