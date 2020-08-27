e82c82fc42ea4118c2140820260fc6cb2f27aa6b
Michał Walenciak <Kicer86@gmail.com>
2020-08-27 20:23:17 +0200

    initial implementation

--- src/gui/desktop/quick_views/selection_manager_component.cpp
+++ src/gui/desktop/quick_views/selection_manager_component.cpp
@@ -1,2 +1,34 @@
 
 #include "selection_manager_component.hpp"
+
+
+SelectionManagerComponent::SelectionManagerComponent(QObject* p)
+    : QObject(p)
+{
+
+}
+
+
+void SelectionManagerComponent::toggleIndexSelection(int index)
+{
+    auto it = m_selected.find(index);
+
+    if (it == m_selected.end())
+        m_selected.insert(index);
+    else
+        m_selected.erase(it);
+}
+
+
+void SelectionManagerComponent::clearSelection()
+{
+    m_selected.clear();
+}
+
+
+bool SelectionManagerComponent::isIndexSelected(int index) const
+{
+    auto it = m_selected.find(index);
+
+    return it != m_selected.end();
+}
--- src/gui/desktop/quick_views/selection_manager_component.hpp
+++ src/gui/desktop/quick_views/selection_manager_component.hpp
@@ -4,10 +4,22 @@
 
 
 #include <QObject>
+#include <unordered_set>
 
 
 class SelectionManagerComponent: public QObject
 {
+        Q_OBJECT
+
+    public:
+        SelectionManagerComponent(QObject * = nullptr);
+
+        Q_INVOKABLE void toggleIndexSelection(int);
+        Q_INVOKABLE void clearSelection();
+        Q_INVOKABLE bool isIndexSelected(int) const;
+
+    private:
+        std::unordered_set<int> m_selected;
 };
 
 #endif // SELECTIONMANAGERCOMPONENT_HPP
