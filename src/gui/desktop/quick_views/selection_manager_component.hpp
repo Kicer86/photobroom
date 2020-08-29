a3cdf8b2e5c893d7a535c71f03df1562791a1193
Michał Walenciak <Kicer86@gmail.com>
2020-08-29 20:13:39 +0200

    todo

--- src/gui/desktop/quick_views/selection_manager_component.hpp
+++ src/gui/desktop/quick_views/selection_manager_component.hpp
@@ -21,6 +21,8 @@ class SelectionManagerComponent: public QObject
         Q_INVOKABLE QList<int> selected() const;
 
     private:
+        // TODO: https://doc.qt.io/qt-5/qtqml-cppintegration-data.html#sequence-type-to-javascript-array
+        // register qset to pass it to qml
         QSet<int> m_selected;
 };
 
