011dd0d5403aa7b75b911401a24caefb39807d7b
Michał Walenciak <Kicer86@gmail.com>
2020-09-03 22:56:01 +0200

    restoring Exiv2 build on Windows

--- conanfile.py
+++ conanfile.py
@@ -4,3 +4,9 @@ class PhotoBroomConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "OpenLibrary/2.3@kicer/stable"
     generators = "cmake"
+    default_options = {}
+
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.requires.add("Exiv2/0.27@piponazo/stable")
+            self.default_options = {"Exiv2:xmp": False}
