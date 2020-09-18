62ad6f0b7251920ac49f0bf2b5b32c638abf4f12
Michał Walenciak <Kicer86@gmail.com>
2020-09-18 22:58:58 +0200

    dropping win32

--- ci/appveyor.bat
+++ ci/appveyor.bat
@@ -2,19 +2,13 @@
 echo ON
 
 rem setup variables
-if "%platform%"=="Win32" (
-    set generator="Visual Studio 16 2019"
-    set qt_arch=msvc2017
-    set arch=win32
-) else (
-    set generator="Visual Studio 16 2019"
-    set qt_arch=msvc2017_64
-    set arch=amd64
-)
+set generator="Visual Studio 16 2019"
+set qt_arch=msvc2017_64
+set arch=amd64
 
 set USE_QT_VER=5.14
 set PATH=C:\Qt\%USE_QT_VER%\%qt_arch%\bin;C:\Program Files\CMake\bin;%PATH%
-set CMAKE_PREFIX_PATH=C:/Qt/%USE_QT_VER%/%qt_arch%;C:\Libraries\boost_1_64_0;c:/projects/install
+set CMAKE_PREFIX_PATH=C:/Qt/%USE_QT_VER%/%qt_arch%;c:/projects/install
 
 mkdir build
 cd build
