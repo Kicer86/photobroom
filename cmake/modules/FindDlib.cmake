daa04b05e410aa0be4872d0835d705df8f0b2129
Michał Walenciak <Kicer86@gmail.com>
2020-12-21 19:46:18 +0100

    cmake 3.16 on travis cannot consume ALIAS for imported targets

--- cmake/modules/FindDlib.cmake
+++ cmake/modules/FindDlib.cmake
@@ -1,16 +1,17 @@
 
+set(DLIB_LIBRARIES)
+
 if(TARGET CONAN_PKG::dlib)
-    add_library(dlib ALIAS CONAN_PKG::dlib)
+    set(DLIB_LIBRARIES CONAN_PKG::dlib)
 
     message("Using dlib from conan")
 else()
     find_package(dlib REQUIRED ONLY_CMAKE_FIND_ROOT_PATH)
     find_package(Threads REQUIRED)
-    add_library(dlib ALIAS dlib::dlib)
+    set(DLIB_LIBRARIES dlib::dlib)
 
     message("Using system dlib")
 endif()
 
-set(DLIB_LIBRARIES dlib)
 
 find_package_handle_standard_args(Dlib DEFAULT_MSG DLIB_LIBRARIES)
