83aa4f8c8a6c1e6dfac780371e1a088c78be21a4
Michał Walenciak <Kicer86@gmail.com>
2021-01-25 22:47:08 +0100

    deprecating config entry
    replaced with magick

--- src/core/constants.hpp
+++ src/core/constants.hpp
@@ -20,6 +20,7 @@ namespace ConfigConsts
 
 namespace ExternalToolsConfigKeys
 {
+    [[deprecated]] const char* const convertPath = "tool_path::convert";
     const char* const aisPath     = "tool_path::align_image_stack";
     const char* const magickPath  = "tool_path::magick";
     const char* const ffmpegPath  = "tool_path::ffmpeg";
