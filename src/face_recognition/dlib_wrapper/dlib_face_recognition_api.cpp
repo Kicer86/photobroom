34196e0874f3557a75d144ed5645a34184740862
Michał Walenciak <Kicer86@gmail.com>
2021-02-09 17:08:59 +0100

    dropping dead function

--- src/face_recognition/dlib_wrapper/dlib_face_recognition_api.cpp
+++ src/face_recognition/dlib_wrapper/dlib_face_recognition_api.cpp
@@ -101,23 +101,6 @@ namespace dlib_api
             }
         };
 
-        dlib::matrix<dlib::rgb_pixel> qimage_to_dlib_matrix(const QImage& qimage)
-        {
-            dlib::matrix<dlib::rgb_pixel> matrix;
-
-            const QRect size = qimage.rect();
-            matrix.set_size(size.height(), size.width());
-
-            for(int r = 0; r < size.height(); r++)
-                for(int c = 0; c < size.width(); c++)
-                {
-                    const QRgb rgb = qimage.pixel(c, r);
-                    matrix(r, c) = dlib::rgb_pixel(qRed(rgb), qGreen(rgb), qBlue(rgb));
-                }
-
-            return matrix;
-        }
-
         QRect dlib_rect_to_qrect(const dlib::rectangle& rect)
         {
             const QRect qrect(rect.left(), rect.top(),
