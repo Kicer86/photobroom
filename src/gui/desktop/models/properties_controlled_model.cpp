e5e1b6ccb7e721139168399a6df1cacfde6a422f
Michał Walenciak <Kicer86@gmail.com>
2021-02-20 11:50:20 +0100

    basic implementation of PropertiesControlledModel

--- src/gui/desktop/models/properties_controlled_model.cpp
+++ src/gui/desktop/models/properties_controlled_model.cpp
@@ -1,2 +1,20 @@
 
 #include "properties_controlled_model.hpp"
+
+PropertiesControlledModel::PropertiesControlledModel(QObject* parent)
+    : QAbstractListModel(parent)
+{
+
+}
+
+
+QVariant PropertiesControlledModel::data(const QModelIndex& index, int role) const
+{
+    return m_model.data(index, role);
+}
+
+
+int PropertiesControlledModel::rowCount(const QModelIndex& parent) const
+{
+    return m_model.rowCount(parent);
+}
--- src/gui/desktop/models/properties_controlled_model.hpp
+++ src/gui/desktop/models/properties_controlled_model.hpp
@@ -3,12 +3,25 @@
 #define PROPERTIES_TO_FILTERS_HPP_INCLUDED
 
 
-#include <QObject>
+#include <QAbstractListModel>
+
+#include "flat_model.hpp"
 
 
-class PropertiesControlledModel: public QObject
+class PropertiesControlledModel: public QAbstractListModel
 {
+    Q_OBJECT
+    // Q_PROPERTY(APhotoInfoModel model READ model WRITE setModel)
 
+    public:
+        PropertiesControlledModel(QObject* parent = nullptr);
+        ~PropertiesControlledModel() = default;
+
+        QVariant data(const QModelIndex & index, int role) const override;
+        int rowCount(const QModelIndex & parent) const override;
+
+    private:
+        FlatModel m_model;
 };
 
 #endif // PROPERTIES_TO_FILTERS_HPP_INCLUDED
