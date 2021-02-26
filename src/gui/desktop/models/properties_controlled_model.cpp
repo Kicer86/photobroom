d8e1b04edf18f3de8816c4e5fa0240d96423e816
Michał Walenciak <Kicer86@gmail.com>
2021-02-26 20:21:06 +0100

    some initial filter build mechanism

--- src/gui/desktop/models/properties_controlled_model.cpp
+++ src/gui/desktop/models/properties_controlled_model.cpp
@@ -24,3 +24,58 @@ void PropertiesControlledModel::setDatabase(Database::IDatabase* db)
 {
     m_model.setDatabase(db);
 }
+
+
+QVariantMap PropertiesControlledModel::generalFlags() const
+{
+    return m_generalFlags;
+}
+
+
+QVariantMap PropertiesControlledModel::tags() const
+{
+    return m_tags;
+}
+
+
+void PropertiesControlledModel::setGeneralFlags(const QVariantMap& generalFlags)
+{
+    m_generalFlags = generalFlags;
+
+    emit generalFlagsChanged(m_generalFlags);
+
+    updateFilters();
+}
+
+
+void PropertiesControlledModel::setTags(const QVariantMap& tags)
+{
+    m_tags = tags;
+
+    emit generalFlagsChanged(m_tags);
+
+    updateFilters();
+}
+
+
+void PropertiesControlledModel::updateFilters()
+{
+    Database::Filter filter = tagsFilter();
+
+    m_model.setFilter(filter);
+}
+
+
+Database::Filter PropertiesControlledModel::tagsFilter()
+{
+    std::vector<Database::Filter> filters;
+
+    for (auto it = m_tags.begin(); it != m_tags.end(); ++it)
+    {
+        if (it.key() == "Date")
+            filters.push_back(Database::FilterPhotosWithTag(TagTypes::Date, TagValue::fromQVariant(it.value()),
+                                                            Database::FilterPhotosWithTag::ValueMode::Equal, true));
+    }
+
+    return Database::GroupFilter(filters);
+}
--- src/gui/desktop/models/properties_controlled_model.hpp
+++ src/gui/desktop/models/properties_controlled_model.hpp
@@ -14,6 +14,9 @@ class PropertiesControlledModel: public QObject
     Q_PROPERTY(QAbstractItemModel* model READ model NOTIFY modelChanged)
     Q_PROPERTY(Database::IDatabase* database READ database WRITE setDatabase)
 
+    Q_PROPERTY(QVariantMap generalFlags READ generalFlags WRITE setGeneralFlags NOTIFY generalFlagsChanged)
+    Q_PROPERTY(QVariantMap tags READ tags WRITE setTags NOTIFY tagsChanged)
+
     public:
         PropertiesControlledModel(QObject* parent = nullptr);
         ~PropertiesControlledModel() = default;
@@ -23,11 +26,25 @@ class PropertiesControlledModel: public QObject
         Database::IDatabase* database() const;
         void setDatabase(Database::IDatabase *);
 
+        QVariantMap generalFlags() const;
+        QVariantMap tags() const;
+        void setGeneralFlags(const QVariantMap &);
+        void setTags(const QVariantMap &);
+
     signals:
         void modelChanged() const;
 
+        void generalFlagsChanged(const QVariantMap &) const;
+        void tagsChanged(const QVariantMap &) const;
+
     private:
         FlatModel m_model;
+
+        QVariantMap m_generalFlags;
+        QVariantMap m_tags;
+
+        void updateFilters();
+        Database::Filter tagsFilter();
 };
 
 #endif // PROPERTIES_TO_FILTERS_HPP_INCLUDED
