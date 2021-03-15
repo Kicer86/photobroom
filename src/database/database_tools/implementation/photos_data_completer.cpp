d2ce4bb295c32c86cbf4a935d331e41753be7555
Michał Walenciak <Kicer86@gmail.com>
2021-03-15 23:24:01 +0100

    collect photos matching our needs

--- src/database/database_tools/implementation/photos_data_completer.cpp
+++ src/database/database_tools/implementation/photos_data_completer.cpp
@@ -1,2 +1,33 @@
 
+#include <functional>
+
+#include "core/function_wrappers.hpp"
+#include "database/filter.hpp"
+#include "database/iphoto_operator.hpp"
 #include "../photos_data_completer.hpp"
+
+
+using namespace std::placeholders;
+
+
+PhotosDataCompleter::PhotosDataCompleter(Database::IDatabase& db)
+    : m_db(db)
+{
+    m_db.exec(std::bind(&PhotosDataCompleter::proces, this, _1));
+}
+
+
+void PhotosDataCompleter::proces(Database::IBackend& backend)
+{
+    const Database::FilterPhotosWithFlags analyzed({ { Photo::FlagsE::ExifLoaded, 1 } });
+    const Database::FilterPhotosWithTag noDate(TagTypes::Date, TagValue(), Database::FilterPhotosWithTag::ValueMode::Equal, true);
+    const Database::GroupFilter filters = {analyzed, noDate};
+    const auto photos = backend.photoOperator().getPhotos(filters);
+
+    invokeMethod(this, &PhotosDataCompleter::photosFetched, photos);
+}
+
+
+void PhotosDataCompleter::photosFetched(const std::vector<Photo::Id>& ids)
+{
+}
--- src/database/database_tools/photos_data_completer.hpp
+++ src/database/database_tools/photos_data_completer.hpp
@@ -3,13 +3,23 @@
 #define PHOTOSDATACOMPLETER_HPP
 
 #include "database_export.h"
+#include "database/idatabase.hpp"
+
 
 /**
  * @brief class provides photos with extra information guessed from filename, content etc.
  */
-class DATABASE_EXPORT PhotosDataCompleter
+class DATABASE_EXPORT PhotosDataCompleter: public QObject
 {
 public:
+    PhotosDataCompleter(Database::IDatabase &);
+
+
+private:
+    Database::IDatabase& m_db;
+
+    void proces(Database::IBackend &);
+    void photosFetched(const std::vector<Photo::Id> &);
 };
 
 #endif
