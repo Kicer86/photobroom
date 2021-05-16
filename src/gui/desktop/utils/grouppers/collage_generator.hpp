2b07b7c357dc0fd4876834b4599e6b2afa004099
Michał Walenciak <Kicer86@gmail.com>
2021-05-16 16:41:36 +0200

    dropping dead code

--- src/gui/desktop/utils/grouppers/collage_generator.hpp
+++ src/gui/desktop/utils/grouppers/collage_generator.hpp
@@ -18,11 +18,6 @@ class CollageGenerator
         IExifReader& m_exifReader;
 
         QImage merge(const QList<QImage> &) const;
-        QImage merge(const QImage& lhs, const QImage& rhs) const;
-        QImage horizontalMerge(const QImage& lhs, const QImage& rhs) const;
-        QImage verticalMerge(const QImage& lhs, const QImage& rhs) const;
-        void adjustHorizontalSizes(QImage& lhs, QImage& rhs) const;
-        void adjustVerticalSizes(QImage& lhs, QImage& rhs) const;
 };
 
 #endif
