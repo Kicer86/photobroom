60b00e0c59d86354bf6279af068cc9e7b754131d
Michał Walenciak <Kicer86@gmail.com>
2021-06-01 17:46:33 +0200

    more unit tests to cover all cases

--- src/database/unit_tests_for_backends/thumbnails_tests.cpp
+++ src/database/unit_tests_for_backends/thumbnails_tests.cpp
@@ -25,3 +25,34 @@ TYPED_TEST(ThumbnailsTest, storesThumbnail)
 
     EXPECT_EQ(thumbnail, "thumbnail");
 }
+
+
+TYPED_TEST(ThumbnailsTest, thumbnailOverride)
+{
+    Photo::DataDelta photo;
+    photo.insert<Photo::Field::Path>("/path/photo.jpeg");
+    std::vector<Photo::DataDelta> photos { photo };
+
+    this->m_backend->addPhotos(photos);
+    const auto id = photos.front().getId();
+
+    this->m_backend->setThumbnail(id, QByteArray("thumbnail"));
+    this->m_backend->setThumbnail(id, QByteArray("thumbnail2"));
+    const QByteArray thumbnail = this->m_backend->getThumbnail(id);
+
+    EXPECT_EQ(thumbnail, "thumbnail2");
+}
+
+
+TYPED_TEST(ThumbnailsTest, emptyResultWhenMissing)
+{
+    Photo::DataDelta photo;
+    photo.insert<Photo::Field::Path>("/path/photo.jpeg");
+    std::vector<Photo::DataDelta> photos { photo };
+
+    this->m_backend->addPhotos(photos);
+    const auto id = photos.front().getId();
+    const QByteArray thumbnail = this->m_backend->getThumbnail(id);
+
+    EXPECT_TRUE(thumbnail.isEmpty());
+}
