1c05b3cee84de982bf12e10e74d9266de60a412d
Michał Walenciak <Kicer86@gmail.com>
2021-06-03 21:52:45 +0200

    fetch image or generate it when it does not exist

--- src/database/database_tools/db_thumbnails_accessor.hpp
+++ src/database/database_tools/db_thumbnails_accessor.hpp
@@ -2,6 +2,8 @@
 #ifndef DB_THUMBNAILS_ACCESSOR_HPP_INCLUDED
 #define DB_THUMBNAILS_ACCESSOR_HPP_INCLUDED
 
+#include <core/ithumbnails_generator.hpp>
+
 #include "ithumbnails_accessor.hpp"
 #include "idatabase.hpp"
 #include "database_export.h"
@@ -12,12 +14,13 @@ namespace Database
     class DATABASE_EXPORT ThumbnailsAccessor: public IThumbnailsAccessor
     {
         public:
-            ThumbnailsAccessor(IDatabase &);
+            ThumbnailsAccessor(IDatabase &, IThumbnailsGenerator &);
 
             QImage getThumbnail(const Photo::Id &) override;
 
         private:
             IDatabase& m_db;
+            IThumbnailsGenerator& m_generator;
     };
 }
 
--- src/database/database_tools/implementation/db_thumbnails_accessor.cpp
+++ src/database/database_tools/implementation/db_thumbnails_accessor.cpp
@@ -1,24 +1,46 @@
 
+#include <QBuffer>
+
 #include <core/task_executor_utils.hpp>
 
 #include "../db_thumbnails_accessor.hpp"
 #include "database_executor_traits.hpp"
 
+
 namespace Database
 {
-    ThumbnailsAccessor::ThumbnailsAccessor(IDatabase& db)
+    ThumbnailsAccessor::ThumbnailsAccessor(IDatabase& db, IThumbnailsGenerator& generator)
         : m_db(db)
+        , m_generator(generator)
     {
 
     }
 
     QImage ThumbnailsAccessor::getThumbnail(const Photo::Id& id)
     {
-        const QByteArray rawImage = evaluate<QByteArray(Database::IBackend &)>(m_db, [](IBackend& backend)
-        {
-            return QByteArray();
-        });
+        const QByteArray thumbnail =
+            evaluate<QByteArray(Database::IBackend &)>(m_db, [id, this](IBackend& backend)
+            {
+                QByteArray thumbnail = backend.getThumbnail(id);
 
-        return QImage();
+                if (thumbnail.isEmpty())
+                {
+                    const auto data = backend.getPhotoDelta(id, {Photo::Field::Path});
+                    const auto path = data.get<Photo::Field::Path>();
+                    const auto image = m_generator.generate(path, {QSize(400, 400)});
+
+                    QBuffer buf(&thumbnail);
+                    image.save(&buf, "JPG");
+
+                    backend.setThumbnail(id, thumbnail);
+                }
+
+                return thumbnail;
+            });
+
+        QImage image;
+        image.load(thumbnail, "JPG");
+
+        return image;
     }
 }
