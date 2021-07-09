e6322fa1c4c6b1b5e82c063ca72bebb2a1da412d
Michał Walenciak <Kicer86@gmail.com>
2021-07-09 23:23:08 +0200

    comment

--- src/gui/unit_tests/model/flat_model_tests.cpp
+++ src/gui/unit_tests/model/flat_model_tests.cpp
@@ -506,7 +506,7 @@ TEST_F(FlatModelTest, complexChangesByBackend)
 
     EXPECT_CALL(photoOperator, onPhotos(_, _))
         .WillOnce(Return(initial_photos_set))                 // first call after db set
-        .WillOnce(Return(final_photos_set));                  // second call after backend.photosAdded
+        .WillOnce(Return(final_photos_set));                  // second call after backend.photosAdded. TODO: this is not nice that test is aware of implementation
 
     model.setDatabase(&db);
     backend.photosRemoved( {Photo::Id(11), Photo::Id(12), Photo::Id(13), Photo::Id(17), Photo::Id(18)} );
