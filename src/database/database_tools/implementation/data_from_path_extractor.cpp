0ad13d4bf5d64f957a9a60b33d21f5551b95053d
Michał Walenciak <Kicer86@gmail.com>
2021-08-02 12:22:15 +0200

    introducing DataFromPathExtractor

---
+++ src/database/database_tools/implementation/data_from_path_extractor.cpp
@@ -0,0 +1,63 @@
+
+#include <QDate>
+
+#include <core/tag.hpp>
+#include "data_from_path_extractor.hpp"
+
+
+DataFromPathExtractor::DataFromPathExtractor()
+//            <  YEAR  >     <  MONTH >     <  DAY   >[   _      <  HOUR  >         < MINUTE >         < SECOND >]
+    : m_expr("([0-9]{4})[.-]?([0-9]{2})[.-]?([0-9]{2})(?:[_ \\-]?([0-9]{2})[\\-.;_]?([0-9]{2})[\\-.;_]?([0-9]{2}))?")
+{
+
+}
+
+
+Tag::TagsList DataFromPathExtractor::extract(const QString& path)
+{
+    auto matchIt = m_expr.globalMatch(path);
+
+    std::vector<QRegularExpressionMatch> matches;
+    while (matchIt.hasNext())
+    {
+        QRegularExpressionMatch match = matchIt.next();
+        matches.push_back(match);
+    };
+
+    // in case of more than one match, put the richer matches first
+    std::sort(matches.begin(), matches.end(), [](const QRegularExpressionMatch& lhs, const QRegularExpressionMatch& rhs) {
+        return lhs.lastCapturedIndex() > rhs.lastCapturedIndex();
+    });
+
+    Tag::TagsList tags;
+
+    for(const auto& match: matches)
+    {
+        const QStringList captured = match.capturedTexts();
+
+        const int y = captured[1].toInt();
+        const int m = captured[2].toInt();
+        const int d = captured[3].toInt();
+        const QDate date(y, m, d);
+
+        if (date.isValid())
+        {
+            tags.emplace(TagTypes::Date, date);
+
+            if (captured.size() == 7)
+            {
+                const int hh = captured[4].toInt();
+                const int mm = captured[5].toInt();
+                const int ss = captured[6].toInt();
+                const QTime time(hh, mm, ss);
+
+                if (time.isValid())
+                    tags.emplace(TagTypes::Time, time);
+            }
+
+            break;
+        }
+    }
+
+    return tags;
+}
---
+++ src/database/unit_tests/data_from_path_extractor_tests.cpp
@@ -0,0 +1,79 @@
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "database_tools/implementation/data_from_path_extractor.cpp"
+
+
+using testing::ElementsAre;
+using testing::UnorderedElementsAre;
+
+
+TEST(DataFromPathExtractorTest, NoDate)
+{
+    DataFromPathExtractor extractor;
+
+    const auto tags1 = extractor.extract("qwer/rertty/11/23/fdgfg/h.jpeg");
+    const auto tags2 = extractor.extract("");
+    const auto tags3 = extractor.extract({});
+    const auto tags4 = extractor.extract("00000000000000000000000000000000000000000000000000000000");
+
+    EXPECT_TRUE(tags1.empty());
+    EXPECT_TRUE(tags2.empty());
+    EXPECT_TRUE(tags3.empty());
+    EXPECT_TRUE(tags4.empty());
+}
+
+
+TEST(DataFromPathExtractorTest, InvalidDate)
+{
+    DataFromPathExtractor extractor;
+
+    const auto tags1 = extractor.extract("2010-13-12");
+    const auto tags2 = extractor.extract("2010-01-32");
+
+    EXPECT_TRUE(tags1.empty());
+    EXPECT_TRUE(tags2.empty());
+}
+
+
+TEST(DataFromPathExtractorTest, ValidDate)
+{
+    DataFromPathExtractor extractor;
+
+    const auto tags1 = extractor.extract("Mandrake.20071106.gif");
+    const auto tags2 = extractor.extract("2022-12-50_11-99-99/2010.10-11/11112010039.mp4");
+    const auto tags3 = extractor.extract("/2010.05.22-23/DSCF8415.mkv");
+    const auto tags4 = extractor.extract("2017-06-01-QQQ-WWW/024.jpg");
+
+    EXPECT_THAT( tags1, ElementsAre( std::pair{TagTypes::Date, QDate(2007, 11, 06)} ) );
+    EXPECT_THAT( tags2, ElementsAre( std::pair{TagTypes::Date, QDate(2010, 10, 11)} ) );
+    EXPECT_THAT( tags3, ElementsAre( std::pair{TagTypes::Date, QDate(2010, 05, 22)} ) );
+    EXPECT_THAT( tags4, ElementsAre( std::pair{TagTypes::Date, QDate(2017, 06, 01)} ) );
+}
+
+
+TEST(DataFromPathExtractorTest, ValidDateAndTime)
+{
+    DataFromPathExtractor extractor;
+
+    const auto tags1 = extractor.extract("WP_20150705_135843Z.mp4");
+    const auto tags2 = extractor.extract("/dir/Camera 2/20141122_164931.mp4");
+    const auto tags3 = extractor.extract("qq/ww/2008-07-06_13-55-04_1280x1024_screenshot.png");
+    const auto tags4 = extractor.extract("/2010-12-07-21_55_11.png");
+    const auto tags5 = extractor.extract("jk/2020-12-01_19_05_39 1-0.png");
+    const auto tags6 = extractor.extract("mm/20161215_092517.mp4");
+    const auto tags7 = extractor.extract("20005678-123456/WP_20180123_09_44_08_Pro.mp4");
+    const auto tags8 = extractor.extract("/2015-10-20/20151020183114.MTS");
+    const auto tags9 = extractor.extract("2020-07-21-sdfgg_t_kjyy/2020-07-21/20200721104521.MTS");
+
+    EXPECT_THAT( tags1, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2015, 07, 05)}, std::pair{TagTypes::Time, QTime(13, 58, 43)} ) );
+    EXPECT_THAT( tags2, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2014, 11, 22)}, std::pair{TagTypes::Time, QTime(16, 49, 31)} ) );
+    EXPECT_THAT( tags3, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2008, 07, 06)}, std::pair{TagTypes::Time, QTime(13, 55, 04)} ) );
+    EXPECT_THAT( tags4, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2010, 12, 07)}, std::pair{TagTypes::Time, QTime(21, 55, 11)} ) );
+    EXPECT_THAT( tags5, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2020, 12, 01)}, std::pair{TagTypes::Time, QTime(19, 05, 39)} ) );
+    EXPECT_THAT( tags6, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2016, 12, 15)}, std::pair{TagTypes::Time, QTime( 9, 25, 17)} ) );
+    EXPECT_THAT( tags7, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2018, 01, 23)}, std::pair{TagTypes::Time, QTime( 9, 44,  8)} ) );
+    EXPECT_THAT( tags8, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2015, 10, 20)}, std::pair{TagTypes::Time, QTime(18, 31, 14)} ) );
+    EXPECT_THAT( tags9, UnorderedElementsAre( std::pair{TagTypes::Date, QDate(2020, 07, 21)}, std::pair{TagTypes::Time, QTime(10, 45, 21)} ) );
+}
