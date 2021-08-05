a5c6e969cccd577ce0f1c8fb3a6a1df5fa98581b
Michał Walenciak <Kicer86@gmail.com>
2021-08-05 07:10:35 +0200

    useless captures removal

--- src/database/database_tools/implementation/series_detector.cpp
+++ src/database/database_tools/implementation/series_detector.cpp
@@ -261,7 +261,7 @@ SeriesDetector::SeriesDetector(Database::IDatabase& db, IExifReader& exif, const
 std::vector<GroupCandidate> SeriesDetector::listCandidates(const Rules& rules) const
 {
     const std::deque<Photo::Data> candidates =
-        evaluate<std::deque<Photo::Data>(Database::IBackend &)>(m_db, [this, &rules](Database::IBackend& backend)
+        evaluate<std::deque<Photo::Data>(Database::IBackend &)>(m_db, [](Database::IBackend& backend)
     {
         std::vector<GroupCandidate> result;
 
