2d7fb9f16bc3024f8c3810baf196f3dc40a784be
Michał Walenciak <Kicer86@gmail.com>
2021-08-26 21:14:12 +0200

    introducing slicing algorithm

---
+++ src/core/slicer.hpp
@@ -0,0 +1,23 @@
+
+#ifndef SLICER_HPP_INCLUDED
+#define SLICER_HPP_INCLUDED
+
+
+template<typename T, typename O>
+void slice(T first, T last, long chunkSize, O o)
+{
+    while(first != last)
+    {
+        const auto maxChunkSize = last - first;
+        chunkSize = std::min(chunkSize, maxChunkSize);
+
+        auto from = first;
+        auto to = first + chunkSize;
+
+        o(from, to);
+
+        first += chunkSize;
+    }
+}
+
+#endif
