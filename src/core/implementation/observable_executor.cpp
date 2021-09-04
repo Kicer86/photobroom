3f98464fdc0cee5340d33e82006993c9b5c77fbe
Michał Walenciak <Kicer86@gmail.com>
2021-09-04 09:53:57 +0200

    disable ObservableExecutor when build in Release

--- src/core/implementation/observable_executor.cpp
+++ src/core/implementation/observable_executor.cpp
@@ -2,21 +2,33 @@
 #include "observable_executor.hpp"
 #include "observables_registry.hpp"
 
+#ifndef NDEBUG
+#define IF_ENABLED
+#else
+#define IF_ENABLED if (false)
+#endif
+
 
 ObservableExecutor::ObservableExecutor()
 {
-    ObservablesRegistry::instance().add(this);
+    IF_ENABLED
+    {
+        ObservablesRegistry::instance().add(this);
 
-    m_executionSpeedBuffer.fill(0);
+        m_executionSpeedBuffer.fill(0);
 
-    connect(&m_executionSpeedTimer, &QTimer::timeout, this, &ObservableExecutor::updateExecutionSpeed);
-    m_executionSpeedTimer.start(std::chrono::seconds(1));
+        connect(&m_executionSpeedTimer, &QTimer::timeout, this, &ObservableExecutor::updateExecutionSpeed);
+        m_executionSpeedTimer.start(std::chrono::seconds(1));
+    }
 }
 
 
 ObservableExecutor::~ObservableExecutor()
 {
-    ObservablesRegistry::instance().remove(this);
+    IF_ENABLED
+    {
+        ObservablesRegistry::instance().remove(this);
+    }
 }
 
 
@@ -40,39 +52,51 @@ double ObservableExecutor::executionSpeed() const
 
 void ObservableExecutor::newTaskInQueue()
 {
-    m_awaitingTasks++;
+    IF_ENABLED
+    {
+        m_awaitingTasks++;
 
-    emit awaitingTasksChanged(m_awaitingTasks);
+        emit awaitingTasksChanged(m_awaitingTasks);
+    }
 }
 
 
 void ObservableExecutor::taskMovedToExecution()
 {
-    m_awaitingTasks--;
-    m_tasksExecuted++;
+    IF_ENABLED
+    {
+        m_awaitingTasks--;
+        m_tasksExecuted++;
 
-    emit awaitingTasksChanged(m_awaitingTasks);
-    emit tasksExecutedChanged(m_tasksExecuted);
+        emit awaitingTasksChanged(m_awaitingTasks);
+        emit tasksExecutedChanged(m_tasksExecuted);
+    }
 }
 
 
 void ObservableExecutor::taskExecuted()
 {
-    m_tasksExecuted--;
-    m_executionSpeedCounter++;
+    IF_ENABLED
+    {
+        m_tasksExecuted--;
+        m_executionSpeedCounter++;
 
-    emit tasksExecutedChanged(m_tasksExecuted);
+        emit tasksExecutedChanged(m_tasksExecuted);
+    }
 }
 
 
 void ObservableExecutor::updateExecutionSpeed()
 {
-    const std::size_t bufferSize = m_executionSpeedBuffer.size();
-    m_executionSpeedBuffer[m_executionSpeedBufferPos] = m_executionSpeedCounter;
-    m_executionSpeedBufferPos = (m_executionSpeedBufferPos + 1) % bufferSize;
-    m_executionSpeedCounter = 0;
+    IF_ENABLED
+    {
+        const std::size_t bufferSize = m_executionSpeedBuffer.size();
+        m_executionSpeedBuffer[m_executionSpeedBufferPos] = m_executionSpeedCounter;
+        m_executionSpeedBufferPos = (m_executionSpeedBufferPos + 1) % bufferSize;
+        m_executionSpeedCounter = 0;
 
-    m_executionSpeed = std::accumulate(m_executionSpeedBuffer.begin(), m_executionSpeedBuffer.end(), 0) / static_cast<double>(bufferSize);
+        m_executionSpeed = std::accumulate(m_executionSpeedBuffer.begin(), m_executionSpeedBuffer.end(), 0) / static_cast<double>(bufferSize);
 
-    emit executionSpeedChanged(m_executionSpeed);
+        emit executionSpeedChanged(m_executionSpeed);
+    }
 }
