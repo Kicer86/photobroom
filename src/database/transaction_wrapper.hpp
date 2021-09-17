769349583c65c508f90fb68065f75ff54a20fd2a
Michał Walenciak <Kicer86@gmail.com>
2021-09-17 23:31:11 +0200

    introducing transaction wrapper
    common code for transaction mechanisms

---
+++ src/database/transaction_wrapper.hpp
@@ -0,0 +1,58 @@
+
+#ifndef ATRANSACTION_HPP_INCLUDED
+#define ATRANSACTION_HPP_INCLUDED
+
+#include "itransaction.hpp"
+
+
+namespace Database
+{
+    template<typename T>
+    class TransactionWrapper: public Database::ITransaction, T
+    {
+        public:
+            template<typename ...Args>
+            TransactionWrapper(Args&... args): T(args...) {}
+
+            ~TransactionWrapper()
+            {
+                if (m_abort)                    // on abort restore saved state
+                    T::rollback();
+                else
+                    T::commit();
+            }
+
+            void abort() override
+            {
+                m_abort = true;
+            }
+
+        private:
+            bool m_abort = false;
+    };
+
+
+    template<typename T> requires std::is_base_of<ITransaction, T>::value
+    class TransactionManager
+    {
+        public:
+            template<typename ...Args>
+            std::shared_ptr<T> openTransaction(Args&... args)
+            {
+                auto tr = m_tr.lock();
+
+                if (tr.get() == nullptr)
+                {
+                    tr = std::make_shared<T>(args...);
+                    m_tr = tr;
+                }
+
+                return tr;
+            }
+
+        private:
+            std::weak_ptr<T> m_tr;
+    };
+}
+
+#endif
