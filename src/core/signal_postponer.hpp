877118eecf760e425217f80cfc050825ed55541c
Michał Walenciak <Kicer86@gmail.com>
2021-09-24 16:31:02 +0200

    return SignalBlocker as result

--- src/core/signal_postponer.hpp
+++ src/core/signal_postponer.hpp
@@ -115,14 +115,16 @@ private:
 
 
 template<typename SrcObj, typename Signal1, typename Dst, typename Signal2>
-void blocked_connect(SrcObj* src, Signal1 sig1,
-                     Dst* dst, Signal2 slot1,
-                     const std::chrono::milliseconds& block = std::chrono::milliseconds(0),
-                     Qt::ConnectionType type = Qt::AutoConnection)
+SignalBlocker* blocked_connect(SrcObj* src, Signal1 sig1,
+                               Dst* dst, Signal2 slot1,
+                               const std::chrono::milliseconds& block = std::chrono::milliseconds(0),
+                               Qt::ConnectionType type = Qt::AutoConnection)
 {
     SignalBlocker* blocker = new SignalBlocker(block, src);
     QObject::connect(src, sig1, blocker, &SignalBlocker::notify);
     QObject::connect(blocker, &SignalBlocker::fire, dst, slot1, type);
+
+    return blocker;
 }
 
 #endif // SIGNALPOSTPONER_HPP
