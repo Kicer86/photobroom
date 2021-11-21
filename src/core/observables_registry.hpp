90bc6888d21430c0c54f957865dab958e76fa96d
Michał Walenciak <Kicer86@gmail.com>
2021-11-21 16:18:21 +0100

    make it possible to disable ObservablesRegistry

--- src/core/observables_registry.hpp
+++ src/core/observables_registry.hpp
@@ -14,19 +14,24 @@ class CORE_EXPORT ObservablesRegistry: public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QSet<ObservableExecutor *> executors READ executors NOTIFY executorsChanged)
+    Q_PROPERTY(bool enabled READ isEnabled NOTIFY enabledChanged)
 
     public:
         static ObservablesRegistry& instance();
 
+        void enable(bool);
+        bool isEnabled() const;
         const QSet<ObservableExecutor *>& executors() const;
         void add(ObservableExecutor *);
         void remove(ObservableExecutor *);
 
     signals:
         void executorsChanged(const QSet<ObservableExecutor *> &) const;
+        void enabledChanged(bool);
 
     private:
         QSet<ObservableExecutor *> m_executors;
+        bool m_enabled;
 
         ObservablesRegistry() = default;
 };
