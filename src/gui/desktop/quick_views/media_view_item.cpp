db85f1cc17b4b4372536b8ae9734e49b2258f02e
Michał Walenciak <Kicer86@gmail.com>
2021-12-02 21:27:03 +0100

    basic implementation of media presentation

--- src/gui/desktop/quick_views/media_view_item.cpp
+++ src/gui/desktop/quick_views/media_view_item.cpp
@@ -1,2 +1,51 @@
 
+#include <QPainter>
+
+#include <core/function_wrappers.hpp>
 #include "media_view_item.hpp"
+#include "objects_accessor.hpp"
+
+
+MediaViewItem::MediaViewItem()
+{
+    connect(this, &MediaViewItem::sourceChanged, this, &MediaViewItem::reload);
+}
+
+
+void MediaViewItem::paint(QPainter *painter)
+{
+    if (m_image.isNull() == false)
+    {
+        const QSize canvasSize = size().toSize();
+        const QImage scaled = m_image.scaled(canvasSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+        const QRect imgRect(QPoint(), scaled.size());
+        QRect paintRect = imgRect;
+        paintRect.moveCenter(QPoint(canvasSize.width() / 2, canvasSize.height() / 2));
+
+        painter->drawImage(paintRect.topLeft(), scaled);
+    }
+}
+
+
+void MediaViewItem::reload(const Photo::Id& id)
+{
+    Database::IDatabase* db = ObjectsAccessor::instance().database();
+
+    if (db)
+    {
+        db->exec([this, id](Database::IBackend& backend)
+        {
+            const Photo::Data data = backend.getPhoto(id);
+
+            invokeMethod(this, &MediaViewItem::setImage, data.path);
+        });
+    }
+}
+
+
+void MediaViewItem::setImage(const QString& path)
+{
+    m_image = QImage(path);
+
+    update();
+}
--- src/gui/desktop/quick_views/media_view_item.hpp
+++ src/gui/desktop/quick_views/media_view_item.hpp
@@ -4,8 +4,18 @@
 
 #include "amedia_item.hpp"
 
-class MediaViewItem : public AMediaItem
+class MediaViewItem: public AMediaItem
 {
+public:
+    MediaViewItem();
+
+    void paint(QPainter *painter) override;
+
+private:
+    QImage m_image;
+
+    void reload(const Photo::Id &);
+    void setImage(const QString &);
 };
 
 #endif
