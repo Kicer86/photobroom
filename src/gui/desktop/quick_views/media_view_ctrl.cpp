b4ba3419a7cd716330c66fc5515e51c3be5d9352
Michał Walenciak <Kicer86@gmail.com>
2021-12-03 18:22:19 +0100

    set path and mode accordingly do photo's id

--- src/gui/desktop/quick_views/media_view_ctrl.cpp
+++ src/gui/desktop/quick_views/media_view_ctrl.cpp
@@ -1,8 +1,58 @@
 
+#include <core/function_wrappers.hpp>
+#include <core/media_types.hpp>
+
 #include "media_view_ctrl.hpp"
+#include "objects_accessor.hpp"
+
+
+void MediaViewCtrl::setSource(const Photo::Id& id)
+{
+    m_id = id;
+
+    emit sourceChanged(id);
+
+    Database::IDatabase* db = ObjectsAccessor::instance().database();
+
+    if (db)
+    {
+        db->exec([this, id](Database::IBackend& backend)
+        {
+            const Photo::Data data = backend.getPhoto(id);
+
+            invokeMethod(this, &MediaViewCtrl::setPath, data.path);
+        });
+    }
+}
+
+
+const Photo::Id& MediaViewCtrl::source() const
+{
+    return m_id;
+}
+
+
+QString MediaViewCtrl::path() const
+{
+    return m_path;
+}
 
 
 MediaViewCtrl::Mode MediaViewCtrl::mode() const
 {
     return m_mode;
+}
+
+
+void MediaViewCtrl::setPath(const QString& path)
+{
+    m_path = path;
+
+    if (MediaTypes::isImageFile(m_path))
+    {
+        m_mode = Mode::StaticImage;
+        emit modeChanged(m_mode);
+    }
+
+    emit pathChanged(m_path);
 }
--- src/gui/desktop/quick_views/media_view_ctrl.hpp
+++ src/gui/desktop/quick_views/media_view_ctrl.hpp
@@ -4,9 +4,14 @@
 
 #include <QObject>
 
+#include <database/photo_types.hpp>
+
+
 class MediaViewCtrl: public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(Photo::Id photoID WRITE setSource READ source NOTIFY sourceChanged)
+    Q_PROPERTY(QString path READ path NOTIFY pathChanged)
     Q_PROPERTY(Mode mode READ mode NOTIFY modeChanged)
     Q_ENUMS(Mode)
 
@@ -18,13 +23,23 @@ public:
         AnimatedImage,
     };
 
+    void setSource(const Photo::Id &);
+
+    const Photo::Id& source() const;
+    QString path() const;
     Mode mode() const;
 
 signals:
+    void sourceChanged(const Photo::Id &) const;
+    void pathChanged(const QString &) const;
     void modeChanged(Mode) const;
 
 private:
+    QString m_path;
     Mode m_mode;
+    Photo::Id m_id;
+
+    void setPath(const QString &);
 };
 
 #endif
