2a0be4e19f31b1b193ef3d5efb345b0f76b2890e
Michał Walenciak <Kicer86@gmail.com>
2021-12-28 22:25:34 +0100

    handle time tag type

--- src/gui/desktop/quick_views/Components/VariantConverter.js
+++ src/gui/desktop/quick_views/Components/VariantConverter.js
@@ -4,8 +4,10 @@ function variantToString(type, variant) {
     if (type === TagEnums.Date) {
         var date = new Date(variant);
         return date.toLocaleDateString(Qt.locale(), Locale.ShortFormat);
-    }
-    else {
+    } else if (type == TagEnums.Time) {
+        var date = new Date(variant);
+        return date.toLocaleTimeString(Qt.locale(), "hh:mm:ss");
+    } else {
         return variant === undefined? null : variant.toString();
     }
 }
