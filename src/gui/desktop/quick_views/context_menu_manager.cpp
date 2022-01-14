8f5ddf71b50372d6d14840f1659adf4f59431776
Michał Walenciak <Kicer86@gmail.com>
2022-01-14 23:04:38 +0100

    prepare ContextMenuManager files

---
+++ src/gui/desktop/quick_views/context_menu_manager.cpp
@@ -0,0 +1,3 @@
+
+#include "context_menu_manager.hpp"
+
---
+++ src/gui/desktop/quick_views/context_menu_manager.hpp
@@ -0,0 +1,13 @@
+
+#ifndef CONTEXT_MENU_MANAGER_HPP_INCLUDED
+#define CONTEXT_MENU_MANAGER_HPP_INCLUDED
+
+
+#include <QObject>
+
+class ContextMenuManager: public QObject
+{
+    Q_OBJECT
+};
+
+#endif
