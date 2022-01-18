2f72bef57f620580b83bfe6f3bdaa6c1a6e27818
Michał Walenciak <Kicer86@gmail.com>
2022-01-18 22:55:28 +0100

    provide access to model via property

--- src/gui/desktop/quick_views/context_menu_manager.cpp
+++ src/gui/desktop/quick_views/context_menu_manager.cpp
@@ -1,3 +1,14 @@
 
 #include "context_menu_manager.hpp"
 
+
+ContextMenuManager::ContextMenuManager()
+{
+
+}
+
+
+const QAbstractItemModel* ContextMenuManager::model() const
+{
+    return &m_model;
+}
--- src/gui/desktop/quick_views/context_menu_manager.hpp
+++ src/gui/desktop/quick_views/context_menu_manager.hpp
@@ -4,10 +4,22 @@
 
 
 #include <QObject>
+#include "models/actions_model.hpp"
+
 
 class ContextMenuManager: public QObject
 {
     Q_OBJECT
+
+    Q_PROPERTY(const QAbstractItemModel* model READ model CONSTANT)
+
+public:
+    ContextMenuManager();
+
+    const QAbstractItemModel* model() const;
+
+private:
+    ActionsModel m_model;
 };
 
 #endif
