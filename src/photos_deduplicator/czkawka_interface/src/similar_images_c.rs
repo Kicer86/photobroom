a02db7a3b3b1f53899d1eeb4d4c28808e84a6b52
Michał Walenciak <Kicer86@gmail.com>
2022-04-06 19:07:45 +0200

    introduce free function for SimilarImages

--- src/photos_deduplicator/czkawka_interface/src/similar_images_c.rs
+++ src/photos_deduplicator/czkawka_interface/src/similar_images_c.rs
@@ -6,3 +6,9 @@ pub unsafe extern "C" fn CreateSimilarImagesObj() -> *mut ()
 {
     return Box::into_raw(Box::new(SimilarImages::new())) as *mut ();
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn DestroySimilarImagesObj(sim_img: &mut ())
+{
+    let _ = Box::from_raw(sim_img);
+}
