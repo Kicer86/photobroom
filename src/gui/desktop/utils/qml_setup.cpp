fa32bd9010629cabfecb95b26a3b4e40efd0023c
Michał Walenciak <Kicer86@gmail.com>
2022-04-23 13:02:04 +0200

    drop not required stuff

--- src/gui/desktop/utils/qml_setup.cpp
+++ src/gui/desktop/utils/qml_setup.cpp
@@ -4,7 +4,6 @@
 #include <database/database_tools/photos_data_guesser.hpp>
 
 #include "qml_setup.hpp"
-#include "models/flat_model.hpp"
 #include "models/tags_model.hpp"
 #include "models/photo_properties_model.hpp"
 #include "utils/variant_display.hpp"
@@ -17,8 +16,6 @@ void register_qml_types()
     qmlRegisterType<TagsModel>("photo_broom.models", 1, 0, "TagsModel");
     qmlRegisterType<Variant>("photo_broom.utils", 1, 0, "Variant");
     qRegisterMetaType<QAbstractItemModel*>("QAbstractItemModel*");
-    qmlRegisterInterface<Database::IDatabase>("Database::IDatabase", 1);
-    qmlRegisterInterface<FlatModel>("FlatModel", 1);
     qmlRegisterUncreatableMetaObject(Photo::staticMetaObject, "photo_broom.enums", 1, 0, "PhotoEnums", "Error: only enums");
     qmlRegisterUncreatableMetaObject(Tag::staticMetaObject, "photo_broom.enums", 1, 0, "TagEnums", "Error: only enums");
 }
