63bf4ba97cfcdc30fde7d3853104a1be2fe8d69c
Michał Walenciak <Kicer86@gmail.com>
2022-04-28 19:48:58 +0200

    provide base for DuplicatesModel

--- src/gui/desktop/models/duplicates_model.cpp
+++ src/gui/desktop/models/duplicates_model.cpp
@@ -1,3 +1,20 @@
 
 #include "duplicates_model.hpp"
 
+
+QVariant DuplicatesModel::data(const QModelIndex& index, int role ) const
+{
+    return {};
+}
+
+
+int DuplicatesModel::rowCount(const QModelIndex& parent ) const
+{
+    return m_duplicates.size();
+}
+
+
+void DuplicatesModel::setDB(Database::IDatabase* db)
+{
+    m_db = db;
+}
--- src/gui/desktop/models/duplicates_model.hpp
+++ src/gui/desktop/models/duplicates_model.hpp
@@ -3,9 +3,21 @@
 #define DUPLICATES_MODEL_HPP_INCLUDED
 
 
-class DuplicatesModel
-{
+#include <QAbstractListModel>
+#include <database/idatabase.hpp>
 
+
+class DuplicatesModel: public QAbstractListModel
+{
+public:
+    QVariant data(const QModelIndex & index, int role) const override;
+    int rowCount(const QModelIndex & parent) const override;
+
+    void setDB(Database::IDatabase *);
+
+private:
+    QList<int> m_duplicates;
+    Database::IDatabase* m_db;
 };
 
 #endif
