62613b2bfb1db8d250f59b9ee21aedf2e8a13737
Michał Walenciak <Kicer86@gmail.com>
2022-05-04 18:52:18 +0200

    fill DuplicatesModel with data

--- src/gui/desktop/models/duplicates_model.cpp
+++ src/gui/desktop/models/duplicates_model.cpp
@@ -1,4 +1,8 @@
 
+#include <core/function_wrappers.hpp>
+#include <database/iphoto_operator.hpp>
+#include <database/photo_utils.hpp>
+
 #include "duplicates_model.hpp"
 
 
@@ -8,13 +12,70 @@ QVariant DuplicatesModel::data(const QModelIndex& index, int role ) const
 }
 
 
-int DuplicatesModel::rowCount(const QModelIndex& parent ) const
+int DuplicatesModel::rowCount(const QModelIndex& parent) const
 {
-    return m_duplicates.size();
+    return parent.isValid()? 0: static_cast<int>(m_duplicates.size());
+}
+
+
+bool DuplicatesModel::canFetchMore(const QModelIndex &parent) const
+{
+    return parent.isValid()? false: m_db != nullptr && m_loaded == false;
+}
+
+
+void DuplicatesModel::fetchMore(const QModelIndex &parent)
+{
+    if (m_db && m_loaded == false &&parent.isValid() == false)
+    {
+        m_loaded = true;
+        auto resultCallback = make_cross_thread_function<std::vector<Photo::DataDelta>>(this, &DuplicatesModel::compileDuplicates);
+
+        m_db->exec([resultCallback](Database::IBackend& backend)
+        {
+            const auto ids = backend.photoOperator().onPhotos(Database::FilterSimilarPhotos{}, Database::Actions::Sort(Database::Actions::Sort::By::PHash));
+
+            std::vector<Photo::DataDelta> data;
+            data.reserve(ids.size());
+
+            for(const auto& id: ids)
+                data.push_back(backend.getPhotoDelta(id, {Photo::Field::PHash}));
+
+            resultCallback(data);
+        },
+        "Looking for photo duplicates"
+        );
+    }
 }
 
 
 void DuplicatesModel::setDB(Database::IDatabase* db)
 {
+    beginResetModel();
     m_db = db;
+    m_loaded = false;
+    endResetModel();
+}
+
+
+void DuplicatesModel::compileDuplicates(const std::vector<Photo::DataDelta>& duplicatePhotos)
+{
+    std::vector<std::vector<Photo::DataDelta>> grouped;
+
+    for(auto it = duplicatePhotos.begin(); it != duplicatePhotos.end();)
+    {
+        const auto currentPHash = it->get<Photo::Field::PHash>();
+
+        auto nextIt = std::find_if_not(it, duplicatePhotos.end(), [&currentPHash](const Photo::DataDelta& data) {
+            return data.get<Photo::Field::PHash>() == currentPHash;
+        });
+
+        grouped.push_back(std::vector(it, nextIt));
+
+        it = nextIt;
+    }
+
+    beginInsertRows({}, 0, grouped.size() - 1);
+    m_duplicates.swap(grouped);
+    endInsertRows();
 }
--- src/gui/desktop/models/duplicates_model.hpp
+++ src/gui/desktop/models/duplicates_model.hpp
@@ -12,12 +12,17 @@ class DuplicatesModel: public QAbstractListModel
 public:
     QVariant data(const QModelIndex & index, int role) const override;
     int rowCount(const QModelIndex & parent) const override;
+    bool canFetchMore(const QModelIndex &parent) const override;
+    void fetchMore(const QModelIndex &parent) override;
 
     void setDB(Database::IDatabase *);
 
 private:
-    QList<int> m_duplicates;
-    Database::IDatabase* m_db;
+    std::vector<std::vector<Photo::DataDelta>> m_duplicates;
+    Database::IDatabase* m_db = nullptr;
+    bool m_loaded = false;
+
+    void compileDuplicates(const std::vector<Photo::DataDelta> &);
 };
 
 #endif
