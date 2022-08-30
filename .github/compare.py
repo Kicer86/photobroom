cb4e4f58505c8e61451dd384db2db351b059d307
Michał Walenciak <Kicer86@gmail.com>
2022-08-30 19:50:21 +0200

    Be more explicit

--- .github/compare.py
+++ .github/compare.py
@@ -1,6 +1,5 @@
 
-from sys import stdin
-from sys import exit
+import sys
 
 expected = open('.github/expected_file_list','rt')
 
@@ -9,14 +8,14 @@ for line in expected:
     expected_files.add(line.strip())
 
 package_files = set()
-for line in stdin:
+for line in sys.stdin:
     package_files.add(line.strip())
 
 missing_expected = list(expected_files - package_files)
 package_additional = list(package_files - expected_files)
 
 if missing_expected:
-    exit(f"There are missing files in package: {missing_expected}")
+    sys.exit(f"There are missing files in package: {missing_expected}")
 
 if package_additional:
-    exit(f"There are additional files in package: {package_additional}")
+    sys.exit(f"There are additional files in package: {package_additional}")
