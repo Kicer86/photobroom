58294b784cce6f7c89edbe0426526d353e8c1bbe
Michał Walenciak <Kicer86@gmail.com>
2022-08-30 14:48:08 +0200

    Sort list of expected files

--- .github/compare.py
+++ .github/compare.py
@@ -4,9 +4,19 @@ from sys import exit
 
 expected = open('.github/expected_file_list','rt')
 
-for line in stdin:
-    line = line.strip()
-    expected_line = next(expected).strip()
+expected_files = set()
+for line in expected:
+    expected_files.add(line.strip())
 
-    if line != expected_line:
-        exit(f"Expected file: {expected_line}, got {line} instead")
+package_files = set()
+for line in stdin:
+    package_files.add(line.strip())
+
+missing_expected = list(expected_files - package_files)
+package_additional = list(package_files - expected_files)
+
+if missing_expected:
+    exit(f"There are missing files in package: {missing_expected}")
+
+if package_additional:
+    exit(f"There are additional files in package: {package_additional}")
