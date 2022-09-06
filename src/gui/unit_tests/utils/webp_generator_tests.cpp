e43ee9c193e45405ee55ee46df6dd32301496139
Michał Walenciak <Kicer86@gmail.com>
2022-09-06 11:06:13 +0200

    Verify multiframe image

--- src/gui/unit_tests/utils/webp_generator_tests.cpp
+++ src/gui/unit_tests/utils/webp_generator_tests.cpp
@@ -18,3 +18,32 @@ TEST(WebPGeneratorTest, singleFrameImage)
 
     EXPECT_EQ(image.pixelColor(50, 50), frame.pixelColor(50, 50));
 }
+
+
+TEST(WebPGeneratorTest, multiFrameImage)
+{
+    QImage frame1(100, 100, QImage::Format_ARGB32);
+    frame1.fill(Qt::red);
+
+    QImage frame2(100, 100, QImage::Format_ARGB32);
+    frame2.fill(Qt::blue);
+
+    QImage frame3(100, 100, QImage::Format_ARGB32);
+    frame3.fill(Qt::green);
+
+    auto data = WebPGenerator().append(frame1).append(frame2).append(frame3).setLossless().save();
+
+    QBuffer buffer(&data);
+    QImageReader reader(&buffer);
+
+    ASSERT_EQ(reader.imageCount(), 3);
+
+    const auto image1 = reader.read();
+    EXPECT_EQ(image1.pixelColor(50, 50), frame1.pixelColor(50, 50));
+
+    const auto image2 = reader.read();
+    EXPECT_EQ(image2.pixelColor(50, 50), frame2.pixelColor(50, 50));
+
+    const auto image3 = reader.read();
+    EXPECT_EQ(image3.pixelColor(50, 50), frame3.pixelColor(50, 50));
+}
