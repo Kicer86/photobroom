0c35af91335af2375fc9a9f5b808c09b6e2ca43f
Michał Walenciak <Kicer86@gmail.com>
2022-09-18 12:33:48 +0200

    Remove special mng file format treatment

--- src/gui/features.hpp
+++ src/gui/features.hpp
@@ -5,7 +5,6 @@
 
 namespace gui::features
 {
-    const char* const MngFile      = "gui::image::mng";
     const char* const ToolAIS      = "gui::tool::align_image_stack";
     const char* const ToolExifTool = "gui::tool::exiftool";
 }
