ad964a2fbf8d17c3da374ee09027ea32800e0774
Michał Walenciak <Kicer86@gmail.com>
2023-02-19 15:17:42 +0100

    Use constants for filters

--- src/gui/desktop/models/qml_flat_model.hpp
+++ src/gui/desktop/models/qml_flat_model.hpp
@@ -12,11 +12,18 @@ class QMLFlatModel: public FlatModel
     Q_PROPERTY(Database::IDatabase* database WRITE setDatabase READ database REQUIRED)
     Q_PROPERTY(QStringList text_filters WRITE setTextFilters READ textFilters)
 
+    // filters
+    Q_PROPERTY(QString facesNotAnalysed READ facesNotAnalysedFilter CONSTANT)
+    Q_PROPERTY(QString validMedia READ validMediaFilter CONSTANT)
+
 public:
     QMLFlatModel();
 
     void setTextFilters(const QStringList& filters);
     const QStringList& textFilters() const;
+
+    QString facesNotAnalysedFilter() const;
+    QString validMediaFilter() const;
 
 private:
     QStringList m_filters;
--- src/gui/desktop/quick_items/Views/BatchFaceDetection.qml
+++ src/gui/desktop/quick_items/Views/BatchFaceDetection.qml
@@ -2,6 +2,7 @@
 import QtQuick 2.15
 import photo_broom.models
 import photo_broom.singletons
+import "ViewsComponents" as Internals
 
 
 Item {
@@ -9,6 +10,11 @@ Item {
     QMLFlatModel {
         id: data_model
         database: PhotoBroomProject.database
-        text_filters: ["faces not analysed"]
+        text_filters: [data_model.facesNotAnalysed, data_model.validMedia]
+    }
+
+    Internals.PhotosGridView {
+        anchors.fill: parent
+        model: data_model
     }
 }
