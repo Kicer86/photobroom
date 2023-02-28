274de24ec7dba651918fa2355bc4b09ee2c65a45
Michał Walenciak <Kicer86@gmail.com>
2023-02-28 21:15:14 +0100

    Read faces from photos

--- src/gui/desktop/utils/batch_face_detector.hpp
+++ src/gui/desktop/utils/batch_face_detector.hpp
@@ -2,32 +2,65 @@
 #ifndef BATCH_FACE_DETECTOR_HPP_INCLUDED
 #define BATCH_FACE_DETECTOR_HPP_INCLUDED
 
+#include <deque>
 #include <QObject>
 
+#include <database/idatabase.hpp>
 #include <core/icore_factory_accessor.hpp>
+#include <core/ilogger.hpp>
+
 #include "models/aphoto_data_model.hpp"
+#include "people_editor.hpp"
 
 
-
-class BatchFaceDetector: public QObject
+class BatchFaceDetector: public QAbstractListModel
 {
     Q_OBJECT
     Q_PROPERTY(ICoreFactoryAccessor* core WRITE setCore READ core REQUIRED)
+    Q_PROPERTY(Database::IDatabase* db WRITE setDB READ db REQUIRED)
     Q_PROPERTY(APhotoDataModel* photos_model WRITE setPhotosModel READ photosModel)
 
 public:
     void setPhotosModel(APhotoDataModel *);
     void setCore(ICoreFactoryAccessor *);
+    void setDB(Database::IDatabase *);
 
     APhotoDataModel* photosModel() const;
     ICoreFactoryAccessor* core() const;
+    Database::IDatabase* db() const;
+
+    int rowCount(const QModelIndex &) const override;
+    QVariant data(const QModelIndex &, int) const override;
 
 private:
+    struct Face
+    {
+        Face(std::unique_ptr<IFace> f, const QImage& img)
+            : faceData(std::move(f))
+            , faceImg(img)
+        {}
+
+        Face(const Face &) = delete;
+        Face(Face &&) = default;
+
+        Face& operator=(const Face &) = delete;
+        Face& operator=(Face &&) = default;
+
+        std::unique_ptr<IFace> faceData;
+        QImage faceImg;
+    };
+
+    std::deque<Photo::Id> m_ids;
+    std::vector<Face> m_faces;
+    std::unique_ptr<ILogger> m_logger;
     APhotoDataModel* m_photosModel = nullptr;
     ICoreFactoryAccessor* m_core = nullptr;
+    Database::IDatabase* m_db = nullptr;
 
-    // thread eating photos from model
+    void kickProcessing();
     void processPhotos();
+    void appendFaces(std::vector<Face> &&);
+    void newPhotos(const QModelIndex &, int, int);
 };
 
 #endif
