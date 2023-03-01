0cbbfcce8249d16770a40f9885a5a07d023050d2
Michał Walenciak <Kicer86@gmail.com>
2023-03-01 16:15:56 +0100

    Show name

--- tr/photo_broom_pl.ts
+++ tr/photo_broom_pl.ts
@@ -66,7 +66,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+20"/>
+        <location line="+29"/>
         <source>Photos to be analyzed</source>
         <translation type="unfinished"></translation>
     </message>
