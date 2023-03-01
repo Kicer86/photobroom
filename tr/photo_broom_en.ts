50e705d1e6391b2f83f84853b6d8b1bd9edbd8f9
Michał Walenciak <Kicer86@gmail.com>
2023-03-01 16:15:56 +0100

    Show name

--- tr/photo_broom_en.ts
+++ tr/photo_broom_en.ts
@@ -65,7 +65,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/quick_items/Views/BatchFaceDetection.qml" line="52"/>
+        <location filename="../src/gui/desktop/quick_items/Views/BatchFaceDetection.qml" line="61"/>
         <source>Photos to be analyzed</source>
         <translation type="unfinished"></translation>
     </message>
