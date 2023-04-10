282c021930dc6056c98e9f2fa4ba4567762e2af0
Michał Walenciak <kicer86@gmail.com>
2023-04-10 21:54:41 +0200

    Fix cs-libguarded overlay

--- .github/vcpkg_overlays/cs-libguarded/portfile.cmake
+++ .github/vcpkg_overlays/cs-libguarded/portfile.cmake
@@ -12,4 +12,6 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/CsLibGuarded)
+vcpkg_cmake_config_fixup(CONFIG_PATH cmake/CsLibGuarded)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
