6141040c9f37df7cf8edbfae39a5be340ecfa5e4
Michał Walenciak <Kicer86@gmail.com>
2023-07-16 20:54:14 +0200

    Use proper exception type

--- src/core/implementation/exiv2_exif_reader.cpp
+++ src/core/implementation/exiv2_exif_reader.cpp
@@ -48,7 +48,7 @@ void Exiv2ExifReader::collect(const QString& path)
             assert(m_exif_data.get() != 0);
             m_exif_data->readMetadata();
         }
-        catch (Exiv2::AnyError &)
+        catch (Exiv2::Error &)
         {
             return;
         }
