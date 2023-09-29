4f2041881e58d88e87c06a51ca605e5eff04f52f
Michał Walenciak <Kicer86@gmail.com>
2023-09-29 18:45:04 +0200

    Make it possible for custom types to be QJsonArrays

--- src/core/json_serializer.hpp
+++ src/core/json_serializer.hpp
@@ -34,44 +34,71 @@ namespace JSon
         template<typename T>
         concept hasCustomSerialization = requires(T a)
         {
-            { JSon::CustomType<T>::serialize(a) };
+            { JSon::CustomType<T>::serialize(a) } -> std::same_as<typename JSon::CustomType<T>::type>;
             { JSon::CustomType<T>::deserialize({}) } -> std::same_as<T>;
         };
 
+        template<typename JT, typename T>
+        JT convertTo(const T& valueRef)
+        {
+            if constexpr (std::is_same_v<JT, QJsonArray>)
+            {
+                assert(valueRef.isArray());
 
-        template<typename T>
-        QJsonObject serialize(const T &);
+                QJsonArray array;
+                if constexpr (std::is_same_v<T, QJsonDocument>)
+                    array = valueRef.array();
+                else
+                    array = valueRef.toArray();
+
+                return array;
+            }
+            else if constexpr (std::is_same_v<JT, QJsonObject>)
+            {
+                assert(valueRef.isObject());
+
+                QJsonObject object;
+                if constexpr (std::is_same_v<T, QJsonDocument>)
+                    object = valueRef.object();
+                else
+                    object = valueRef.toObject();
+
+                return object;
+            }
+            else
+                static_assert(false, "Unexpected type");
+        }
+
 
         template<typename T>
         auto getSerialized(const T &);
 
         template<typename T>
-        requires Container<T>
-        QJsonArray serialize(const T& arr)
+        auto serialize(const T& obj)
         {
-            QJsonArray jsonArr;
-
-            for(const auto& i: arr)
-                jsonArr.append(getSerialized(i));
-
-            return jsonArr;
-        }
-
-        template<typename T>
-        QJsonObject serialize(const T& obj)
-        {
-            QJsonObject jsonObj;
-
             if constexpr (hasCustomSerialization<T>)
-                jsonObj = JSon::CustomType<T>::serialize(obj);
+                return JSon::CustomType<T>::serialize(obj);
+            else if constexpr (Container<T>)
+            {
+                QJsonArray jsonArr;
+
+                for(const auto& i: obj)
+                    jsonArr.append(getSerialized(i));
+
+                return jsonArr;
+            }
             else
+            {
+                QJsonObject jsonObj;
+
                 reflectpp::get_object_members(obj, [&jsonObj](auto member_info, const auto& member)
                 {
                     const QString name = QString::fromStdString(std::string(member_info.name));
                     jsonObj[name] = getSerialized(member);
                 });
 
-            return jsonObj;
+                return jsonObj;
+            }
         }
 
         template<typename T>
@@ -91,47 +118,28 @@ namespace JSon
         }
 
         template<typename T, typename JT>
-        T deserialize(const JT &);
-
-        template<typename T>
-        T getDeserialized(const QJsonValueRef &);
-
-        template<typename T, typename JT>
-        requires Container<T>
-        T deserialize(const JT& json)
-        {
-            assert(json.isArray());
-            T r;
-
-            QJsonArray array;
-            if constexpr (std::is_same_v<JT, QJsonDocument>)
-                array = json.array();
-            else
-                array = json.toArray();
-
-            using VT = typename T::value_type;
-
-            for(const auto& e: array)
-                r.push_back(getDeserialized<VT>(e));
-
-            return r;
-        }
+        T getDeserialized(const JT &);
 
         template<typename T, typename JT>
         T deserialize(const JT& json)
         {
-            assert(json.isObject());
             T r;
-
-            QJsonObject object;
-            if constexpr (std::is_same_v<JT, QJsonDocument>)
-                object = json.object();
-            else
-                object = json.toObject();
 
             if constexpr (hasCustomSerialization<T>)
-                return JSon::CustomType<T>::deserialize(object);
+                return JSon::CustomType<T>::deserialize(convertTo<typename JSon::CustomType<T>::type>(json));
+            else if constexpr (Container<T>)
+            {
+                const QJsonArray array = convertTo<QJsonArray>(json);
+
+                using VT = typename T::value_type;
+
+                for(const auto& e: array)
+                    r.push_back(getDeserialized<VT>(e));
+            }
             else
+            {
+                const QJsonObject object = convertTo<QJsonObject>(json);
+
                 reflectpp::set_object_members(r, [&object](const auto member_info)
                 {
                     using VT = decltype(member_info)::type;
@@ -140,15 +148,16 @@ namespace JSon
 
                     return getDeserialized<VT>(value);
                 });
+            }
 
             return r;
         }
 
-        template<typename T>
-        T getDeserialized(const QJsonValueRef& value)
+        template<typename T, typename JT>
+        T getDeserialized(const JT& value)
         {
             if constexpr (hasAssignmentOperator<QJsonValueRef, T>)
-                return value.toVariant().value<T>();
+                return value.toVariant().template value<T>();
             else if constexpr (isQtStreamable<T>)
             {
                 QByteArray d = QByteArray::fromBase64(value.toString().toUtf8());
