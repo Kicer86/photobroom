b807422ee3fc9eb1f7c40bdc9166bfc2cb498119
Kicer86 <Kicer86@users.noreply.github.com>
2023-12-03 10:03:10 +0000

    deploy: 7d3386d8a8e2b67e2d3c4dd67c3afebe09872ac9

--- search/namespaces_3.js
+++ search/namespaces_3.js
@@ -2,8 +2,7 @@ var searchData=
 [
   ['features_0',['features',['../da/dda/namespacegui_1_1features.html',1,'gui']]],
   ['generatorutils_1',['GeneratorUtils',['../de/de4/namespace_generator_utils.html',1,'']]],
-  ['github_2',['GitHub',['../da/d82/namespace_git_hub.html',1,'']]],
-  ['group_3',['Group',['../de/d4b/namespace_group.html',1,'']]],
-  ['groupsmanager_4',['GroupsManager',['../df/d40/namespace_groups_manager.html',1,'']]],
-  ['gui_5',['gui',['../d7/dc4/namespacegui.html',1,'']]]
+  ['group_2',['Group',['../de/d4b/namespace_group.html',1,'']]],
+  ['groupsmanager_3',['GroupsManager',['../df/d40/namespace_groups_manager.html',1,'']]],
+  ['gui_4',['gui',['../d7/dc4/namespacegui.html',1,'']]]
 ];
