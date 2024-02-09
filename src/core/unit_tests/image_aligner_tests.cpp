7dbcf67440203beb58ec139a50b48773044d4e1e
Michał Walenciak <michalwalenciak@gmail.com>
2024-02-09 20:37:43 +0100

    Allow 1 pixel differences

--- src/core/unit_tests/image_aligner_tests.cpp
+++ src/core/unit_tests/image_aligner_tests.cpp
@@ -9,6 +9,54 @@
 using testing::ElementsAre;
 
 
+template<typename>
+struct is_std_vector : std::false_type {};
+
+template<typename T, typename A>
+struct is_std_vector<std::vector<T,A>> : std::true_type {};
+
+template<typename T> struct always_false : std::false_type {};
+
+template<typename T>
+bool areNotSimilar(const T& lhs, const T& rhs)
+{
+    return std::abs(lhs - rhs) > 1;
+}
+
+
+MATCHER_P(isSimilarTo, v, "")
+{
+    using argT = std::remove_cvref_t<decltype(arg)>;
+    using vT = std::remove_cvref_t<decltype(v)>;
+    static_assert(std::is_same_v<argT, vT>, "Argument and expected value are of different types");
+
+    if constexpr (is_std_vector<argT>::value)
+    {
+        const auto s = arg.size();
+        assert(v.size() == s);
+
+        for(std::size_t i = 0; i < s; i++)
+            if (areNotSimilar(arg[i], v[i]))
+                return false;
+    }
+    else if constexpr (std::is_same_v<argT, QRect>)
+    {
+        if (areNotSimilar(arg.x(), v.x()))
+            return false;
+        if (areNotSimilar(arg.y(), v.y()))
+            return false;
+        if (areNotSimilar(arg.width(), v.width()))
+            return false;
+        if (areNotSimilar(arg.height(), v.height()))
+            return false;
+    }
+    else
+        static_assert(always_false<argT>::value, "Argument type is not supported");
+
+    return true;
+}
+
+
 namespace
 {
     std::vector<long> transformationValues(const cv::Mat& mat)
@@ -42,14 +90,15 @@ TEST(ImageAlignerTest, mixOfImagesOfTheSameSize)
 
     auto transformationV = range_to<std::vector<std::vector<long>>>(transformations | std::ranges::views::transform(transformationValues));
 
+    // allow 1 pixel differences as different versions of openCV produce a little bit different results
     EXPECT_THAT(transformationV, ElementsAre(
-        std::vector<long>{1, 0, 0, 0, 1, 0},
-        std::vector<long>{1, 0, -20, 0, 1, 0},
-        std::vector<long>{1, 0, 20, 0, 1, 0},
-        std::vector<long>{1, 0, 189, 0, 1, -159},
-        std::vector<long>{1, 0, -158, 0, 1, 189}
+        isSimilarTo(std::vector<long>{1, 0, 0, 0, 1, 0}),
+        isSimilarTo(std::vector<long>{1, 0, -20, 0, 1, 0}),
+        isSimilarTo(std::vector<long>{1, 0, 20, 0, 1, 0}),
+        isSimilarTo(std::vector<long>{1, 0, 189, 0, 1, -159}),
+        isSimilarTo(std::vector<long>{1, 0, -158, 0, 1, 189})
     ));
 
     const auto commonPart = aligner.imagesCommonPart(transformations);
-    EXPECT_EQ(commonPart, QRect(158, 159, 1633, 1652));
+    EXPECT_THAT(commonPart, isSimilarTo(QRect(158, 159, 1633, 1652)));
 }
