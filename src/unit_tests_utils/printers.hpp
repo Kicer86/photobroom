f12bc770af5462950688b3ef52456eee881cffd9
Michał Walenciak <michalwalenciak@gmail.com>
2024-02-12 19:26:55 +0100

    Expand PrintTo for most Qt types with one simple trick

--- src/unit_tests_utils/printers.hpp
+++ src/unit_tests_utils/printers.hpp
@@ -9,32 +9,23 @@
 #include <QSize>
 #include <QString>
 
-inline void PrintTo(const QString& qstring, ::std::ostream* os)
-{
-    *os << qstring.toStdString();
-}
 
-inline void PrintTo(const QPoint& qpoint, std::ostream* os)
+template<typename T>
+concept isQtPrintable = requires(QDebug d, T a)
 {
-    *os << "x = " << qpoint.x() << ", y = " << qpoint.y();
-}
+    { d << a };
+};
 
-inline void PrintTo(const QSize& qsize, std::ostream* os)
-{
-    *os << "width = " << qsize.width() << ", height = " << qsize.height();
-}
 
-inline void PrintTo(const QRect& qrect, std::ostream* os)
+template<typename T>
+requires isQtPrintable<T>
+inline void PrintTo(const T& qtObj, ::std::ostream* os)
 {
-    PrintTo(qrect.topLeft(), os);
-    *os << ", ";
-    PrintTo(qrect.size(), os);
-}
+    QString qtStr;
+    QDebug debug(&qtStr);
+    debug << qtObj;
 
-inline void PrintTo(const QModelIndex& qmodelindex, std::ostream* os)
-{
-    *os << "row = " << qmodelindex.row() << ", column = " << qmodelindex.column();
-    *os << ", model = " << qmodelindex.model() << ", internalId = " << qmodelindex.internalId();
+    *os << qtStr.toStdString();
 }
 
 #endif
