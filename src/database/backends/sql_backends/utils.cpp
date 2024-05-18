1a3e003dc81c60102f11f54117a283c962ab5490
Michał Walenciak <michalwalenciak@gmail.com>
2024-05-18 10:59:57 +0200

    Handle invalid input

--- src/database/backends/sql_backends/utils.cpp
+++ src/database/backends/sql_backends/utils.cpp
@@ -10,12 +10,17 @@ namespace Database
     QRect decodeFaceLocation(const QString& l)
     {
         const QStringList location_list = l.split(QRegularExpression("[ ,x]"));
-        const QRect location(location_list[0].toInt(),
-                             location_list[1].toInt(),
-                             location_list[2].toInt(),
-                             location_list[3].toInt());
+        if (location_list.size() == 4)
+        {
+            const QRect location(location_list[0].toInt(),
+                                 location_list[1].toInt(),
+                                 location_list[2].toInt(),
+                                 location_list[3].toInt());
 
-        return location;
+            return location;
+        }
+        else
+            return {};
     }
 
 
