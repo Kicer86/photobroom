a1511d6a580d6b95b2124335cba965659ff2889c
Michał Walenciak <michalwalenciak@gmail.com>
2024-10-06 21:15:24 +0200

    Make sure photos grid grabs focus

--- tr/photo_broom_en.ts
+++ tr/photo_broom_en.ts
@@ -750,17 +750,17 @@ Error code: %1</source>
 <context>
     <name>PhotosView</name>
     <message>
-        <location filename="../src/gui/desktop/quick_items/Views/PhotosView.qml" line="214"/>
+        <location filename="../src/gui/desktop/quick_items/Views/PhotosView.qml" line="215"/>
         <source>&lt;b&gt;Properties&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/quick_items/Views/PhotosView.qml" line="227"/>
+        <location filename="../src/gui/desktop/quick_items/Views/PhotosView.qml" line="228"/>
         <source>&lt;b&gt;Media information&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/desktop/quick_items/Views/PhotosView.qml" line="240"/>
+        <location filename="../src/gui/desktop/quick_items/Views/PhotosView.qml" line="241"/>
         <source>&lt;b&gt;Debug window&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
