ad400f34d2bbaaeeec8f2784adff5b3c8b5e0a02
Michał Walenciak <michalwalenciak@gmail.com>
2025-01-14 21:31:24 +0100

    Move all computations into thread

--- src/gui/desktop/quick_items/media_view_ctrl.cpp
+++ src/gui/desktop/quick_items/media_view_ctrl.cpp
@@ -5,20 +5,54 @@
 #include <core/function_wrappers.hpp>
 #include <core/iexif_reader.hpp>
 #include <core/media_types.hpp>
+#include <core/task_executor_utils.hpp>
+#include <database/database_executor_traits.hpp>
 
 #include "media_view_ctrl.hpp"
 #include "objects_accessor.hpp"
 
 
+namespace
+{
+    MediaViewCtrl::Mode getModeFor(ICoreFactoryAccessor* core, const QString& path)
+    {
+        MediaViewCtrl::Mode mode = MediaViewCtrl::Mode::Unknown;
+
+        if (MediaTypes::isAnimatedImageFile(path))
+            mode = MediaViewCtrl::Mode::AnimatedImage;
+        else if (MediaTypes::isImageFile(path))
+        {
+            auto& exifReader = core->getExifReaderFactory().get();
+            const auto projection = exifReader.get(path, IExifReader::TagType::Projection);
+            if (projection && std::any_cast<std::string>(*projection) == "equirectangular")
+                mode = MediaViewCtrl::Mode::EquirectangularProjectionImage;
+            else
+                mode = MediaViewCtrl::Mode::StaticImage;
+        }
+        else if (MediaTypes::isVideoFile(path))
+            mode = MediaViewCtrl::Mode::Video;
+        else
+            mode = MediaViewCtrl::Mode::Error;
+
+        return mode;
+    }
+}
+
+
 MediaViewCtrl::~MediaViewCtrl()
 {
     m_pathFetchFuture.cancel();
-    m_pathFetchFuture.waitForFinished();
 }
 
 
 void MediaViewCtrl::setSource(const Photo::Id& id)
 {
+    if (m_core == nullptr)
+    {
+        invokeMethodLater(this, &MediaViewCtrl::setSource, id);
+        return;
+    }
+
     assert(id.valid());
     m_id = id;
 
@@ -29,20 +63,29 @@ void MediaViewCtrl::setSource(const Photo::Id& id)
 
     if (db && id.valid())
     {
-        QPromise<QString> promise;
+        QPromise<std::pair<QUrl, Mode>> promise;
         m_pathFetchFuture = promise.future();
-        m_pathFetchFuture.then(this, std::bind(&MediaViewCtrl::setPath, this, std::placeholders::_1));
-
-        auto task = [id, promise = std::move(promise)](Database::IBackend& backend) mutable
+        m_pathFetchFuture.then(this, [this](const std::pair<QUrl, Mode>& url_and_mode)
         {
-            // MediaViewCtrl may be destroyed here
-            const auto data = backend.getPhotoDelta<Photo::Field::Path>(id);
+            setPath(url_and_mode.first);
+            setMode(url_and_mode.second);
+        });
 
-            promise.addResult(data.get<Photo::Field::Path>());
+        runOn(m_core->getTaskExecutor(), [db, id, core = m_core, promise = std::move(promise)]() mutable
+        {
+            const auto data = evaluate(*db, [id](Database::IBackend& backend)
+            {
+                return backend.getPhotoDelta<Photo::Field::Path>(id);
+            });
+
+            const auto path = data.get<Photo::Field::Path>();
+            const QFileInfo pathInfo(path);
+            const auto url = QUrl::fromLocalFile(pathInfo.absoluteFilePath());       // QML's MediaPlayer does not like 'prj:' prefix
+            const auto mode = getModeFor(core, path);
+
+            promise.addResult(std::make_pair(url, mode));
             promise.finish();
-        };
-
-        db->exec(std::move(task));
+        });
     }
 }
 
@@ -71,36 +114,10 @@ MediaViewCtrl::Mode MediaViewCtrl::mode() const
 }
 
 
-void MediaViewCtrl::setPath(const QString& path)
+void MediaViewCtrl::setPath(const QUrl& path)
 {
-    if (m_core == nullptr)
-    {
-        invokeMethodLater(this, &MediaViewCtrl::setPath, path);
-        return;
-    }
-
-    setMode(Mode::Unknown);
-
-    const QFileInfo pathInfo(path);
-
-    m_path = QUrl::fromLocalFile(pathInfo.absoluteFilePath());       // QML's MediaPlayer does not like 'prj:' prefix
+    m_path = path;
     emit pathChanged(m_path);
-
-    if (MediaTypes::isAnimatedImageFile(path))
-        setMode(Mode::AnimatedImage);
-    else if (MediaTypes::isImageFile(path))
-    {
-        auto& exifReader = m_core->getExifReaderFactory().get();
-        const auto projection = exifReader.get(path, IExifReader::TagType::Projection);
-        if (projection && std::any_cast<std::string>(*projection) == "equirectangular")
-            setMode(Mode::EquirectangularProjectionImage);
-        else
-            setMode(Mode::StaticImage);
-    }
-    else if (MediaTypes::isVideoFile(path))
-        setMode(Mode::Video);
-    else
-        setMode(Mode::Error);
 }
 
 
