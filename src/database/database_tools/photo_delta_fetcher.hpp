11ffd8c72ada2715c226447de0935323a5bac725
Michał Walenciak <michalwalenciak@gmail.com>
2026-02-09 20:41:08 +0100

    Use coroutines

--- src/database/database_tools/photo_delta_fetcher.hpp
+++ src/database/database_tools/photo_delta_fetcher.hpp
@@ -2,7 +2,6 @@
 #pragma once
 
 #include <QObject>
-#include <QFuture>
 
 #include <database/photo_data.hpp>
 #include <database/idatabase.hpp>
@@ -16,15 +15,11 @@ class DATABASE_EXPORT PhotoDeltaFetcher: public QObject
 
     public:
         explicit PhotoDeltaFetcher(Database::IDatabase &);
-        ~PhotoDeltaFetcher();
 
         void fetchIds(const std::vector<Photo::Id> &, const std::set<Photo::Field> &);
 
     private:
-        QFuture<std::vector<Photo::DataDelta>> m_dataFetchFuture;
         Database::IDatabase& m_db;
-
-        void storePhotoData(const std::vector<Photo::DataDelta> &);
 
     signals:
         void photoDataDeltaFetched(const std::vector<Photo::DataDelta> &) const;
