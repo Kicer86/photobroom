bac2571bb317496cf2ca49d91b6af5a72137da0f
Michał Walenciak <michalwalenciak@gmail.com>
2026-07-25 23:04:08 +0200

    Introduce base for Filesystem ecosystem

---
+++ src/core/filesystem.hpp
@@ -0,0 +1,14 @@
+
+#ifndef FILESYSTEM_HPP_INCLUDED
+#define FILESYSTEM_HPP_INCLUDED
+
+
+namespace Filesystem
+{
+    class IFile
+    {
+
+    };
+}
+
+#endif // FILESYSTEM_HPP_INCLUDED
---
+++ src/core/implementation/filesystem.cpp
@@ -0,0 +1,7 @@
+
+#include "filesystem.hpp"
+
+namespace Filesystem
+{
+
+}
